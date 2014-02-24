da41eb3883622a333e6e84056c05964bc2e4e402
David Steele <github@thelabyrinth.net>
2014-02-24 17:59:42 -0500

    Added more warnings

--- pg_backrest_backup.pm
+++ pg_backrest_backup.pm
@@ -168,9 +168,11 @@ sub backup_thread_complete
         {
             if (time() - $lTimeBegin >= $iTimeout)
             {
+                &log(WARN, "threads have been running more than ${iTimeout} seconds, exiting...");
+
                 backup_thread_kill();
 
-                confess &log(WARN, "threads have been running more than ${iTimeout} seconds, exiting...")
+                confess &log(WARN, "all threads have exited, aborting...");
             }
         }
         
