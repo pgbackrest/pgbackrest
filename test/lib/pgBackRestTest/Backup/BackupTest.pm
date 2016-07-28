e033bf43980943b355bc4946c8b9605abb02554e
Cynthia Shang <cynthia.shang@crunchydata.com>
2016-07-28 12:12:19 -0400

    Fixed archive-get tests to copy pg_control for the getCheck function.

--- test/lib/pgBackRestTest/Backup/BackupTest.pm
+++ test/lib/pgBackRestTest/Backup/BackupTest.pm
@@ -470,6 +470,13 @@ sub backupTestRun
                 my $strXlogPath = $oHostDbMaster->dbBasePath() . '/pg_xlog';
                 filePathCreate($strXlogPath, undef, false, true);
 
+                # Create the test path for pg_control
+                filePathCreate(($oHostDbMaster->dbBasePath() . '/' . DB_PATH_GLOBAL), undef, false, true);
+
+                # Copy pg_control
+                executeTest('cp ' . testDataPath() . '/pg_control ' .
+                            $oHostDbMaster->dbBasePath() . '/' . DB_FILE_PGCONTROL);
+
                 my $strCommand =
                     $oHostDbMaster->backrestExe() .
                     ' --config=' . $oHostDbMaster->backrestConfig() .
@@ -481,7 +488,7 @@ sub backupTestRun
 
                 # Create the archive info file
                 filePathCreate($oFile->pathGet(PATH_BACKUP_ARCHIVE), '0770', undef, true);
-                (new pgBackRest::ArchiveInfo($oFile->pathGet(PATH_BACKUP_ARCHIVE)))->check(PG_VERSION_93, 1234567890123456789);
+                (new pgBackRest::ArchiveInfo($oFile->pathGet(PATH_BACKUP_ARCHIVE)))->check(PG_VERSION_93, 6156904820763115222);
 
                 if (defined($oLogTest))
                 {
@@ -1081,7 +1088,7 @@ sub backupTestRun
             $oHostDbMaster->manifestReference(\%oManifest, $strFullBackup);
 
             # Break the database version
-            $oHostBackup->infoMunge(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)),
+            $oHostBackup->infoMunge($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO),
                             {&INFO_BACKUP_SECTION_DB =>
                                 {&INFO_BACKUP_KEY_DB_VERSION => '8.0'}});
 
@@ -1091,10 +1098,10 @@ sub backupTestRun
                     strOptionalParam => '--log-level-console=detail'});
 
             # Restore the file to its original condition
-            $oHostBackup->infoRestore(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)));
+            $oHostBackup->infoRestore($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO));
 
             # Break the database system id
-            $oHostBackup->infoMunge(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)),
+            $oHostBackup->infoMunge($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO),
                             {&INFO_BACKUP_SECTION_DB =>
                                 {&INFO_BACKUP_KEY_SYSTEM_ID => 6999999999999999999}});
 
@@ -1104,10 +1111,10 @@ sub backupTestRun
                     strOptionalParam => '--log-level-console=detail'});
 
             # Restore the file to its original condition
-            $oHostBackup->infoRestore(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)));
+            $oHostBackup->infoRestore($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO));
 
             # Break the control version
-            $oHostBackup->infoMunge(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)),
+            $oHostBackup->infoMunge($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO),
                             {&INFO_BACKUP_SECTION_DB =>
                                 {&INFO_BACKUP_KEY_CONTROL => 842}});
 
@@ -1117,10 +1124,10 @@ sub backupTestRun
                     strOptionalParam => '--log-level-console=detail'});
 
             # Restore the file to its original condition
-            $oHostBackup->infoRestore(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)));
+            $oHostBackup->infoRestore($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO));
 
             # Break the catalog version
-            $oHostBackup->infoMunge(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)),
+            $oHostBackup->infoMunge($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO),
                             {&INFO_BACKUP_SECTION_DB =>
                                 {&INFO_BACKUP_KEY_CATALOG => 197208141}});
 
@@ -1130,7 +1137,7 @@ sub backupTestRun
                     strOptionalParam => '--log-level-console=detail'});
 
             # Restore the file to its original condition
-            $oHostBackup->infoRestore(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)));
+            $oHostBackup->infoRestore($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO));
 
             # Test broken tablespace configuration
             #-----------------------------------------------------------------------------------------------------------------------
@@ -1637,7 +1644,7 @@ sub backupTestRun
             $strComment = 'fail on archive mismatch after upgrade';
 
             # load the archive info file and munge it for testing by breaking the database version
-            $oHostBackup->infoMunge(($oFile->pathGet(PATH_BACKUP_ARCHIVE, ARCHIVE_INFO_FILE)),
+            $oHostBackup->infoMunge($oFile->pathGet(PATH_BACKUP_ARCHIVE, ARCHIVE_INFO_FILE),
                                         {&INFO_ARCHIVE_SECTION_DB => {&INFO_ARCHIVE_KEY_DB_VERSION => '8.0'}});
             $oHostDbMaster->check($strComment, {iTimeout => 0.1, iExpectedExitStatus => ERROR_ARCHIVE_MISMATCH});
             # If running the remote tests then also need to run check locally
@@ -1683,7 +1690,7 @@ sub backupTestRun
                 $oHostBackup->check($strComment, {iTimeout => 5, iExpectedExitStatus => ERROR_BACKUP_MISMATCH});
 
                 # Restore the file to its original condition
-                $oHostBackup->infoRestore(($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO)));
+                $oHostBackup->infoRestore($oFile->pathGet(PATH_BACKUP_CLUSTER, FILE_BACKUP_INFO));
 
                 # Providing a sufficient archive-timeout, verify that the check command runs successfully now with valid
                 # archive.info and backup.info files
