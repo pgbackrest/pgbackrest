862f6fe56018d6a891a462520bc4200da7f063e8
David Steele <david@pgmasters.net>
2017-04-09 21:04:17 -0400

    Coverage change.

--- test/lib/pgBackRestTest/Common/RunTest.pm
+++ test/lib/pgBackRestTest/Common/RunTest.pm
@@ -502,9 +502,8 @@ sub testRunExe
     if (defined($strPerlModule))
     {
         $strExe =
-            'perl -MDevel::Cover=-silent,1,-dir,' . $strCoveragePath . ',-subs_only,1' .
-            ",-select${strPerlModule},+inc," . $strBackRestBasePath .
-            ',-coverage,statement,branch,condition,path,subroutine' . " ${strExe}";
+            "perl -MDevel::Cover=-silent,1,-dir,${strCoveragePath},-select${strPerlModule},+inc,${strBackRestBasePath}" .
+            ",-coverage,statement,branch,condition,path,subroutine ${strExe}";
 
         if (defined($bLog) && $bLog)
         {
