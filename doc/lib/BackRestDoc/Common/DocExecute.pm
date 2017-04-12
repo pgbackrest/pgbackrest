pgbackrest/pgbackrest

commit 5d2e79242da79854ee922c1a84dec0e67d52f090

Fixed user change in docs missed in vagrant upgrade.

doc/lib/BackRestDoc/Common/DocExecute.pm
@@ -7,6 +7,7 @@ use parent 'BackRestDoc::Common::DocRender';
 use strict;
 use warnings FATAL => qw(all);
 use Carp qw(confess);
+use English '-no_match_vars';
 
 use Exporter qw(import);
     our @EXPORT = qw();
@@ -25,6 +26,11 @@ use pgBackRestTest::Common::HostTest;
 use pgBackRestTest::Common::HostGroupTest;
 
 use BackRestDoc::Common::DocManifest;
+
+####################################################################################################################################
+# User that's building the docs
+####################################################################################################################################
+use constant DOC_USER                                              => getpwuid($UID) . '';
 
 ####################################################################################################################################
 # CONSTRUCTOR
@@ -103,7 +109,7 @@ sub executeKey
     # Add user to command
     my $strCommand = $self->{oManifest}->variableReplace(trim($oCommand->fieldGet('exe-cmd')));
     my $strUser = $self->{oManifest}->variableReplace($oCommand->paramGet('user', false, 'postgres'));
-    $strCommand = ($strUser eq 'vagrant' ? '' : ('sudo ' . ($strUser eq 'root' ? '' : "-u $strUser "))) . $strCommand;
+    $strCommand = ($strUser eq DOC_USER ? '' : ('sudo ' . ($strUser eq 'root' ? '' : "-u $strUser "))) . $strCommand;
 
     # Format and split command
     $strCommand =~ s/[ ]*\n[ ]*/ \\\n    /smg;
@@ -553,7 +559,7 @@ sub backrestConfig
                 }
             }
 
-            my $strLocalFile = "/home/vagrant/data/pgbackrest.conf";
+            my $strLocalFile = '/home/' . DOC_USER . '/data/pgbackrest.conf';
 
             # Save the ini file
             iniSave($strLocalFile, $self->{config}{$strHostName}{$$hCacheKey{file}}, true);
@@ -651,7 +657,7 @@ sub postgresConfig
                 confess &log(ERROR, "cannot configure postgres on host ${strHostName} because the host does not exist");
             }
 
-            my $strLocalFile = '/home/vagrant/data/postgresql.conf';
+            my $strLocalFile = '/home/' . DOC_USER . '/data/postgresql.conf';
             $oHost->copyFrom($$hCacheKey{file}, $strLocalFile);
 
             if (!defined(${$self->{'pg-config'}}{$strHostName}{$$hCacheKey{file}}{base}) && $self->{bExe})

doc/xml/user-guide.xml
@@ -48,7 +48,7 @@
         <variable key="host-os" keyword="default">u14</variable>
         <variable key="host-os" keyword="co6">co6</variable>
 
-        <variable key="host-user">vagrant</variable>
+        <variable key="host-user" eval="y">use English '-no_match_vars'; return getpwuid($UID) . '';</variable>
         <variable key="host-mount">/backrest:/backrest</variable>
         <variable key="image-repo">pgbackrest/test</variable>
 

commit 5c635e0f0a1f5dc8ab8ef8c65204e81d3495afcb

Go back to using static user for documentation.
Making this dynamic in commit 5d2e792 broke doc builds from cache.  The long-term solution is to create a special user for doc builds but that’s beyond the scope of this release.

doc/lib/BackRestDoc/Common/DocExecute.pm
@@ -30,7 +30,7 @@ use BackRestDoc::Common::DocManifest;
 ####################################################################################################################################
 # User that's building the docs
 ####################################################################################################################################
-use constant DOC_USER                                              => getpwuid($UID) . '';
+use constant DOC_USER                                              => 'ubuntu';
 
 ####################################################################################################################################
 # CONSTRUCTOR

doc/xml/user-guide.xml
@@ -61,7 +61,7 @@
         <variable key="host-os" keyword="default">u14</variable>
         <variable key="host-os" keyword="co6">co6</variable>
 
-        <variable key="host-user" eval="y">use English '-no_match_vars'; return getpwuid($UID) . '';</variable>
+        <variable key="host-user">ubuntu</variable>
         <variable key="host-mount">/backrest:/backrest</variable>
         <variable key="image-repo">pgbackrest/test</variable>
 
@@ -171,14 +171,14 @@
         <title>Installation</title>
 
         <host-add name="{[host-db-master]}" user="{[host-db-master-user]}" image="{[host-db-master-image]}" os="{[host-os]}" mount="{[host-db-master-mount]}">
-            <execute user="{[host-user]}">
-                <exe-cmd>mkdir /home/{[host-user]}/pgbackrest-release-{[version]}</exe-cmd>
+            <execute user="root">
+                <exe-cmd>mkdir /root/pgbackrest-release-{[version]}</exe-cmd>
             </execute>
-            <execute user="{[host-user]}">
-                <exe-cmd>cp -r /backrest/bin /home/{[host-user]}/pgbackrest-release-{[version]}</exe-cmd>
+            <execute user="root">
+                <exe-cmd>cp -r /backrest/bin /root/pgbackrest-release-{[version]}</exe-cmd>
             </execute>
-            <execute user="{[host-user]}">
-                <exe-cmd>cp -r /backrest/lib /home/{[host-user]}/pgbackrest-release-{[version]}</exe-cmd>
+            <execute user="root">
+                <exe-cmd>cp -r /backrest/lib /root/pgbackrest-release-{[version]}</exe-cmd>
             </execute>
         </host-add>
 
@@ -212,10 +212,10 @@
         <execute-list host="{[host-db-master]}">
             <title>Download version <id>{[version]}</id> of <backrest/></title>
 
-            <execute user="{[host-user]}" skip="y">
+            <execute user="root" skip="y">
                 <exe-cmd>wget -q -O -
                     {[github-url-release]}/{[version]}.tar.gz |
-                    tar zx -C ~</exe-cmd>
+                    tar zx -C /root</exe-cmd>
             </execute>
         </execute-list>
 
@@ -250,7 +250,7 @@
             <title>Install <backrest/></title>
 
             <execute user="root">
-                <exe-cmd>cp -r ~/pgbackrest-release-{[version]}/lib/pgBackRest
+                <exe-cmd>cp -r /root/pgbackrest-release-{[version]}/lib/pgBackRest
                     {[perl-lib-path]}</exe-cmd>
             </execute>
             <execute user="root">
@@ -260,7 +260,7 @@
                 <exe-cmd>find {[perl-lib-path]}/pgBackRest -type d -exec chmod 755 {} +</exe-cmd>
             </execute>
             <execute user="root">
-                <exe-cmd>cp ~/pgbackrest-release-{[version]}/bin/{[project-exe]} {[perl-bin-path]}/{[project-exe]}</exe-cmd>
+                <exe-cmd>cp /root/pgbackrest-release-{[version]}/bin/{[project-exe]} {[perl-bin-path]}/{[project-exe]}</exe-cmd>
             </execute>
             <execute user="root">
                 <exe-cmd>chmod 755 {[perl-bin-path]}/{[project-exe]}</exe-cmd>