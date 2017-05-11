6f807bf9cffacca0ff6b7c1acad1487787024431
Cynthia Shang <cynthia.shang@crunchydata.com>
2017-05-11 10:34:09 -0400

    Moved check for standby and added WARNING if not object defined but backup-standby=y

--- lib/pgBackRest/Backup.pm
+++ lib/pgBackRest/Backup.pm
@@ -490,6 +490,15 @@ sub process
     # Get the database objects
     ($oDbMaster, $self->{iMasterRemoteIdx}, $oDbStandby, $self->{iCopyRemoteIdx}) = dbObjectGet();
 
+    # Backup from standby option is set but the configuration is not correct. Turn off OPTION_BACKUP_STANDBY and warn that
+    # the backup will be performed from the master.
+    if (!defined($oDbStandby) && optionGet(OPTION_BACKUP_STANDBY))
+    {
+        optionSet(OPTION_BACKUP_STANDBY, false);
+        &log(WARN, 'option backup-standby is enabled but standby is not properly configured - ' .
+            'backup will be performed from the master');
+    }
+
     # If remote copy was not explicitly set then set it equal to master
     if (!defined($self->{iCopyRemoteIdx}))
     {
@@ -653,34 +662,23 @@ sub process
         # Wait for replay on the standby to catch up
         if (optionGet(OPTION_BACKUP_STANDBY))
         {
-            if (defined($oDbStandby))
-            {
-                my ($strStandbyDbVersion, $iStandbyControlVersion, $iStandbyCatalogVersion, $ullStandbyDbSysId) = $oDbStandby->info();
-                $oBackupInfo->check($strStandbyDbVersion, $iStandbyControlVersion, $iStandbyCatalogVersion, $ullStandbyDbSysId);
+            my ($strStandbyDbVersion, $iStandbyControlVersion, $iStandbyCatalogVersion, $ullStandbyDbSysId) = $oDbStandby->info();
+            $oBackupInfo->check($strStandbyDbVersion, $iStandbyControlVersion, $iStandbyCatalogVersion, $ullStandbyDbSysId);
 
-                $oDbStandby->configValidate();
+            $oDbStandby->configValidate();
 
-                &log(INFO, "wait for replay on the standby to reach ${strLsnStart}");
+            &log(INFO, "wait for replay on the standby to reach ${strLsnStart}");
 
-                my ($strReplayedLSN, $strCheckpointLSN) = $oDbStandby->replayWait($strLsnStart);
+            my ($strReplayedLSN, $strCheckpointLSN) = $oDbStandby->replayWait($strLsnStart);
 
-                &log(
-                    INFO,
-                    "replay on the standby reached ${strReplayedLSN}" .
-                        (defined($strCheckpointLSN) ? ", checkpoint ${strCheckpointLSN}" : ''));
+            &log(
+                INFO,
+                "replay on the standby reached ${strReplayedLSN}" .
+                    (defined($strCheckpointLSN) ? ", checkpoint ${strCheckpointLSN}" : ''));
 
-                # The standby db object won't be used anymore so undef it to catch any subsequent references
-                undef($oDbStandby);
-                protocolDestroy(DB, $self->{iCopyRemoteIdx}, true);
-            }
-            # Backup from standby option is set but the configuration is not correct. Turn off OPTION_BACKUP_STANDBY and warn that
-            # the backup will be from the master.
-            else
-            {
-                optionSet(OPTION_BACKUP_STANDBY, false);
-                &log(WARN, 'option backup-standby is enabled but standby is not properly configured; ' .
-                    'backup will be perfromed from the master');
-            }
+            # The standby db object won't be used anymore so undef it to catch any subsequent references
+            undef($oDbStandby);
+            protocolDestroy(DB, $self->{iCopyRemoteIdx}, true);
         }
     }
 
