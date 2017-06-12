c10c5bb0ef1de519371e1bdae0b9da7cf8670c95
David Steele <david@crunchydata.com>
2017-06-12 11:36:00 -0400

    v1.19: S3 Support
    Bug Fixes:
    
    * Fixed the info command so the WAL archive min/max displayed is for the current database version. (Fixed by Cynthia Shang.)
    * Fixed the backup command so the backup-standby option is reset (and the backup proceeds on the master) if the standby is not configured and/or reachable. (Fixed by Cynthia Shang.)
    * Fixed config warnings raised from a remote process causing errors in the master process. (Fixed by Cynthia Shang.)
    
    Features:
    
    * Amazon S3 repository support. (Reviewed by Cynthia Shang.)
    
    Refactoring:
    
    * Refactor storage layer to allow for new repository filesystems using drivers. (Reviewed by Cynthia Shang.)
    * Refactor IO layer to allow for new compression formats, checksum types, and other capabilities using filters. (Reviewed by Cynthia Shang.)
    * Move modules in Protocol directory in subdirectories.
    * Move backup modules into Backup directory.

--- libc/lib/pgBackRest/LibC.pm
+++ libc/lib/pgBackRest/LibC.pm
@@ -11,7 +11,7 @@ use AutoLoader;
 our @ISA = qw(Exporter);
 
 # Library version (add .999 during development)
-our $VERSION = '1.19.999';
+our $VERSION = '1.19';
 
 sub libCVersion {return $VERSION};
 
