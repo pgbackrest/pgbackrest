9b98c2e7d6492b626267ffa0c50c67922dd43bda
David Steele <david@pgmasters.net>
2017-10-22 13:11:03 -0400

    Run all tests on tempfs rather than local disk.

--- test/test.pl
+++ test/test.pl
@@ -335,7 +335,11 @@ eval
                 push(@{$oyProcess}, undef);
             }
 
+
+            executeTest("sudo umount ${strTestPath}", {bSuppressError => true});
             executeTest("sudo rm -rf ${strTestPath}/*");
+            $oStorageTest->pathCreate($strTestPath, {strMode => '0770', bIgnoreExists => true});
+            executeTest("sudo mount -t tmpfs -o size=2560M test ${strTestPath}");
             $oStorageTest->pathCreate($strCoveragePath, {strMode => '0770', bIgnoreMissing => true, bCreateParent => true});
         }
 
