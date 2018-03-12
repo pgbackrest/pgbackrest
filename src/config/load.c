dd31ae832d6d5278079d5e84c471b0258f51940b
Cynthia Shang <cynthia.shang@crunchydata.com>
2018-03-12 11:25:20 -0400

    Port most of Config::Config::configLoad() from Perl to C.
    Contributed by Cynthia Shang.

--- src/config/load.c
+++ src/config/load.c
@@ -2,6 +2,7 @@
 Configuration Load
 ***********************************************************************************************************************************/
 #include <string.h>
+#include <sys/stat.h>
 
 #include "command/command.h"
 #include "common/memContext.h"
@@ -82,6 +83,133 @@ cfgLoadParam(unsigned int argListSize, const char *argList[], String *exe)
                 {
                     if (cfgOptionTest(cfgOptPgHost + optionIdx) && cfgOptionSource(cfgOptPgHostCmd + optionIdx) == cfgSourceDefault)
                         cfgOptionDefaultSet(cfgOptPgHostCmd + optionIdx, varNewStr(cfgExe()));
+                }
+            }
+        }
+
+        // Neutralize the umask to make the repository file/path modes more consistent
+        if (cfgOptionValid(cfgOptNeutralUmask) && cfgOptionBool(cfgOptNeutralUmask))
+            umask(0000);
+
+        // Protocol timeout should be greater than db timeout
+        if (cfgOptionTest(cfgOptDbTimeout) && cfgOptionTest(cfgOptProtocolTimeout) &&
+            cfgOptionDbl(cfgOptProtocolTimeout) <= cfgOptionDbl(cfgOptDbTimeout))
+        {
+            // If protocol-timeout is default then increase it to be greater than db-timeout
+            if (cfgOptionSource(cfgOptProtocolTimeout) == cfgSourceDefault)
+                cfgOptionSet(cfgOptProtocolTimeout, cfgSourceDefault, varNewDbl(cfgOptionDbl(cfgOptDbTimeout) + 30));
+            else
+            {
+                THROW(OptionInvalidValueError,
+                    "'%f' is not valid for '%s' option\nHINT '%s' option (%f) should be greater than '%s' option (%f).",
+                    cfgOptionDbl(cfgOptProtocolTimeout), cfgOptionName(cfgOptProtocolTimeout),
+                    cfgOptionName(cfgOptProtocolTimeout), cfgOptionDbl(cfgOptProtocolTimeout), cfgOptionName(cfgOptDbTimeout),
+                    cfgOptionDbl(cfgOptDbTimeout));
+            }
+        }
+
+        // Make sure that repo and pg host settings are not both set - cannot both be remote
+        if (cfgOptionValid(cfgOptPgHost) && cfgOptionValid(cfgOptRepoHost))
+        {
+            bool pgHostFound = false;
+
+            for (unsigned int optionIdx = 0; optionIdx < cfgOptionIndexTotal(cfgOptPgHost); optionIdx++)
+            {
+                if (cfgOptionTest(cfgOptPgHost + optionIdx))
+                {
+                    pgHostFound = true;
+                    break;
+                }
+            }
+
+            // If a pg-host was found, see if a repo-host is configured
+            if (pgHostFound == true)
+            {
+                for (unsigned int optionIdx = 0; optionIdx < cfgOptionIndexTotal(cfgOptRepoHost); optionIdx++)
+                {
+                    if (cfgOptionTest(cfgOptRepoHost + optionIdx))
+                        THROW(ConfigError, "pg and repo hosts cannot both be configured as remote");
+                }
+            }
+        }
+
+        // Warn when repo-retention-full is not set on a configured repo
+        if (cfgOptionValid(cfgOptRepoRetentionFull))
+        {
+            for (unsigned int optionIdx = 0; optionIdx < cfgOptionIndexTotal(cfgOptRepoType); optionIdx++)
+            {
+                // If the repo-type is defined, then see if corresponding retention-full is set
+                if (cfgOptionTest(cfgOptRepoType + optionIdx) && !cfgOptionTest(cfgOptRepoRetentionFull + optionIdx))
+                {
+                    LOG_WARN("option %s is not set, the repository may run out of space\n"
+                        "HINT: to retain full backups indefinitely (without warning), set option '%s' to the maximum.",
+                        cfgOptionName(cfgOptRepoRetentionFull + optionIdx),
+                        cfgOptionName(cfgOptRepoRetentionFull + optionIdx));
+                }
+            }
+        }
+
+        // If archive retention is valid for the command, then set archive settings
+        if (cfgOptionValid(cfgOptRepoRetentionArchive))
+        {
+            // For each possible repo, check and adjust the settings as appropriate
+            for (unsigned int optionIdx = 0; optionIdx < cfgOptionIndexTotal(cfgOptRepoType); optionIdx++)
+            {
+                const String *archiveRetentionType = cfgOptionStr(cfgOptRepoRetentionArchiveType + optionIdx);
+
+                const String *msgArchiveOff = strNewFmt("WAL segments will not be expired: option '%s=%s' but",
+                    cfgOptionName(cfgOptRepoRetentionArchiveType), strPtr(archiveRetentionType));
+
+                // If the archive retention is not explicitly set then determine what it should be defaulted to
+                // to.
+                if (!cfgOptionTest(cfgOptRepoRetentionArchive + optionIdx))
+                {
+                    // If repo-retention-archive-type is default, then if repo-retention-full is set, set the repo-retention-archive
+                    // to this value, else ignore archiving
+                    if (strEqZ(archiveRetentionType, CFGOPTVAL_TMP_REPO_RETENTION_ARCHIVE_TYPE_FULL))
+                    {
+                        if (cfgOptionTest(cfgOptRepoRetentionFull + optionIdx))
+                        {
+                            cfgOptionSet(cfgOptRepoRetentionArchive + optionIdx, cfgSourceDefault,
+                                varNewInt(cfgOptionInt(cfgOptRepoRetentionFull + optionIdx)));
+                        }
+                    }
+                    else if (strEqZ(archiveRetentionType, CFGOPTVAL_TMP_REPO_RETENTION_ARCHIVE_TYPE_DIFF))
+                    {
+                        // if repo-retention-diff is set then user must have set it
+                        if (cfgOptionTest(cfgOptRepoRetentionDiff + optionIdx))
+                        {
+                            cfgOptionSet(cfgOptRepoRetentionArchive + optionIdx, cfgSourceDefault,
+                                varNewInt(cfgOptionInt(cfgOptRepoRetentionDiff + optionIdx)));
+                        }
+                        else
+                        {
+                            LOG_WARN("%s neither option '%s' nor option '%s' is set", strPtr(msgArchiveOff),
+                                cfgOptionName(cfgOptRepoRetentionArchive + optionIdx),
+                                cfgOptionName(cfgOptRepoRetentionDiff + optionIdx));
+                        }
+                    }
+                    else if (strEqZ(archiveRetentionType, CFGOPTVAL_TMP_REPO_RETENTION_ARCHIVE_TYPE_INCR))
+                    {
+                        LOG_WARN("%s option '%s' is not set", strPtr(msgArchiveOff),
+                            cfgOptionName(cfgOptRepoRetentionArchive + optionIdx));
+                    }
+                }
+                else
+                {
+                    // If repo-retention-archive is set then check repo-retention-archive-type and issue a warning if the
+                    // corresponding setting is UNDEF since UNDEF means backups will not be expired but they should be in the
+                    // practice of setting this value even though expiring the archive itself is OK and will be performed.
+                    if ((strEqZ(archiveRetentionType, CFGOPTVAL_TMP_REPO_RETENTION_ARCHIVE_TYPE_DIFF)) &&
+                        (!cfgOptionTest(cfgOptRepoRetentionDiff + optionIdx)))
+                    {
+                        LOG_WARN("option '%s' is not set for '%s=%s'\n"
+                            "HINT: to retain differential backups indefinitely (without warning), set option '%s' to the maximum.",
+                            cfgOptionName(cfgOptRepoRetentionDiff + optionIdx),
+                            cfgOptionName(cfgOptRepoRetentionArchiveType + optionIdx),
+                            CFGOPTVAL_TMP_REPO_RETENTION_ARCHIVE_TYPE_DIFF,
+                            cfgOptionName(cfgOptRepoRetentionDiff + optionIdx));
+                    }
                 }
             }
         }
--- test/src/module/config/loadTest.c
+++ test/src/module/config/loadTest.c
@@ -70,6 +70,7 @@ testRun()
         argList = strLstNew();
         strLstAdd(argList, strNew("pgbackrest"));
         strLstAdd(argList, strNew("--pg1-path=/path/to/db"));
+        strLstAdd(argList, strNew("--repo1-retention-full=1"));
         strLstAdd(argList, strNew("--stanza=db"));
         strLstAdd(argList, strNew("backup"));
 
@@ -83,6 +84,7 @@ testRun()
         strLstAdd(argList, strNew("--pg1-path=/path/to/db"));
         strLstAdd(argList, strNew("--pg3-host=db"));
         strLstAdd(argList, strNew("--pg3-path=/path/to/db"));
+        strLstAdd(argList, strNew("--repo1-retention-full=1"));
         strLstAdd(argList, strNew("--stanza=db"));
         strLstAdd(argList, strNew("backup"));
 
@@ -93,6 +95,176 @@ testRun()
         TEST_RESULT_STR(
             strPtr(varStr(cfgOptionDefault(cfgOptPgHostCmd + 2))), strPtr(cfgExe()),
             "    command backup, option pg3-host-cmd default");
+
+        // Set a distinct umask value and test that the umask is reset by configLoad since default for neutral-umask=y
+        // -------------------------------------------------------------------------------------------------------------------------
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("archive-get"));
+
+        umask(0111);
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for neutral-umask");
+        TEST_RESULT_INT(umask(0111), 0000, "    umask was reset");
+
+        // Set a distinct umask value and test that the umask is not reset by configLoad with option --no-neutral-umask
+        // -------------------------------------------------------------------------------------------------------------------------
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--no-neutral-umask"));
+        strLstAdd(argList, strNew("archive-get"));
+
+        umask(0111);
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for no-neutral-umask");
+        TEST_RESULT_INT(umask(0), 0111, "    umask was not reset");
+
+        // db-timeout / protocol-timeout tests
+        // -------------------------------------------------------------------------------------------------------------------------
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--db-timeout=1830"));
+        strLstAdd(argList, strNew("archive-get"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for protocol-timeout reset");
+        TEST_RESULT_DOUBLE(cfgOptionDbl(cfgOptDbTimeout), 1830, "    db-timeout set");
+        TEST_RESULT_DOUBLE(cfgOptionDbl(cfgOptProtocolTimeout), 1860, "    protocol-timeout set greater than db-timeout");
+
+        // Set the protocol-timeout so the source is the command line and not the default
+        strLstAdd(argList, strNew("--protocol-timeout=1820"));
+        TEST_ERROR(
+            cfgLoad(strLstSize(argList), strLstPtr(argList)), OptionInvalidValueError,
+            strPtr(strNewFmt("'%f' is not valid for '%s' option\n"
+            "HINT '%s' option (%f) should be greater than '%s' option (%f).",
+            cfgOptionDbl(cfgOptProtocolTimeout), cfgOptionName(cfgOptProtocolTimeout),
+            cfgOptionName(cfgOptProtocolTimeout), cfgOptionDbl(cfgOptProtocolTimeout), cfgOptionName(cfgOptDbTimeout),
+            cfgOptionDbl(cfgOptDbTimeout))));
+
+        // pg-host and repo-host tests
+        // -------------------------------------------------------------------------------------------------------------------------
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--pg2-host=pg2"));
+        strLstAdd(argList, strNew("archive-get"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for pg and repo host test");
+
+        strLstAdd(argList, strNew("--repo1-host=repo1"));
+
+        TEST_ERROR(
+            cfgLoad(strLstSize(argList), strLstPtr(argList)), ConfigError,
+            "pg and repo hosts cannot both be configured as remote");
+
+        // retention
+        // -------------------------------------------------------------------------------------------------------------------------
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--log-level-console=info"));
+        strLstAdd(argList, strNew("--log-level-stderr=off"));
+        strLstAdd(argList, strNew("--no-log-timestamp"));
+        strLstAdd(argList, strNew("expire"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --stanza=db\n"
+            "P00   WARN: option repo1-retention-full is not set, the repository may run out of space\n"
+            "            HINT: to retain full backups indefinitely (without warning), set option"
+                " 'repo1-retention-full' to the maximum.");
+        TEST_RESULT_BOOL(cfgOptionTest(cfgOptRepoRetentionArchive), false, "    repo1-retention-archive not set");
+
+        strLstAdd(argList, strNew("--repo1-retention-full=1"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config no retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --repo1-retention-full=1 --stanza=db");
+        TEST_RESULT_INT(cfgOptionInt(cfgOptRepoRetentionArchive), 1, "    repo1-retention-archive set");
+
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--log-level-console=info"));
+        strLstAdd(argList, strNew("--log-level-stderr=off"));
+        strLstAdd(argList, strNew("--no-log-timestamp"));
+        strLstAdd(argList, strNew("--repo1-retention-archive-type=incr"));
+        strLstAdd(argList, strNew("expire"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --repo1-retention-archive-type=incr --stanza=db\n"
+            "P00   WARN: option repo1-retention-full is not set, the repository may run out of space\n"
+                "            HINT: to retain full backups indefinitely (without warning), set option 'repo1-retention-full'"
+                " to the maximum.\n"
+            "P00   WARN: WAL segments will not be expired: option 'repo1-retention-archive-type=incr' but option"
+                " 'repo1-retention-archive' is not set");
+        TEST_RESULT_BOOL(cfgOptionTest(cfgOptRepoRetentionArchive), false, "    repo1-retention-archive not set");
+
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--log-level-console=info"));
+        strLstAdd(argList, strNew("--log-level-stderr=off"));
+        strLstAdd(argList, strNew("--no-log-timestamp"));
+        strLstAdd(argList, strNew("--repo1-retention-archive-type=diff"));
+        strLstAdd(argList, strNew("expire"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --repo1-retention-archive-type=diff --stanza=db\n"
+            "P00   WARN: option repo1-retention-full is not set, the repository may run out of space\n"
+            "            HINT: to retain full backups indefinitely (without warning), set option"
+                " 'repo1-retention-full' to the maximum.\n"
+            "P00   WARN: WAL segments will not be expired: option 'repo1-retention-archive-type=diff' but neither option"
+                " 'repo1-retention-archive' nor option 'repo1-retention-diff' is set");
+        TEST_RESULT_BOOL(cfgOptionTest(cfgOptRepoRetentionArchive), false, "    repo1-retention-archive not set");
+
+        strLstAdd(argList, strNew("--repo1-retention-diff=2"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --repo1-retention-archive-type=diff --repo1-retention-diff=2 --stanza=db\n"
+            "P00   WARN: option repo1-retention-full is not set, the repository may run out of space\n"
+            "            HINT: to retain full backups indefinitely (without warning), set option"
+                " 'repo1-retention-full' to the maximum.");
+        TEST_RESULT_INT(cfgOptionInt(cfgOptRepoRetentionArchive), 2, "    repo1-retention-archive set to retention-diff");
+
+        argList = strLstNew();
+        strLstAdd(argList, strNew("pgbackrest"));
+        strLstAdd(argList, strNew("--stanza=db"));
+        strLstAdd(argList, strNew("--log-level-console=info"));
+        strLstAdd(argList, strNew("--log-level-stderr=off"));
+        strLstAdd(argList, strNew("--no-log-timestamp"));
+        strLstAdd(argList, strNew("--repo1-retention-archive-type=diff"));
+        strLstAdd(argList, strNew("--repo1-retention-archive=3"));
+        strLstAdd(argList, strNew("--repo1-retention-full=1"));
+        strLstAdd(argList, strNew("expire"));
+
+        TEST_RESULT_VOID(cfgLoad(strLstSize(argList), strLstPtr(argList)), "load config for retention warning");
+        testLogResult(
+            "P00   WARN: unable to open log file '/var/log/pgbackrest/db-expire.log': No such file or directory\n"
+            "            NOTE: process will continue without log file.\n"
+            "P00   INFO: expire command begin 2.01dev: --log-level-console=info --log-level-stderr=off --no-log-timestamp"
+                " --repo1-retention-archive=3 --repo1-retention-archive-type=diff --repo1-retention-full=1 --stanza=db\n"
+            "P00   WARN: option 'repo1-retention-diff' is not set for 'repo1-retention-archive-type=diff'\n"
+            "            HINT: to retain differential backups indefinitely (without warning), set option 'repo1-retention-diff'"
+                " to the maximum.");
 
         testLogErrResult(
             "WARN: unable to open log file '/var/log/pgbackrest/db-backup.log': No such file or directory\n"
