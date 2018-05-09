a9069f3fd3236d020c589265117817d3fddc6a5d
David Steele <david@pgmasters.net>
2018-05-09 10:50:05 -0400

    Begin v2.03 development.

--- src/version.h
+++ src/version.h
@@ -17,6 +17,6 @@ Standard binary name
 /***********************************************************************************************************************************
 Version of the software.  Currently this value is maintained in Version.pm and updated by test.pl.
 ***********************************************************************************************************************************/
-#define PGBACKREST_VERSION                                          "2.02"
+#define PGBACKREST_VERSION                                          "2.03dev"
 
 #endif
