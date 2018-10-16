06d68eada00e6ec57c662aa9ad2bacdc86eb5861
David Steele <david@pgmasters.net>
2018-10-16 17:21:01 +0100

    Begin v2.07 development.

--- src/version.h
+++ src/version.h
@@ -23,6 +23,6 @@ repository will be invalid unless migration functions are written.
 /***********************************************************************************************************************************
 Software version.  Currently this value is maintained in Version.pm and updated by test.pl.
 ***********************************************************************************************************************************/
-#define PGBACKREST_VERSION                                          "2.06"
+#define PGBACKREST_VERSION                                          "2.07dev"
 
 #endif
