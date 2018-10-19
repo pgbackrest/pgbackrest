e28c3b4591d945d99d3d8cde9a44d4a128c4e312
Cynthia Shang <cynthia.shang@crunchydata.com>
2018-10-19 10:35:59 -0400

    Merge squashed br-json-enhancement-ci in order to use the jsonToKv parser for backup:current section of backup.info.

--- src/common/type/json.c
+++ src/common/type/json.c
@@ -8,13 +8,61 @@ Convert JSON to/from KeyValue
 #include "common/type/json.h"
 
 /***********************************************************************************************************************************
+Internal helper functions
+***********************************************************************************************************************************/
+static Variant *
+jsonString(const char *jsonC, size_t strSize, unsigned int *jsonPos, unsigned int *valueBeginPos)
+{
+    FUNCTION_DEBUG_BEGIN(logLevelTrace);
+        FUNCTION_DEBUG_PARAM(CHARPY, jsonC);
+        FUNCTION_DEBUG_PARAM(SIZE, strSize);
+        FUNCTION_DEBUG_PARAM(UINTP, jsonPos);
+        FUNCTION_DEBUG_PARAM(UINTP, valueBeginPos);
+    FUNCTION_DEBUG_END();
+
+    *valueBeginPos = *valueBeginPos + 1;
+    *jsonPos = *jsonPos + 1;
+
+    while (jsonC[*jsonPos] != '"' && *jsonPos < strSize - 1)
+        *jsonPos = *jsonPos + 1;
+
+    if (jsonC[*jsonPos] != '"')
+        THROW_FMT(JsonFormatError, "expected '\"' but found '%c'", jsonC[*jsonPos]);
+
+    Variant *result = varNewStr(strNewN(jsonC + *valueBeginPos, *jsonPos - *valueBeginPos));
+
+    *jsonPos = *jsonPos + 1;
+
+    FUNCTION_TEST_RESULT(VARIANT, result);
+}
+
+static Variant *
+jsonNumeric(const char *jsonC, size_t strSize, unsigned int *jsonPos, unsigned int *valueBeginPos)
+{
+        FUNCTION_DEBUG_BEGIN(logLevelTrace);
+            FUNCTION_DEBUG_PARAM(CHARPY, jsonC);
+            FUNCTION_DEBUG_PARAM(SIZE, strSize);
+            FUNCTION_DEBUG_PARAM(UINTP, jsonPos);
+            FUNCTION_DEBUG_PARAM(UINTP, valueBeginPos);
+        FUNCTION_DEBUG_END();
+
+    while (isdigit(jsonC[*jsonPos]) && *jsonPos < strSize - 1)
+        *jsonPos = *jsonPos + 1;
+
+    Variant *result = varNewUInt64(cvtZToUInt64(strPtr(strNewN(jsonC + *valueBeginPos, *jsonPos - *valueBeginPos))));
+
+    FUNCTION_TEST_RESULT(VARIANT, result);
+}
+
+/***********************************************************************************************************************************
 Convert JSON to KeyValue object
 
 Currently this function is only intended to convert the limited types that are included in info files.  More types will be added as
 needed.  Since this function is only intended to read internally-generated JSON it is assumed to be well-formed with no extraneous
 whitespace.
 ***********************************************************************************************************************************/
-KeyValue *jsonToKv(const String *json)
+KeyValue *
+jsonToKv(const String *json)
 {
     FUNCTION_DEBUG_BEGIN(logLevelTrace);
         FUNCTION_DEBUG_PARAM(STRING, json);
@@ -68,32 +116,93 @@ KeyValue *jsonToKv(const String *json)
             // The value appears to be a string
             if (jsonC[jsonPos] == '"')
             {
-                valueBeginPos++;
-                jsonPos++;
-
-                while (jsonC[jsonPos] != '"' && jsonPos < strSize(json) - 1)
-                    jsonPos++;
-
-                if (jsonC[jsonPos] != '"')
-                    THROW_FMT(JsonFormatError, "expected '\"' but found '%c'", jsonC[jsonPos]);
-
-                value = varNewStr(strNewN(jsonC + valueBeginPos, jsonPos - valueBeginPos));
-
-                jsonPos++;
+                value = jsonString(jsonC, strSize(json), &jsonPos, &valueBeginPos);
             }
 
             // The value appears to be a number
             else if (isdigit(jsonC[jsonPos]))
             {
-                while (isdigit(jsonC[jsonPos]) && jsonPos < strSize(json) - 1)
+                value = jsonNumeric(jsonC, strSize(json), &jsonPos, &valueBeginPos);
+            }
+
+            // The value appears to be a boolean
+            else if (jsonC[jsonPos] == 't' || jsonC[jsonPos] == 'f')
+            {
+                valueBeginPos = jsonPos;
+
+                while (jsonC[jsonPos] != 'e' && jsonPos < strSize(json) - 1)
                     jsonPos++;
+
+                if (jsonC[jsonPos] != 'e')
+                    THROW_FMT(JsonFormatError, "expected boolean but found '%c'", jsonC[jsonPos]);
+
+                jsonPos++;
 
                 String *valueStr = strNewN(jsonC + valueBeginPos, jsonPos - valueBeginPos);
 
-                value = varNewUInt64(cvtZToUInt64(strPtr(valueStr)));
+                if (strCmpZ(valueStr, "true") != 0 && strCmpZ(valueStr, "false") != 0)
+                    THROW_FMT(JsonFormatError, "expected 'true' or 'false' but found '%s'", strPtr(valueStr));
+
+                value = varNewBool(varBoolForce(varNewStr(valueStr)));
             }
 
-            // Else not sure what it is.  Currently booleans and nulls will error.
+            // The value appears to be an array.
+            else if (jsonC[jsonPos] == '[')
+            {
+                // Add a pointer to an empty variant list as the value for the key.
+                Variant *valueList = varNewVarLst(varLstNew());
+                kvAdd(result, varNewStr(key), valueList);
+
+                unsigned char arrayType = '\0';
+                // ??? Currently only working with same-type simple single-dimensional arrays
+                do
+                {
+                    jsonPos++;
+                    valueBeginPos = jsonPos;
+
+                    // The value appears to be a string
+                    if (jsonC[jsonPos] == '"')
+                    {
+                        if (arrayType != '\0' && arrayType != 's')
+                            THROW_FMT(JsonFormatError, "string found in array of type '%c'", arrayType);
+
+                        arrayType = 's';
+
+                        value = jsonString(jsonC, strSize(json), &jsonPos, &valueBeginPos);
+                    }
+
+                    // The value appears to be a number
+                    else if (isdigit(jsonC[jsonPos]))
+                    {
+                        if (arrayType != '\0' && arrayType != 'n')
+                            THROW_FMT(JsonFormatError, "number found in array of type '%c'", arrayType);
+
+                        arrayType = 'n';
+
+                        value = jsonNumeric(jsonC, strSize(json), &jsonPos, &valueBeginPos);
+                    }
+
+                    else
+                        THROW(JsonFormatError, "unknown array value type");
+
+                    kvAdd(result, varNewStr(key), value);
+                }
+                while (jsonC[jsonPos] == ',');
+
+                if (jsonC[jsonPos] != ']')
+                    THROW_FMT(JsonFormatError, "expected array delimeter ']' but found '%c'", jsonC[jsonPos]);
+
+                jsonPos++;
+
+                continue;
+            }
+            // Else not sure what it is.  Currently nulls will error.
+// CSHANG: NULLS - NULLS and empty are different in JSON. If we got here, we could just check for jsonC[jsonPos] == 'n' and then make sure it
+// is null: String *valueStr = strNewN(jsonC[jsonPos], 5); if strCmpZ(valueStr, "null,") != 0 || strCmpZ(valueStr, "null}") != 0 then
+// error else advance the jsonPos to the , or } and set value = NULL;  DO WE HAVE ANY OF THESE IN OUR INFO/CONF/MAINFEST FILES? AS
+// far as I can see we do not.
+// CSHANG And how to we handle empty objects like pg_data={}?
+
             else
                 THROW(JsonFormatError, "unknown value type");
 
@@ -103,7 +212,7 @@ KeyValue *jsonToKv(const String *json)
 
         // Look for end delimiter
         if (jsonC[jsonPos] != '}')
-                THROW_FMT(JsonFormatError, "expected '}' but found '%c'", jsonC[jsonPos]);
+            THROW_FMT(JsonFormatError, "expected '}' but found '%c'", jsonC[jsonPos]);
     }
     MEM_CONTEXT_TEMP_END();
 
--- test/src/module/common/typeJsonTest.c
+++ test/src/module/common/typeJsonTest.c
@@ -19,10 +19,21 @@ testRun(void)
         TEST_ERROR(jsonToKv(strNew("{\"\"")), JsonFormatError, "zero-length key not allowed");
         TEST_ERROR(jsonToKv(strNew("{\"key1\"")), JsonFormatError, "expected ':' but found '");
         TEST_ERROR(jsonToKv(strNew("{\"key1'")), JsonFormatError, "expected '\"' but found '''");
-        TEST_ERROR(jsonToKv(strNew("{\"key1\":t")), JsonFormatError, "unknown value type");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":x")), JsonFormatError, "unknown value type");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":t")), JsonFormatError, "expected boolean but found 't'");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":toe")), JsonFormatError, "expected 'true' or 'false' but found 'toe'");
+        TEST_ERROR(jsonToKv(strNew("{\"checksum\":\"6721d92c9fcdf4248acff1f9a13\",\"master\":falsehood,\"reference\":\"backup\"}")),
+            JsonFormatError, "expected '}' but found 'h'");
         TEST_ERROR(jsonToKv(strNew("{\"key1\":123")), JsonFormatError, "expected '}' but found '3'");
         TEST_ERROR(jsonToKv(strNew("{\"key1\":123>")), JsonFormatError, "expected '}' but found '>'");
         TEST_ERROR(jsonToKv(strNew("{\"key1\":\"123'}")), JsonFormatError, "expected '\"' but found '}'");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":[\"\",]}")), JsonFormatError, "unknown array value type");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":[\"\",1]}")), JsonFormatError, "number found in array of type 's'");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":[1,\"\"]}")), JsonFormatError, "string found in array of type 'n'");
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":[1}")), JsonFormatError, "expected array delimeter ']' but found '}'");
+        // CSHANG Should we handle this as an empty array? Should we set it to NULL? Right now in the backup.info file, it appears
+        // that only the backup-reference is an array - and if that is empty, something is REALLY wrong since it's only set on non-full
+        TEST_ERROR(jsonToKv(strNew("{\"key1\":[]}")), JsonFormatError, "unknown array value type");
 
         // -------------------------------------------------------------------------------------------------------------------------
         KeyValue *kv = NULL;
@@ -32,6 +43,12 @@ testRun(void)
 
         TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":\"value1\"}")), "single string value");
         TEST_RESULT_STR(strPtr(varStr(kvGet(kv, varNewStr(strNew("key1"))))), "value1", "  check string");
+
+        TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":true}")), "boolean true");
+        TEST_RESULT_BOOL(varBool(kvGet(kv, varNewStr(strNew("key1")))), true, "  check boolean");
+
+        TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":false}")), "boolean true");
+        TEST_RESULT_BOOL(varBool(kvGet(kv, varNewStr(strNew("key1")))), false, "  check boolean");
 
         TEST_ASSIGN(
             kv,
@@ -44,6 +61,39 @@ testRun(void)
         TEST_RESULT_UINT(varUInt64(kvGet(kv, varNewStr(strNew("db-control-version")))), 942, "  check integer");
         TEST_RESULT_UINT(varUInt64(kvGet(kv, varNewStr(strNew("db-system-id")))), 6116111691796124355, "  check integer");
         TEST_RESULT_STR(strPtr(varStr(kvGet(kv, varNewStr(strNew("db-version"))))), "9.4", "  check string");
+
+        // CSHANG If empty string, should it be converted to NULL as the output? Probably not - should only do that if null is an
+        // element of the array - do we even have that in the info or manifest files? I know we have {} so is that a NULL object or
+        // empty object?
+        TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":[\"\"]}")), "single-dimension empty string array");
+        TEST_RESULT_STR(strPtr(varStr(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("key1")))), 0))), "", "  array[0]");
+
+        TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":[\"12AB\",\"34-EF\"]}")), "single-dimension string array");
+        TEST_RESULT_STR(strPtr(varStr(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("key1")))), 0))), "12AB", "  array[0]");
+        TEST_RESULT_STR(strPtr(varStr(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("key1")))), 1))), "34-EF", "  array[1]");
+
+        TEST_ASSIGN(kv, jsonToKv(strNew("{\"key1\":[12,34]}")), "single-dimension number array");
+        TEST_RESULT_UINT(varUInt64(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("key1")))), 0)), 12, "  array[0]");
+        TEST_RESULT_UINT(varUInt64(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("key1")))), 1)), 34, "  array[1]");
+
+        TEST_ASSIGN(
+            kv,
+            jsonToKv(
+                strNew(
+                "{\"backup-info-size-delta\":1982702,\"backup-prior\":\"20161219-212741F_20161219-212803I\","
+                "\"backup-reference\":[\"20161219-212741F\",\"20161219-212741F_20161219-212803I\"],"
+                "\"checksum-page-error\":[1],\"backup-timestamp-start\":1482182951}")),
+            "multpile values with array");
+        TEST_RESULT_UINT(varUInt64(kvGet(kv, varNewStr(strNew("backup-info-size-delta")))), 1982702, "  check integer");
+        TEST_RESULT_STR(strPtr(varStr(kvGet(kv, varNewStr(strNew("backup-prior"))))), "20161219-212741F_20161219-212803I",
+            "  check string");
+        TEST_RESULT_STR(strPtr(varStr(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("backup-reference")))), 0))),
+            "20161219-212741F", "  check string array[0]");
+        TEST_RESULT_STR(strPtr(varStr(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("backup-reference")))), 1))),
+            "20161219-212741F_20161219-212803I", "  check string array[1]");
+        TEST_RESULT_UINT(varUInt64(varLstGet(varVarLst(kvGet(kv, varNewStr(strNew("checksum-page-error")))), 0)), 1,
+            "  check integer array[0]");
+        TEST_RESULT_UINT(varUInt64(kvGet(kv, varNewStr(strNew("backup-timestamp-start")))), 1482182951, "  check integer");
     }
 
     FUNCTION_HARNESS_RESULT_VOID();
