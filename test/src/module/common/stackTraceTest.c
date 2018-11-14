14d2f8b86c4fd3755af6cd9f5210cf1e34e8d090
David Steele <david@pgmasters.net>
2018-11-14 08:11:11 -0500

    Reword misleading message in stack trace when parameter buffer is full.
    The prior message stated that there had been a buffer overrun which is not true since the code prevents that.
    
    In fact, this message means the parameter buffer filled while building the parameter list. Rather than display a partial list we output this message instead.
    
    Also remove !!! which by convention we use as a marker for code that needs attention before it can be committed to master.

--- test/src/module/common/stackTraceTest.c
+++ test/src/module/common/stackTraceTest.c
@@ -148,14 +148,14 @@ testRun(void)
                 stackTrace[stackSize - 1].param += 1;
                 stackTrace[stackSize - 1].paramSize = 0;
                 stackTraceParamAdd((size_t)snprintf(stackTraceParamBuffer("param1"), STACK_TRACE_PARAM_MAX, "value1"));
-                assert(strcmp(stackTraceParam(), "!!! buffer overflow - parameters not available !!!") == 0);
+                assert(strcmp(stackTraceParam(), "buffer full - parameters not available") == 0);
 
                 stackTraceToZ(buffer, sizeof(buffer), "file4.c", "function4", 99);
 
 #ifdef WITH_BACKTRACE
                 TEST_RESULT_STR(
                     buffer,
-                    "file4:function4:99:(!!! buffer overflow - parameters not available !!!)\n"
+                    "file4:function4:99:(buffer full - parameters not available)\n"
                     "file3:function3:7777:(param1: value1, param2: value2)\n"
                     "file2:function2:7777:(param1: value1)\n"
                     "file1:function2:7777:(debug log level required for parameters)\n"
@@ -164,7 +164,7 @@ testRun(void)
 #else
                 TEST_RESULT_STR(
                     buffer,
-                    "file4:function4:99:(!!! buffer overflow - parameters not available !!!)\n"
+                    "file4:function4:99:(buffer full - parameters not available)\n"
                     "file3:function3:(param1: value1, param2: value2)\n"
                     "file2:function2:(param1: value1)\n"
                     "file1:function2:(debug log level required for parameters)\n"
