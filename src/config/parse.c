pgbackrest/pgbackrest

commit 5e3b7cbe6ee7a409ad6e311668eb764fee82939c

Apply String constant macros where appropriate.
This may not be all the places where the macros can be useful but it's a good start.

src/config/parse.c
@@ -22,6 +22,7 @@ Standard config file name and old default path and name
 ***********************************************************************************************************************************/
 #define PGBACKREST_CONFIG_FILE                                      PGBACKREST_BIN ".conf"
 #define PGBACKREST_CONFIG_ORIG_PATH_FILE                            "/etc/" PGBACKREST_CONFIG_FILE
+    STRING_STATIC(PGBACKREST_CONFIG_ORIG_PATH_FILE_STR,             PGBACKREST_CONFIG_FILE)
 
 /***********************************************************************************************************************************
 Prefix for environment variables
@@ -108,7 +109,7 @@ convertToByte(String **value, double *valueDbl)
     String *result = strLower(strDup(*value));
 
     // Match the value against possible values
-    if (regExpMatchOne(strNew("^[0-9]+(kb|k|mb|m|gb|g|tb|t|pb|p|b)*$"), result))
+    if (regExpMatchOne(STRING_CONST("^[0-9]+(kb|k|mb|m|gb|g|tb|t|pb|p|b)*$"), result))
     {
         // Get the character array and size
         const char *strArray = strPtr(result);
@@ -315,7 +316,7 @@ cfgFileLoad(                                                        // NOTE: Pas
 
         // Get a list of conf files from the specified path -error on missing directory if the option was passed on the command line
         StringList *list = storageListP(
-            storageLocal(), configIncludePath, .expression = strNew(".+\\.conf$"), .errorOnMissing = configIncludeRequired);
+            storageLocal(), configIncludePath, .expression = STRING_CONST(".+\\.conf$"), .errorOnMissing = configIncludeRequired);
 
         // If conf files are found, then add them to the config string
         if (list != NULL && strLstSize(list) > 0)
@@ -641,7 +642,7 @@ configParse(unsigned int argListSize, const char *argList[], bool resetLogLevel)
             String *configString = cfgFileLoad(parseOptionList,
                 strNew(cfgDefOptionDefault(commandDefId, cfgOptionDefIdFromId(cfgOptConfig))),
                 strNew(cfgDefOptionDefault(commandDefId, cfgOptionDefIdFromId(cfgOptConfigIncludePath))),
-                strNew(PGBACKREST_CONFIG_ORIG_PATH_FILE));
+                PGBACKREST_CONFIG_ORIG_PATH_FILE_STR);
 
             if (configString != NULL)
             {
@@ -663,7 +664,7 @@ configParse(unsigned int argListSize, const char *argList[], bool resetLogLevel)
                 }
 
                 strLstAdd(sectionList, strNewFmt(CFGDEF_SECTION_GLOBAL ":%s", cfgCommandName(cfgCommand())));
-                strLstAdd(sectionList, strNew(CFGDEF_SECTION_GLOBAL));
+                strLstAdd(sectionList, CFGDEF_SECTION_GLOBAL_STR);
 
                 // Loop through sections to search for options
                 for (unsigned int sectionIdx = 0; sectionIdx < strLstSize(sectionList); sectionIdx++)
@@ -980,7 +981,8 @@ configParse(unsigned int argListSize, const char *argList[], bool resetLogLevel)
                             String *value = strLstGet(parseOption->valueList, 0);
 
                             // If a numeric type check that the value is valid
-                            if (optionDefType == cfgDefOptTypeInteger || optionDefType == cfgDefOptTypeFloat || optionDefType == cfgDefOptTypeSize)
+                            if (optionDefType == cfgDefOptTypeInteger || optionDefType == cfgDefOptTypeFloat ||
+                                optionDefType == cfgDefOptTypeSize)
                             {
                                 double valueDbl = 0;
 

doc/xml/release.xml
@@ -75,7 +75,7 @@
                     </release-item>
 
                     <release-item>
-                        <p>Add <code>strCatChr()</code>and constant macros to <code>String</code> object.</p>
+                        <p>Add <code>strCatChr()</code> and constant macros to <code>String</code> object.</p>
                     </release-item>
 
                     <release-item>

src/Makefile
@@ -283,7 +283,7 @@ compress/gzipDecompress.o: compress/gzipDecompress.c common/debug.h common/error
 config/config.o: config/config.c common/assert.h common/debug.h common/error.auto.h common/error.h common/lock.h common/log.h common/logLevel.h common/memContext.h common/stackTrace.h common/time.h common/type/buffer.h common/type/convert.h common/type/keyValue.h common/type/string.h common/type/stringList.h common/type/variant.h common/type/variantList.h config/config.auto.c config/config.auto.h config/config.h config/define.auto.h config/define.h
 	$(CC) $(CFLAGS) -c config/config.c -o config/config.o
 
-config/define.o: config/define.c common/assert.h common/debug.h common/error.auto.h common/error.h common/logLevel.h common/stackTrace.h common/type/convert.h config/define.auto.c config/define.auto.h config/define.h
+config/define.o: config/define.c common/assert.h common/debug.h common/error.auto.h common/error.h common/logLevel.h common/memContext.h common/stackTrace.h common/type/buffer.h common/type/convert.h common/type/string.h config/define.auto.c config/define.auto.h config/define.h
 	$(CC) $(CFLAGS) -c config/define.c -o config/define.o
 
 config/load.o: config/load.c command/command.h common/debug.h common/error.auto.h common/error.h common/io/io.h common/lock.h common/log.h common/logLevel.h common/memContext.h common/stackTrace.h common/time.h common/type/buffer.h common/type/convert.h common/type/keyValue.h common/type/string.h common/type/stringList.h common/type/variant.h common/type/variantList.h config/config.auto.h config/config.h config/define.auto.h config/define.h config/load.h config/parse.h

src/command/archive/common.c
@@ -17,6 +17,12 @@ Archive Push Command
 #include "storage/helper.h"
 
 /***********************************************************************************************************************************
+WAL segment constants
+***********************************************************************************************************************************/
+STRING_EXTERN(WAL_SEGMENT_REGEXP_STR,                               WAL_SEGMENT_REGEXP);
+STRING_EXTERN(WAL_SEGMENT_PARTIAL_REGEXP_STR,                       WAL_SEGMENT_PARTIAL_REGEXP);
+
+/***********************************************************************************************************************************
 Check for ok/error status files in the spool in/out directory
 ***********************************************************************************************************************************/
 bool
@@ -32,7 +38,7 @@ archiveAsyncStatus(ArchiveMode archiveMode, const String *walSegment, bool confe
 
     MEM_CONTEXT_TEMP_BEGIN()
     {
-        String *spoolQueue = strNew(archiveMode == archiveModeGet ? STORAGE_SPOOL_ARCHIVE_IN : STORAGE_SPOOL_ARCHIVE_OUT);
+        const String *spoolQueue = (archiveMode == archiveModeGet ? STORAGE_SPOOL_ARCHIVE_IN_STR : STORAGE_SPOOL_ARCHIVE_OUT_STR);
         String *okFile = strNewFmt("%s.ok", strPtr(walSegment));
         String *errorFile = strNewFmt("%s.error", strPtr(walSegment));
 
@@ -148,7 +154,7 @@ walIsSegment(const String *walSegment)
     {
         MEM_CONTEXT_BEGIN(memContextTop())
         {
-            regExpSegment = regExpNew(strNew(WAL_SEGMENT_PARTIAL_REGEXP));
+            regExpSegment = regExpNew(WAL_SEGMENT_PARTIAL_REGEXP_STR);
         }
         MEM_CONTEXT_END();
     }

src/command/archive/common.h
@@ -31,9 +31,11 @@ WAL segment constants
 
 // Match on a WAL segment without checksum appended
 #define WAL_SEGMENT_REGEXP                                          WAL_SEGMENT_PREFIX_REGEXP "$"
+    STRING_DECLARE(WAL_SEGMENT_REGEXP_STR);
 
 // Match on a WAL segment with partial allowed
 #define WAL_SEGMENT_PARTIAL_REGEXP                                  WAL_SEGMENT_PREFIX_REGEXP "(\\.partial){0,1}$"
+    STRING_DECLARE(WAL_SEGMENT_PARTIAL_REGEXP_STR);
 
 // Defines the size of standard WAL segment name -- hopefully this won't change
 #define WAL_SEGMENT_NAME_SIZE                                       ((unsigned int)24)

src/command/archive/get/file.c
@@ -35,7 +35,7 @@ archiveGetCheck(const String *archiveFile)
         PgControl controlInfo = pgControlFromFile(cfgOptionStr(cfgOptPgPath));
 
         // Attempt to load the archive info file
-        InfoArchive *info = infoArchiveNew(storageRepo(), strNew(STORAGE_REPO_ARCHIVE "/" INFO_ARCHIVE_FILE), false);
+        InfoArchive *info = infoArchiveNew(storageRepo(), STRING_CONST(STORAGE_REPO_ARCHIVE "/" INFO_ARCHIVE_FILE), false);
 
         // Loop through the pg history in case the WAL we need is not in the most recent archive id
         String *archiveId = NULL;

src/command/archive/get/get.c
@@ -59,7 +59,7 @@ queueNeed(const String *walSegment, bool found, size_t queueSize, size_t walSegm
 
         // Get the list of files actually in the queue
         StringList *actualQueue = strLstSort(
-            storageListP(storageSpool(), strNew(STORAGE_SPOOL_ARCHIVE_IN), .errorOnMissing = true), sortOrderAsc);
+            storageListP(storageSpool(), STORAGE_SPOOL_ARCHIVE_IN_STR, .errorOnMissing = true), sortOrderAsc);
 
         // Only preserve files that match the ideal queue. '.error'/'.ok' files are deleted so the async process can try again.
         RegExp *regExpPreserve = regExpNew(strNewFmt("^(%s)$", strPtr(strLstJoin(idealQueue, "|"))));
@@ -184,7 +184,7 @@ cmdArchiveGet(void)
 
                     // Get a list of WAL segments left in the queue
                     StringList *queue = storageListP(
-                        storageSpool(), strNew(STORAGE_SPOOL_ARCHIVE_IN), .expression = strNew(WAL_SEGMENT_REGEXP));
+                        storageSpool(), STORAGE_SPOOL_ARCHIVE_IN_STR, .expression = WAL_SEGMENT_REGEXP_STR);
 
                     if (strLstSize(queue) > 0)
                     {
@@ -224,7 +224,7 @@ cmdArchiveGet(void)
                             PgControl pgControl = pgControlFromFile(cfgOptionStr(cfgOptPgPath));
 
                             // Create the queue
-                            storagePathCreateNP(storageSpoolWrite(), strNew(STORAGE_SPOOL_ARCHIVE_IN));
+                            storagePathCreateNP(storageSpoolWrite(), STORAGE_SPOOL_ARCHIVE_IN_STR);
 
                             // Clean the current queue using the list of WAL that we ideally want in the queue.  queueNeed()
                             // will return the list of WAL needed to fill the queue and this will be passed to the async process.

src/command/help/help.c
@@ -72,27 +72,27 @@ helpRenderText(const String *text, size_t indent, bool indentFirst, size_t lengt
 /***********************************************************************************************************************************
 Helper function for helpRender() to output values as strings
 ***********************************************************************************************************************************/
-static String *
+static const String *
 helpRenderValue(const Variant *value)
 {
     FUNCTION_DEBUG_BEGIN(logLevelTrace);
         FUNCTION_DEBUG_PARAM(VARIANT, value);
     FUNCTION_DEBUG_END();
 
-    String *result = NULL;
+    const String *result = NULL;
 
     if (value != NULL)
     {
         if (varType(value) == varTypeBool)
         {
             if (varBool(value))
-                result = strNew("y");
+                result = Y_STR;
             else
-                result = strNew("n");
+                result = N_STR;
         }
         else if (varType(value) == varTypeKeyValue)
         {
-            result = strNew("");
+            String *resultTemp = strNew("");
 
             const KeyValue *optionKv = varKv(value);
             const VariantList *keyList = kvKeyList(optionKv);
@@ -100,32 +100,36 @@ helpRenderValue(const Variant *value)
             for (unsigned int keyIdx = 0; keyIdx < varLstSize(keyList); keyIdx++)
             {
                 if (keyIdx != 0)
-                    strCat(result, ", ");
+                    strCat(resultTemp, ", ");
 
                 strCatFmt(
-                    result, "%s=%s", strPtr(varStr(varLstGet(keyList, keyIdx))),
+                    resultTemp, "%s=%s", strPtr(varStr(varLstGet(keyList, keyIdx))),
                     strPtr(varStrForce(kvGet(optionKv, varLstGet(keyList, keyIdx)))));
             }
+
+            result = resultTemp;
         }
         else if (varType(value) == varTypeVariantList)
         {
-            result = strNew("");
+            String *resultTemp = strNew("");
 
             const VariantList *list = varVarLst(value);
 
             for (unsigned int listIdx = 0; listIdx < varLstSize(list); listIdx++)
             {
                 if (listIdx != 0)
-                    strCat(result, ", ");
+                    strCat(resultTemp, ", ");
 
-                strCatFmt(result, "%s", strPtr(varStr(varLstGet(list, listIdx))));
+                strCatFmt(resultTemp, "%s", strPtr(varStr(varLstGet(list, listIdx))));
             }
+
+            result = resultTemp;
         }
         else
             result = varStrForce(value);
     }
 
-    FUNCTION_DEBUG_RESULT(STRING, result);
+    FUNCTION_DEBUG_RESULT(CONST_STRING, result);
 }
 
 /***********************************************************************************************************************************
@@ -260,8 +264,8 @@ helpRender(void)
                             strlen(cfgDefOptionHelpSummary(commandDefId, optionDefId)) - 1));
 
                         // Ouput current and default values if they exist
-                        String *defaultValue = helpRenderValue(cfgOptionDefault(optionId));
-                        String *value = NULL;
+                        const String *defaultValue = helpRenderValue(cfgOptionDefault(optionId));
+                        const String *value = NULL;
 
                         if (cfgOptionSource(optionId) != cfgSourceDefault)
                             value = helpRenderValue(cfgOption(optionId));
@@ -330,8 +334,8 @@ helpRender(void)
                     strPtr(helpRenderText(strNew(cfgDefOptionHelpDescription(commandDefId, optionDefId)), 0, true, CONSOLE_WIDTH)));
 
                 // Ouput current and default values if they exist
-                String *defaultValue = helpRenderValue(cfgOptionDefault(optionId));
-                String *value = NULL;
+                const String *defaultValue = helpRenderValue(cfgOptionDefault(optionId));
+                const String *value = NULL;
 
                 if (cfgOptionSource(optionId) != cfgSourceDefault)
                     value = helpRenderValue(cfgOption(optionId));

commit 332a68ea8d713f24d1db08e20d5b95bcf32cc74e

Fix incorrect config constant introduced in 5e3b7cbe.
This commit introduced PGBACKREST_CONFIG_ORIG_PATH_FILE_STR as a String constant for PGBACKREST_CONFIG_ORIG_PATH_FILE but failed to get the value correct.

Also, no test was added for PGBACKREST_CONFIG_ORIG_PATH_FILE_STR to prevent regressions as there is for  PGBACKREST_CONFIG_ORIG_PATH_FILE.

src/config/parse.c
@@ -22,7 +22,7 @@ Standard config file name and old default path and name
 ***********************************************************************************************************************************/
 #define PGBACKREST_CONFIG_FILE                                      PGBACKREST_BIN ".conf"
 #define PGBACKREST_CONFIG_ORIG_PATH_FILE                            "/etc/" PGBACKREST_CONFIG_FILE
-    STRING_STATIC(PGBACKREST_CONFIG_ORIG_PATH_FILE_STR,             PGBACKREST_CONFIG_FILE)
+    STRING_STATIC(PGBACKREST_CONFIG_ORIG_PATH_FILE_STR,             PGBACKREST_CONFIG_ORIG_PATH_FILE)
 
 /***********************************************************************************************************************************
 Prefix for environment variables

test/src/module/config/parseTest.c
@@ -47,9 +47,10 @@ testRun(void)
         String *configIncludePath = strNewFmt("%s/conf.d", testPath());
         mkdir(strPtr(configIncludePath), 0750);
 
-        // Check old config file constant
+        // Check old config file constants
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_RESULT_STR(PGBACKREST_CONFIG_ORIG_PATH_FILE, "/etc/pgbackrest.conf", "check old config path");
+        TEST_RESULT_STR(strPtr(PGBACKREST_CONFIG_ORIG_PATH_FILE_STR), "/etc/pgbackrest.conf", "check old config path str");
 
         // Confirm same behavior with multiple config include files
         //--------------------------------------------------------------------------------------------------------------------------