abc613b454f8f1aa9da5e842c5da822d26504163
David Steele <david@pgmasters.net>
2019-02-02 14:50:24 +0200

    Begin v2.10 development.

--- src/perl/embed.auto.c
+++ src/perl/embed.auto.c
@@ -21022,7 +21022,7 @@ static const EmbeddedModule embeddedModule[] =
             "\n"
             "push @EXPORT, qw(projectBin projectBinSet);\n"
             "\n\n\n\n\n\n"
-            "use constant PROJECT_VERSION => '2.09';\n"
+            "use constant PROJECT_VERSION => '2.10dev';\n"
             "push @EXPORT, qw(PROJECT_VERSION);\n"
             "\n\n\n\n\n\n"
             "use constant REPOSITORY_FORMAT => 5;\n"
