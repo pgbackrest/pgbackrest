9f0829cbf2fcd9be869cac21fbf974fd7ecce019
David Steele <david@pgmasters.net>
2019-04-19 11:29:08 -0400

    Add cfgOptionUInt() and cfgOptionUInt64().
    These are better than casting from cfgOptionInt() since bounds checking is performed.

--- src/config/config.h
+++ src/config/config.h
@@ -57,6 +57,8 @@ int64_t cfgOptionInt64(ConfigOption optionId);
 const KeyValue *cfgOptionKv(ConfigOption optionId);
 const VariantList *cfgOptionLst(ConfigOption optionId);
 const String *cfgOptionStr(ConfigOption optionId);
+unsigned int cfgOptionUInt(ConfigOption optionId);
+uint64_t cfgOptionUInt64(ConfigOption optionId);
 
 unsigned int cfgOptionIndex(ConfigOption optionId);
 unsigned int cfgOptionIndexTotal(ConfigOption optionDefId);
