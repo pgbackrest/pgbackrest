70d2e10caef5b1faac228fa171f638269561ca1c
David Steele <david@pgmasters.net>
2019-04-24 14:58:30 -0400

    In progress...

--- src/common/compress/lz4/compress.c
+++ src/common/compress/lz4/compress.c
@@ -27,6 +27,8 @@ struct Lz4Compress
     MemContext *memContext;                                         // Context to store data
     LZ4F_compressionContext_t context;                              // LZ4 compression context
     IoFilter *filter;                                               // Filter interface
+    Buffer *buffer;                                                 // For when the output buffer can't accept all compressed data
+    bool first;                                                     // Is this the first call to process?
 
     bool inputSame;                                                 // Is the same input required on the next process call?
     bool flush;                                                     // Is input complete and flushing in progress?
@@ -42,11 +44,10 @@ Compression constants
 New object
 ***********************************************************************************************************************************/
 Lz4Compress *
-lz4CompressNew(int level, bool raw)
+lz4CompressNew(int level)
 {
     FUNCTION_LOG_BEGIN(logLevelTrace);
         FUNCTION_LOG_PARAM(INT, level);
-        FUNCTION_LOG_PARAM(BOOL, raw);
     FUNCTION_LOG_END();
 
     ASSERT(level >= -1 && level <= 9);
@@ -58,6 +59,7 @@ lz4CompressNew(int level, bool raw)
         // Allocate state and set context
         this = memNew(sizeof(Lz4Compress));
         this->memContext = MEM_CONTEXT_NEW();
+        this->first = true;
 
         // Create lz4 context
         lz4Error(LZ4F_createCompressionContext(&this->context, LZ4F_VERSION));
@@ -77,6 +79,42 @@ lz4CompressNew(int level, bool raw)
 }
 
 /***********************************************************************************************************************************
+Return a buffer where output will be written
+***********************************************************************************************************************************/
+static Buffer *
+lz4CompressBuffer(Lz4Compress *this, size_t required, Buffer *output)
+{
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(LZ4_COMPRESS, this);
+        FUNCTION_LOG_PARAM(SIZE, required);
+        FUNCTION_LOG_PARAM(BUFFER, output);
+    FUNCTION_LOG_END();
+
+    Buffer *result = output;
+
+    if (required >= bufRemains(output) || lz4CompressInputSame(this))
+    {
+        // If buffer has not been allocated yet
+        if (this->buffer == NULL)
+        {
+            MEM_CONTEXT_BEGIN(this->memContext)
+            {
+                this->buffer = bufNew(required);
+            }
+            MEM_CONTEXT_END();
+        }
+        // Else buffer is not large enough
+        else if (required >= bufRemains(this->buffer))
+            bufResize(this->buffer, bufUsed(this->buffer) + required);
+
+        this->inputSame = true;
+        result = this->buffer;
+    }
+
+    FUNCTION_LOG_RETURN(BUFFER, result);
+}
+
+/***********************************************************************************************************************************
 Compress data
 ***********************************************************************************************************************************/
 void
@@ -93,41 +131,35 @@ lz4CompressProcess(Lz4Compress *this, const Buffer *uncompressed, Buffer *compre
     ASSERT(this->context != NULL);
     ASSERT(compressed != NULL);
     ASSERT(!this->flush || uncompressed == NULL);
-    // ASSERT(this->flush || (!this->inputSame || this->stream->avail_in != 0));
 
-    // // Flushing
-    // if (uncompressed == NULL)
-    // {
-    //     this->stream->avail_in = 0;
-    //     this->flush = true;
-    // }
-    // // More input
-    // else
-    // {
-    //     // Is new input allowed?
-    //     if (!this->inputSame)
-    //     {
-    //         this->stream->avail_in = (unsigned int)bufUsed(uncompressed);
-    //         this->stream->next_in = bufPtr(uncompressed);
-    //     }
-    // }
-    //
-    // // Initialize compressed output buffer
-    // this->stream->avail_out = (unsigned int)bufRemains(compressed);
-    // this->stream->next_out = bufPtr(compressed) + bufUsed(compressed);
-    //
-    // // Perform compression
-    // lz4Error(deflate(this->stream, this->flush ? Z_FINISH : Z_NO_FLUSH));
-    //
-    // // Set buffer used space
-    // bufUsedSet(compressed, bufSize(compressed) - (size_t)this->stream->avail_out);
-    //
-    // // Is compression done?
-    // if (this->flush && this->stream->avail_out > 0)
-    //     this->done = true;
-    //
-    // // Can more input be provided on the next call?
-    // this->inputSame = this->flush ? !this->done : this->stream->avail_in != 0;
+    if (lz4CompressInputSame(this))
+    {
+        // !!! flush buffer here
+    }
+    else
+    {
+        // If first call to process then begin compression
+        if (this->first)
+        {
+            Buffer *output = lz4CompressBuffer(this, 15, compressed);
+            bufUsedInc(output, LZ4F_compressBegin(this->context, bufRemainsPtr(output), bufRemains(output), NULL));
+
+            this->first = false;
+        }
+
+        // Normal processing call
+        if (uncompressed != NULL)
+        {
+            Buffer *output = lz4CompressBuffer(this, LZ4F_compressBound(bufUsed(uncompressed), NULL), compressed);
+            bufUsedInc(
+                output, LZ4F_compressUpdate(this->context, bufRemainsPtr(output), bufRemains(output), bufPtr(uncompressed), NULL));
+        }
+        // Else flush remaining output
+        else
+        {
+            this->flush = true;
+        }
+    }
 
     FUNCTION_LOG_RETURN_VOID();
 }
@@ -184,8 +216,8 @@ String *
 lz4CompressToLog(const Lz4Compress *this)
 {
     return strNewFmt(
-        "{inputSame: %s, done: %s, flushing: %s}", cvtBoolToConstZ(this->inputSame), cvtBoolToConstZ(this->done),
-        cvtBoolToConstZ(this->done));
+        "{first: %s, inputSame: %s, done: %s, flushing: %s}", cvtBoolToConstZ(this->first), cvtBoolToConstZ(this->inputSame),
+        cvtBoolToConstZ(this->done), cvtBoolToConstZ(this->done));
 }
 
 /***********************************************************************************************************************************
--- test/src/module/common/compressLz4Test.c
+++ test/src/module/common/compressLz4Test.c
@@ -9,68 +9,68 @@ Test LZ4
 /***********************************************************************************************************************************
 Compress data
 ***********************************************************************************************************************************/
-// static Buffer *
-// testCompress(GzipCompress *compress, Buffer *decompressed, size_t inputSize, size_t outputSize)
-// {
-//     Buffer *compressed = bufNew(1024 * 1024);
-//     size_t inputTotal = 0;
-//     ioBufferSizeSet(outputSize);
-//
-//     IoFilterGroup *filterGroup = ioFilterGroupNew();
-//     ioFilterGroupAdd(filterGroup, gzipCompressFilter(compress));
-//     IoWrite *write = ioBufferWriteIo(ioBufferWriteNew(compressed));
-//     ioWriteFilterGroupSet(write, filterGroup);
-//     ioWriteOpen(write);
-//
-//     // Compress input data
-//     while (inputTotal < bufSize(decompressed))
-//     {
-//         // Generate the input buffer based on input size.  This breaks the data up into chunks as it would be in a real scenario.
-//         Buffer *input = bufNewC(
-//             inputSize > bufSize(decompressed) - inputTotal ? bufSize(decompressed) - inputTotal : inputSize,
-//             bufPtr(decompressed) + inputTotal);
-//
-//         ioWrite(write, input);
-//
-//         inputTotal += bufUsed(input);
-//         bufFree(input);
-//     }
-//
-//     ioWriteClose(write);
-//     gzipCompressFree(compress);
-//
-//     return compressed;
-// }
+static Buffer *
+testCompress(Lz4Compress *compress, Buffer *decompressed, size_t inputSize, size_t outputSize)
+{
+    Buffer *compressed = bufNew(1024 * 1024);
+    size_t inputTotal = 0;
+    ioBufferSizeSet(outputSize);
+
+    IoFilterGroup *filterGroup = ioFilterGroupNew();
+    ioFilterGroupAdd(filterGroup, lz4CompressFilter(compress));
+    IoWrite *write = ioBufferWriteIo(ioBufferWriteNew(compressed));
+    ioWriteFilterGroupSet(write, filterGroup);
+    ioWriteOpen(write);
+
+    // Compress input data
+    while (inputTotal < bufSize(decompressed))
+    {
+        // Generate the input buffer based on input size.  This breaks the data up into chunks as it would be in a real scenario.
+        Buffer *input = bufNewC(
+            inputSize > bufSize(decompressed) - inputTotal ? bufSize(decompressed) - inputTotal : inputSize,
+            bufPtr(decompressed) + inputTotal);
+
+        ioWrite(write, input);
+
+        inputTotal += bufUsed(input);
+        bufFree(input);
+    }
+
+    ioWriteClose(write);
+    lz4CompressFree(compress);
+
+    return compressed;
+}
 
 /***********************************************************************************************************************************
 Decompress data
 ***********************************************************************************************************************************/
-// static Buffer *
-// testDecompress(GzipDecompress *decompress, Buffer *compressed, size_t inputSize, size_t outputSize)
-// {
-//     Buffer *decompressed = bufNew(1024 * 1024);
-//     Buffer *output = bufNew(outputSize);
-//     ioBufferSizeSet(inputSize);
-//
-//     IoFilterGroup *filterGroup = ioFilterGroupNew();
-//     ioFilterGroupAdd(filterGroup, gzipDecompressFilter(decompress));
-//     IoRead *read = ioBufferReadIo(ioBufferReadNew(compressed));
-//     ioReadFilterGroupSet(read, filterGroup);
-//     ioReadOpen(read);
-//
-//     while (!ioReadEof(read))
-//     {
-//         ioRead(read, output);
-//         bufCat(decompressed, output);
-//         bufUsedZero(output);
-//     }
-//
-//     ioReadClose(read);
-//     bufFree(output);
-//     gzipDecompressFree(decompress);
-//
-//     return decompressed;
-// }
+static Buffer *
+testDecompress(Lz4Decompress *decompress, Buffer *compressed, size_t inputSize, size_t outputSize)
+{
+    Buffer *decompressed = bufNew(1024 * 1024);
+    Buffer *output = bufNew(outputSize);
+    ioBufferSizeSet(inputSize);
+
+    IoFilterGroup *filterGroup = ioFilterGroupNew();
+    ioFilterGroupAdd(filterGroup, lz4DecompressFilter(decompress));
+    IoRead *read = ioBufferReadIo(ioBufferReadNew(compressed));
+    ioReadFilterGroupSet(read, filterGroup);
+    ioReadOpen(read);
+
+    while (!ioReadEof(read))
+    {
+        ioRead(read, output);
+        bufCat(decompressed, output);
+        bufUsedZero(output);
+    }
+
+    ioReadClose(read);
+    bufFree(output);
+    lz4DecompressFree(decompress);
+
+    return decompressed;
+}
 
 /***********************************************************************************************************************************
 Test Run
@@ -96,59 +96,59 @@ testRun(void)
     {
         #ifndef WITHOUT_LZ4
 
-        // const char *simpleData = "A simple string";
-        // Buffer *compressed = NULL;
-        // Buffer *decompressed = bufNewC(strlen(simpleData), simpleData);
-        //
-        // TEST_ASSIGN(
-        //     compressed, testCompress(gzipCompressNew(3, false), decompressed, 1024, 1024),
-        //     "simple data - compress large in/large out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(compressed, testCompress(gzipCompressNew(3, false), decompressed, 1024, 1)), true,
-        //     "simple data - compress large in/small out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(compressed, testCompress(gzipCompressNew(3, false), decompressed, 1, 1024)), true,
-        //     "simple data - compress small in/large out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(compressed, testCompress(gzipCompressNew(3, false), decompressed, 1, 1)), true,
-        //     "simple data - compress small in/small out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(decompressed, testDecompress(gzipDecompressNew(false), compressed, 1024, 1024)), true,
-        //     "simple data - decompress large in/large out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(decompressed, testDecompress(gzipDecompressNew(false), compressed, 1024, 1)), true,
-        //     "simple data - decompress large in/small out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(decompressed, testDecompress(gzipDecompressNew(false), compressed, 1, 1024)), true,
-        //     "simple data - decompress small in/large out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(decompressed, testDecompress(gzipDecompressNew(false), compressed, 1, 1)), true,
-        //     "simple data - decompress small in/small out buffer");
-        //
-        // // Compress a large zero input buffer into small output buffer
-        // // -------------------------------------------------------------------------------------------------------------------------
-        // decompressed = bufNew(1024 * 1024 - 1);
-        // memset(bufPtr(decompressed), 0, bufSize(decompressed));
-        // bufUsedSet(decompressed, bufSize(decompressed));
-        //
-        // TEST_ASSIGN(
-        //     compressed, testCompress(gzipCompressNew(3, true), decompressed, bufSize(decompressed), 1024),
-        //     "zero data - compress large in/small out buffer");
-        //
-        // TEST_RESULT_BOOL(
-        //     bufEq(decompressed, testDecompress(gzipDecompressNew(true), compressed, bufSize(compressed), 1024 * 256)), true,
-        //     "zero data - decompress large in/small out buffer");
-        //
-        // // -------------------------------------------------------------------------------------------------------------------------
-        // TEST_RESULT_VOID(gzipCompressFree(NULL), "free null decompress object");
-        // TEST_RESULT_VOID(gzipDecompressFree(NULL), "free null decompress object");
+        const char *simpleData = "A simple string";
+        Buffer *compressed = NULL;
+        Buffer *decompressed = bufNewC(strlen(simpleData), simpleData);
+
+        TEST_ASSIGN(
+            compressed, testCompress(lz4CompressNew(0), decompressed, 1024, 1024),
+            "simple data - compress large in/large out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(compressed, testCompress(lz4CompressNew(0), decompressed, 1024, 1)), true,
+            "simple data - compress large in/small out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(compressed, testCompress(lz4CompressNew(0), decompressed, 1, 1024)), true,
+            "simple data - compress small in/large out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(compressed, testCompress(lz4CompressNew(0), decompressed, 1, 1)), true,
+            "simple data - compress small in/small out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(decompressed, testDecompress(lz4DecompressNew(false), compressed, 1024, 1024)), true,
+            "simple data - decompress large in/large out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(decompressed, testDecompress(lz4DecompressNew(false), compressed, 1024, 1)), true,
+            "simple data - decompress large in/small out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(decompressed, testDecompress(lz4DecompressNew(false), compressed, 1, 1024)), true,
+            "simple data - decompress small in/large out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(decompressed, testDecompress(lz4DecompressNew(false), compressed, 1, 1)), true,
+            "simple data - decompress small in/small out buffer");
+
+        // Compress a large zero input buffer into small output buffer
+        // -------------------------------------------------------------------------------------------------------------------------
+        decompressed = bufNew(1024 * 1024 - 1);
+        memset(bufPtr(decompressed), 0, bufSize(decompressed));
+        bufUsedSet(decompressed, bufSize(decompressed));
+
+        TEST_ASSIGN(
+            compressed, testCompress(lz4CompressNew(0), decompressed, bufSize(decompressed), 1024),
+            "zero data - compress large in/small out buffer");
+
+        TEST_RESULT_BOOL(
+            bufEq(decompressed, testDecompress(lz4DecompressNew(true), compressed, bufSize(compressed), 1024 * 256)), true,
+            "zero data - decompress large in/small out buffer");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_RESULT_VOID(lz4CompressFree(NULL), "free null decompress object");
+        TEST_RESULT_VOID(lz4DecompressFree(NULL), "free null decompress object");
 
         #endif // WITHOUT_LZ4
     }
@@ -158,13 +158,13 @@ testRun(void)
     {
         #ifndef WITHOUT_LZ4
 
-        // GzipDecompress *decompress = gzipDecompressNew(false);
+        // Lz4Decompress *decompress = lz4DecompressNew(false);
         //
-        // TEST_RESULT_STR(strPtr(gzipDecompressToLog(decompress)), "{inputSame: false, done: false, availIn: 0}", "format object");
+        // TEST_RESULT_STR(strPtr(lz4DecompressToLog(decompress)), "{inputSame: false, done: false, availIn: 0}", "format object");
         //
         // decompress->inputSame = true;
         // decompress->done = true;
-        // TEST_RESULT_STR(strPtr(gzipDecompressToLog(decompress)), "{inputSame: true, done: true, availIn: 0}", "format object");
+        // TEST_RESULT_STR(strPtr(lz4DecompressToLog(decompress)), "{inputSame: true, done: true, availIn: 0}", "format object");
 
         #endif // WITHOUT_LZ4
     }
