e4cc008b982d47ac526962e310c18626e2aefbc2
David Steele <david@crunchydata.com>
2019-05-22 11:31:38 -0400

    Remove duplicated hint.
    At some point this hint got added to the underlying code so it is no longer needed here.

--- lib/pgBackRest/Stanza.pm
+++ lib/pgBackRest/Stanza.pm
@@ -36,7 +36,6 @@ my $strHintForce = "\nHINT: use stanza-create --force to force the stanza data t
 my $strInfoMissing = " information missing";
 my $strStanzaCreateErrorMsg = "not empty";
 my $strRepoEncryptedMsg = " and repo is encrypted and info file(s) are missing, --force cannot be used";
-my $strHintStanzaCreate = "\nHINT: has a stanza-create been performed?";
 
 ####################################################################################################################################
 # CONSTRUCTOR
@@ -592,8 +591,7 @@ sub infoObject
         {
             if ($iResult == ERROR_FILE_MISSING)
             {
-                confess &log(ERROR, cfgOptionValid(CFGOPT_FORCE) ? $strResultMessage . $strHintForce :
-                    $strResultMessage . $strHintStanzaCreate, $iResult);
+                confess &log(ERROR, cfgOptionValid(CFGOPT_FORCE) ? $strResultMessage . $strHintForce : $strResultMessage, $iResult);
             }
             else
             {
