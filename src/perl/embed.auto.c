9e730c1bd61b00fa506ea443cdd6161e88ed31ae
David Steele <david@pgmasters.net>
2019-08-05 12:03:04 -0400

    v2.16: C Migrations and Bug Fixes
    Bug Fixes:
    
    * Retry S3 RequestTimeTooSkewed errors instead of immediately terminating. (Reported by sean0101n, Tim Garton, Jesper St John, Aleš Zelený.)
    * Fix incorrect handling of transfer-encoding response to HEAD request. (Reported by Pavel Suderevsky.)
    * Fix scoping violations exposed by optimizations in gcc 9. (Reported by Christian Lange, Ned T. Crigler.)
    
    Features:
    
    * Add repo-s3-port option for setting a non-standard S3 service port.
    
    Improvements:
    
    * The local command for backup is implemented entirely in C. (Contributed by David Steele, Cynthia Shang.)
    * The check command is implemented partly in C. (Reviewed by Cynthia Shang.)

--- src/perl/embed.auto.c
+++ src/perl/embed.auto.c
@@ -15672,7 +15672,7 @@ static const EmbeddedModule embeddedModule[] =
             "\n"
             "push @EXPORT, qw(projectBin projectBinSet);\n"
             "\n\n\n\n\n\n"
-            "use constant PROJECT_VERSION => '2.16dev';\n"
+            "use constant PROJECT_VERSION => '2.16';\n"
             "push @EXPORT, qw(PROJECT_VERSION);\n"
             "\n\n\n\n\n\n"
             "use constant REPOSITORY_FORMAT => 5;\n"
