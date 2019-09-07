843a602080e13ea0f205720892f7bfee58e075fb
David Steele <david@pgmasters.net>
2019-09-07 15:41:55 -0400

    Add user/group id to storageInfo().
    This is required for some operations on Posix storage.

--- src/storage/info.h
+++ src/storage/info.h
@@ -26,8 +26,10 @@ typedef struct StorageInfo
     const String *linkDestination;                                  // Destination if this is a link
     StorageType type;                                               // Type file/path/link)
     bool exists;                                                    // Does the path/file/link exist?
-    const String *user;                                             // User that owns the file
-    const String *group;                                            // Group that owns the file
+    uid_t userId;                                                   // User that owns the file
+    const String *user;                                             // Name of user that owns the file
+    uid_t groupId;                                                  // Group that owns the file
+    const String *group;                                            // Name of group that owns the file
     mode_t mode;                                                    // Mode of path/file/link
     time_t timeModified;                                            // Time file was last modified
     uint64_t size;                                                  // Size (path/link is 0)
