158e439689c8247ba2e53da4bfe65934e391b690
David Steele <david@pgmasters.net>
2019-11-26 17:16:45 -0500

    Remove obsolete Perl archive code.
    This should have been removed in a1c13a50 but was missed.

--- lib/pgBackRest/Protocol/Remote/Minion.pm
+++ lib/pgBackRest/Protocol/Remote/Minion.pm
@@ -14,7 +14,6 @@ use pgBackRest::Backup::File;
 use pgBackRest::Common::Log;
 use pgBackRest::Common::Io::Buffered;
 use pgBackRest::Common::Wait;
-use pgBackRest::Archive::Get::File;
 use pgBackRest::Config::Config;
 use pgBackRest::Db;
 use pgBackRest::Protocol::Command::Minion;
@@ -72,9 +71,6 @@ sub init
     # Create anonymous subs for each command
     my $hCommandMap =
     {
-        # ArchiveGet commands
-        &OP_ARCHIVE_GET_CHECK => sub {archiveGetCheck(@{shift()})},
-
         # Db commands
         &OP_DB_CONNECT => sub {$oDb->connect()},
         &OP_DB_EXECUTE_SQL => sub {$oDb->executeSql(@{shift()})},
