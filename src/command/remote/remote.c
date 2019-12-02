28116918ff3a00b133d74d524df452e29398082d
David Steele <david@pgmasters.net>
2019-12-02 07:35:36 -0500

    Error in remote command when stop file exists.
    This duplicates the Perl functionality.

--- src/command/remote/remote.c
+++ src/command/remote/remote.c
@@ -5,6 +5,7 @@ Remote Command
 
 #include <string.h>
 
+#include "command/control/common.h"
 #include "common/debug.h"
 #include "common/io/handleRead.h"
 #include "common/io/handleWrite.h"
@@ -53,9 +54,16 @@ cmdRemote(int handleRead, int handleWrite)
 
                 // Acquire a lock if this command requires a lock
                 if (cfgLockRemoteRequired(commandId))
+                {
+                    // Make sure the local host is not stopped
+                    lockStopTest();
+
+                    // Acquire the lock
                     lockAcquire(cfgOptionStr(cfgOptLockPath), cfgOptionStr(cfgOptStanza), cfgLockRemoteType(commandId), 0, true);
+                }
             }
 
+            // Notify the client of success
             protocolServerResponse(server, NULL);
             success = true;
         }
