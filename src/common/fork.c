8ecbc0115fbf4ca81d6613d1c8e43e29f6e16e8f
David Steele <david@pgmasters.net>
2020-02-05 21:34:07 -0800

    Fork improvements.

--- src/common/fork.c
+++ src/common/fork.c
@@ -3,6 +3,8 @@ Fork Handler
 ***********************************************************************************************************************************/
 #include "build.auto.h"
 
+#include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "common/debug.h"
@@ -33,11 +35,23 @@ forkDetach(void)
 {
     FUNCTION_LOG_VOID(logLevelTrace);
 
-    // Change the working directory to / so we won't error if the old working directory goes away
-    THROW_ON_SYS_ERROR(chdir("/") == -1, PathMissingError, "unable to change directory to '/'");
-
     // Make this process a group leader so the parent process won't block waiting for it to finish
     THROW_ON_SYS_ERROR(setsid() == -1, KernelError, "unable to create new session group");
+
+    // The process should never receive a SIGHUP but ignore it just in case
+    signal(SIGHUP, SIG_IGN);
+
+    // Fork again and let the parent process terminate to ensure that we get rid of the session leading process. Only session
+    // leaders may get a TTY again.
+    pid_t pid = fork();
+
+    THROW_ON_SYS_ERROR(pid < 0, KernelError, "unable to fork");
+
+    if (fork() > 0)
+        exit(0);
+
+    // Change the working directory to / so we won't error if the old working directory goes away
+    THROW_ON_SYS_ERROR(chdir("/") == -1, PathMissingError, "unable to change directory to '/'");
 
     // Close standard file handles
     THROW_ON_SYS_ERROR(close(STDIN_FILENO) == -1, FileCloseError, "unable to close stdin");
