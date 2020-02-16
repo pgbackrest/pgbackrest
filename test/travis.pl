6203ec4d435fa651cdfe6e055dbe5f11c74d26cb
David Steele <david@pgmasters.net>
2020-02-16 14:13:15 -0600

    Move lz4 packages.

--- test/travis.pl
+++ test/travis.pl
@@ -133,7 +133,7 @@ eval
     processExec('sudo apt-get -qq update', {bSuppressStdErr => true, bSuppressError => true});
     processExec(
         'sudo apt-get install -y rsync zlib1g-dev libssl-dev libxml2-dev libpq-dev libxml-checker-perl libyaml-libyaml-perl' .
-            ' pkg-config liblz4-dev liblz4-tool',
+            ' pkg-config',
         {bSuppressStdErr => true});
     processEnd();
 
@@ -181,7 +181,7 @@ eval
 
         if ($strVm eq VM_NONE)
         {
-            $strPackage .= " valgrind";
+            $strPackage .= " valgrind liblz4-dev liblz4-tool";
         }
         else
         {
