948be1a08800f8bd82b6bb81b8e7fc16664acc63
Cynthia Shang <cynthia.shang@crunchydata.com>
2020-03-24 13:58:57 -0400

    Replace one more unnecessary disk write in expireTest.c

--- test/src/module/command/expireTest.c
+++ test/src/module/command/expireTest.c
@@ -156,10 +156,8 @@ testRun(void)
         TEST_TITLE("manifest file removal");
 
         // Create backup.info
-        TEST_RESULT_VOID(storagePutP(storageNewWriteP(storageTest, backupInfoFileName), backupInfoBase), "write backup.info");
-
         InfoBackup *infoBackup = NULL;
-        TEST_ASSIGN(infoBackup, infoBackupLoadFile(storageTest, backupInfoFileName, cipherTypeNone, NULL), "get backup.info");
+        TEST_ASSIGN(infoBackup, infoBackupNewLoad(ioBufferReadNew(backupInfoBase)), "get backup.info");
 
         // Create backup directories and manifest files
         String *full1 = strNew("20181119-152138F");
