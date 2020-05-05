333cef0112047c27a58094733ec3e3a5334cf10e
Stephen Frost <sfrost@snowman.net>
2020-05-05 12:51:07 -0400

    fixup! fixup! fixup! fixup! fixup! Add bzip2 unit tests.

--- src/common/compress/bz2/common.c
+++ src/common/compress/bz2/common.c
@@ -18,83 +18,83 @@ bz2Error(int error)
     FUNCTION_TEST_END();
 
     if (error < 0)
-	{
-		const char *errorMsg;
-		const ErrorType *errorType = &FormatError;
-		
-		switch (error)
-		{
-			case BZ_SEQUENCE_ERROR:
-			{
-				errorMsg = "sequence error";
-				errorType = &AssertError;
-				break;
-			}
+    {
+        const char *errorMsg;
+        const ErrorType *errorType = &FormatError;
+        
+        switch (error)
+        {
+            case BZ_SEQUENCE_ERROR:
+            {
+                errorMsg = "sequence error";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_PARAM_ERROR:
-			{
-				errorMsg = "parameter error";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_PARAM_ERROR:
+            {
+                errorMsg = "parameter error";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_MEM_ERROR:
-			{
-				errorMsg = "memory error";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_MEM_ERROR:
+            {
+                errorMsg = "memory error";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_DATA_ERROR:
-			{
-				errorMsg = "data error";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_DATA_ERROR:
+            {
+                errorMsg = "data error";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_DATA_ERROR_MAGIC:
-			{
-				errorMsg = "data error magic";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_DATA_ERROR_MAGIC:
+            {
+                errorMsg = "data error magic";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_IO_ERROR:
-			{
-				errorMsg = "io error";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_IO_ERROR:
+            {
+                errorMsg = "io error";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_UNEXPECTED_EOF:
-			{
-				errorMsg = "unexpected eof";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_UNEXPECTED_EOF:
+            {
+                errorMsg = "unexpected eof";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_OUTBUFF_FULL:
-			{
-				errorMsg = "outbuff full";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_OUTBUFF_FULL:
+            {
+                errorMsg = "outbuff full";
+                errorType = &AssertError;
+                break;
+            }
 
-			case BZ_CONFIG_ERROR:
-			{
-				errorMsg = "config error";
-				errorType = &AssertError;
-				break;
-			}
+            case BZ_CONFIG_ERROR:
+            {
+                errorMsg = "config error";
+                errorType = &AssertError;
+                break;
+            }
 
-			default:
-			{
-				errorMsg = "unknown error";
-				errorType = &AssertError;
-			}
-		}
+            default:
+            {
+                errorMsg = "unknown error";
+                errorType = &AssertError;
+            }
+        }
         THROWP_FMT(errorType, "bz2 error: [%d] %s", error, errorMsg);
-	}
+    }
 
     FUNCTION_TEST_RETURN(error);
 }
