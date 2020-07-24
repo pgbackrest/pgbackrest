e7bd160497a3366e5af1f0dea1ca505597dc8a07
David Steele <david@pgmasters.net>
2020-07-24 18:54:57 -0400

    Improved memory management.

--- test/src/module/performance/typeTest.c
+++ test/src/module/performance/typeTest.c
@@ -6,16 +6,19 @@ up" in terms of execution time if there are performance problems without taking 
 
 These starting values can then be scaled up for profiling and stress testing as needed.  In general we hope to scale to 1000 without
 running out of memory on the test systems or taking an undue amount of time.  It should be noted that in this context scaling to
-1000 is nowhere near to turning it up to 11.
+1000 is nowhere near turning it up to 11.
 ***********************************************************************************************************************************/
 #include "common/ini.h"
 #include "common/io/bufferRead.h"
 #include "common/io/bufferWrite.h"
 #include "common/time.h"
 #include "common/type/list.h"
+#include "common/type/object.h"
 #include "info/manifest.h"
+#include "postgres/version.h"
 
 #include "common/harnessInfo.h"
+#include "common/harnessStorage.h"
 
 /***********************************************************************************************************************************
 Test sort comparator
@@ -45,6 +48,100 @@ testIniLoadCountCallback(void *data, const String *section, const String *key, c
     (void)section;
     (void)key;
     (void)value;
+}
+
+/***********************************************************************************************************************************
+Driver to test manifestNewBuild()
+***********************************************************************************************************************************/
+typedef struct
+{
+    STORAGE_COMMON_MEMBER;
+    uint64_t fileTotal;
+} StorageTestManifestNewBuild;
+
+static StorageInfo
+storageTestManifestNewBuildInfo(THIS_VOID, const String *file, StorageInfoLevel level, StorageInterfaceInfoParam param)
+{
+    (void)thisVoid; (void)level; (void)param;
+
+    StorageInfo result =
+    {
+        .level = storageInfoLevelDetail,
+        .exists = true,
+        .type = storageTypePath,
+        .mode = 0600,
+        .userId = 100,
+        .groupId = 100,
+        .user = STRDEF("test"),
+        .group = STRDEF("test"),
+    };
+
+    if (strEq(file, STRDEF("/pg")))
+    {
+        result.type = storageTypePath;
+    }
+    else
+        THROW_FMT(AssertError, "unhandled file info '%s'", strPtr(file));
+
+    return result;
+}
+
+static bool
+storageTestManifestNewBuildInfoList(
+    THIS_VOID, const String *path, StorageInfoLevel level, StorageInfoListCallback callback, void *callbackData,
+    StorageInterfaceInfoListParam param)
+{
+    THIS(StorageTestManifestNewBuild);
+    (void)path; (void)level; (void)param;
+
+    MEM_CONTEXT_TEMP_BEGIN()
+    {
+        StorageInfo result =
+        {
+            .level = storageInfoLevelDetail,
+            .exists = true,
+            .type = storageTypePath,
+            .mode = 0700,
+            .userId = 100,
+            .groupId = 100,
+            .user = STRDEF("test"),
+            .group = STRDEF("test"),
+        };
+
+        if (strEq(path, STRDEF("/pg")))
+        {
+            result.name = STRDEF("base");
+            callback(callbackData, &result);
+        }
+        else if (strEq(path, STRDEF("/pg/base")))
+        {
+            result.name = STRDEF("1000000000");
+            callback(callbackData, &result);
+        }
+        else if (strEq(path, STRDEF("/pg/base/1000000000")))
+        {
+            result.type = storageTypeFile;
+            result.size = 8192;
+            result.timeModified = 1595627966;
+            result.mode = 0600;
+
+            MEM_CONTEXT_TEMP_RESET_BEGIN()
+            {
+                for (unsigned int fileIdx = 0; fileIdx < this->fileTotal; fileIdx++)
+                {
+                    result.name = strNewFmt("%u", 1000000000 + fileIdx);
+                    callback(callbackData, &result);
+                    MEM_CONTEXT_TEMP_RESET(1000);
+                }
+            }
+            MEM_CONTEXT_TEMP_END();
+        }
+        else
+            THROW_FMT(AssertError, "unhandled file list info '%s'", strPtr(path));
+    }
+    MEM_CONTEXT_TEMP_END();
+
+    return true;
 }
 
 /***********************************************************************************************************************************
@@ -273,6 +370,46 @@ testRun(void)
         }
 
         TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
+    }
+
+    // Build larger manifest to test performance and memory usage. The default sizing is for a "typical" cluster but this can be
+    // scaled to test larger cluster sizes.
+    // *****************************************************************************************************************************
+    if (testBegin("manifestNewBuild()"))
+    {
+        CHECK(testScale() <= 1000000);
+
+        // Create a storage driver to test manifest build with an arbitrary number of files
+        StorageTestManifestNewBuild driver =
+        {
+            .interface = storageInterfaceTestDummy,
+            .fileTotal = 100000 * (unsigned int)testScale(),
+        };
+
+        driver.interface.info = storageTestManifestNewBuildInfo;
+        driver.interface.infoList = storageTestManifestNewBuildInfoList;
+
+        Storage *storagePg = storageNew(STRDEF("TEST"), STRDEF("/pg"), 0, 0, false, NULL, &driver, driver.interface);
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("build manifest");
+
+        TimeMSec timeBegin = timeMSec();
+        MemContext *memContext = memContextNew("test");
+        Manifest *manifest = NULL;
+
+        MEM_CONTEXT_BEGIN(memContext)
+        {
+            TEST_ASSIGN(
+                manifest, manifestNewBuild(storagePg, PG_VERSION_91, false, false, NULL, NULL), "build with %" PRIu64 " files",
+                driver.fileTotal);
+        }
+        MEM_CONTEXT_END();
+
+        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
+        TEST_LOG_FMT("memory used %zu", memContextSize(memContext));
+
+        TEST_RESULT_UINT(manifestFileTotal(manifest), driver.fileTotal, "   check file total");
     }
 
     FUNCTION_HARNESS_RESULT_VOID();
