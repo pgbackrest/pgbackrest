8b2ecac3fa5005930660699d9857efb42dde8f3d
David Steele <david@pgmasters.net>
2020-07-25 10:57:24 -0400

    Consolidate tests.

--- test/src/module/performance/typeTest.c
+++ test/src/module/performance/typeTest.c
@@ -6,7 +6,7 @@ up" in terms of execution time if there are performance problems without taking 
 
 These starting values can then be scaled up for profiling and stress testing as needed.  In general we hope to scale to 1000 without
 running out of memory on the test systems or taking an undue amount of time.  It should be noted that in this context scaling to
-1000 is nowhere near turning it up to 11.
+1000 is nowhere near to turning it up to 11.
 ***********************************************************************************************************************************/
 #include "common/ini.h"
 #include "common/io/bufferRead.h"
@@ -51,7 +51,7 @@ testIniLoadCountCallback(void *data, const String *section, const String *key, c
 }
 
 /***********************************************************************************************************************************
-Driver to test manifestNewBuild()
+Driver to test manifestNewBuild(). Generates files for a valid-looking PostgreSQL cluster that can be scaled to any size.
 ***********************************************************************************************************************************/
 typedef struct
 {
@@ -94,7 +94,7 @@ storageTestManifestNewBuildInfoList(
     THIS(StorageTestManifestNewBuild);
     (void)path; (void)level; (void)param;
 
-    MEM_CONTEXT_TEMP_BEGIN()
+    MEM_CONTEXT_TEMP_RESET_BEGIN()
     {
         StorageInfo result =
         {
@@ -125,16 +125,12 @@ storageTestManifestNewBuildInfoList(
             result.timeModified = 1595627966;
             result.mode = 0600;
 
-            MEM_CONTEXT_TEMP_RESET_BEGIN()
+            for (unsigned int fileIdx = 0; fileIdx < this->fileTotal; fileIdx++)
             {
-                for (unsigned int fileIdx = 0; fileIdx < this->fileTotal; fileIdx++)
-                {
-                    result.name = strNewFmt("%u", 1000000000 + fileIdx);
-                    callback(callbackData, &result);
-                    MEM_CONTEXT_TEMP_RESET(1000);
-                }
+                result.name = strNewFmt("%u", 1000000000 + fileIdx);
+                callback(callbackData, &result);
+                MEM_CONTEXT_TEMP_RESET(10000);
             }
-            MEM_CONTEXT_TEMP_END();
         }
         else
             THROW_FMT(AssertError, "unhandled file list info '%s'", strPtr(path));
@@ -210,172 +206,10 @@ testRun(void)
         TEST_RESULT_INT(iniTotal, iniMax, "    check ini total");
     }
 
-    // Load/save a larger manifest to test performance and memory usage.  The default sizing is for a "typical" cluster but this can
-    // be scaled to test larger cluster sizes.
+    // Build/load/save a larger manifest to test performance and memory usage. The default sizing is for a "typical" large cluster
+    // but this can be scaled to test larger cluster sizes.
     // *****************************************************************************************************************************
-    if (testBegin("manifestNewLoad()/manifestSave()"))
-    {
-        CHECK(testScale() <= 1000000);
-
-        // Manifest with all features
-        // -------------------------------------------------------------------------------------------------------------------------
-        TEST_TITLE("generate manifest");
-
-        String *manifestStr = strNew
-        (
-            "[backup]\n"
-            "backup-label=\"20190818-084502F_20190820-084502D\"\n"
-            "backup-prior=\"20190818-084502F\"\n"
-            "backup-timestamp-copy-start=1566290707\n"
-            "backup-timestamp-start=1566290702\n"
-            "backup-timestamp-stop=1566290710\n"
-            "backup-type=\"diff\"\n"
-            "\n"
-            "[backup:db]\n"
-            "db-catalog-version=201809051\n"
-            "db-control-version=1100\n"
-            "db-id=2\n"
-            "db-system-id=6689162560678426440\n"
-            "db-version=\"11\"\n"
-            "\n"
-            "[backup:option]\n"
-            "option-archive-check=true\n"
-            "option-archive-copy=false\n"
-            "option-backup-standby=false\n"
-            "option-buffer-size=1048576\n"
-            "option-checksum-page=true\n"
-            "option-compress=true\n"
-            "option-compress-level=9\n"
-            "option-compress-level-network=3\n"
-            "option-delta=false\n"
-            "option-hardlink=false\n"
-            "option-online=false\n"
-            "option-process-max=2\n"
-            "\n"
-            "[backup:target]\n"
-            "pg_data={\"path\":\"/pg/base\",\"type\":\"path\"}\n");
-
-        for (unsigned int linkIdx = 0; linkIdx < 1; linkIdx++)
-            strCatFmt(manifestStr, "pg_data/pg_stat%u={\"path\":\"../pg_stat\",\"type\":\"link\"}\n", linkIdx);
-
-        strCatZ(
-            manifestStr,
-            "\n"
-            "[target:file]\n");
-
-        unsigned int fileTotal = 100000 * (unsigned int)testScale();
-
-        // Because of the way the filenames are formatted they will end up badly out of order.  We'll be depending on the sort after
-        // load the fix this.  Normally the files won't need sorting, but a collation issue could well cause problems for us without
-        // it.
-        for (unsigned int fileIdx = 0; fileIdx < fileTotal; fileIdx++)
-        {
-            strCatFmt(
-                manifestStr,
-                "pg_data/base/16384/%u={\"checksum\":\"184473f470864e067ee3a22e64b47b0a1c356f29\",\"size\":16384"
-                    ",\"timestamp\":1565282114}\n",
-                16384 + fileIdx);
-        }
-
-        strCatZ(
-            manifestStr,
-            "\n"
-            "[target:file:default]\n"
-            "group=\"postgres\"\n"
-            "master=false\n"
-            "mode=\"0600\"\n"
-            "user=\"postgres\"\n"
-            "\n"
-            "[target:link]\n"
-            "pg_data/pg_stat={\"destination\":\"../pg_stat\"}\n"
-            "\n"
-            "[target:link:default]\n"
-            "group=\"postgres\"\n"
-            "user=\"postgres\"\n"
-            "\n"
-            "[target:path]\n"
-            "pg_data={}\n"
-            "pg_data/base={}\n"
-            "pg_data/base/1={}\n"
-            "pg_data/base/13124={}\n"
-            "pg_data/base/13125={}\n"
-            "pg_data/base/16391={}\n"
-            "pg_data/global={}\n"
-            "pg_data/pg_commit_ts={}\n"
-            "pg_data/pg_dynshmem={}\n"
-            "pg_data/pg_logical={}\n"
-            "pg_data/pg_logical/mappings={}\n"
-            "pg_data/pg_logical/snapshots={}\n"
-            "pg_data/pg_multixact={}\n"
-            "pg_data/pg_multixact/members={}\n"
-            "pg_data/pg_multixact/offsets={}\n"
-            "pg_data/pg_notify={}\n"
-            "pg_data/pg_replslot={}\n"
-            "pg_data/pg_serial={}\n"
-            "pg_data/pg_snapshots={}\n"
-            "pg_data/pg_stat={}\n"
-            "pg_data/pg_stat_tmp={}\n"
-            "pg_data/pg_subtrans={}\n"
-            "pg_data/pg_tblspc={}\n"
-            "pg_data/pg_twophase={}\n"
-            "pg_data/pg_wal={}\n"
-            "pg_data/pg_wal/archive_status={}\n"
-            "pg_data/pg_xact={}\n"
-            "\n"
-            "[target:path:default]\n"
-            "group=\"postgres\"\n"
-            "mode=\"0700\"\n"
-            "user=\"postgres\"\n"
-        );
-
-        const Buffer *contentLoad = harnessInfoChecksum(manifestStr);
-
-        TEST_LOG_FMT("%s manifest generated with %u files", strPtr(strSizeFormat(bufUsed(contentLoad))), fileTotal);
-
-        // -------------------------------------------------------------------------------------------------------------------------
-        TEST_TITLE("load manifest");
-
-        TimeMSec timeBegin = timeMSec();
-        MemContext *memContext = memContextNew("test");
-        Manifest *manifest = NULL;
-
-        MEM_CONTEXT_BEGIN(memContext)
-        {
-            manifest = manifestNewLoad(ioBufferReadNew(contentLoad));
-        }
-        MEM_CONTEXT_END();
-
-        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
-        TEST_LOG_FMT("memory used %zu", memContextSize(memContext));
-
-        TEST_RESULT_UINT(manifestFileTotal(manifest), fileTotal, "   check file total");
-
-        // -------------------------------------------------------------------------------------------------------------------------
-        TEST_TITLE("save manifest");
-
-        Buffer *contentSave = bufNew(0);
-        timeBegin = timeMSec();
-        manifestSave(manifest, ioBufferWriteNew(contentSave));
-        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
-
-        // -------------------------------------------------------------------------------------------------------------------------
-        TEST_TITLE("find all files");
-
-        timeBegin = timeMSec();
-
-        for (unsigned int fileIdx = 0; fileIdx < manifestFileTotal(manifest); fileIdx++)
-        {
-            const ManifestFile *file = manifestFile(manifest, fileIdx);
-            CHECK(file == manifestFileFind(manifest, file->name));
-        }
-
-        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
-    }
-
-    // Build larger manifest to test performance and memory usage. The default sizing is for a "typical" cluster but this can be
-    // scaled to test larger cluster sizes.
-    // *****************************************************************************************************************************
-    if (testBegin("manifestNewBuild()"))
+    if (testBegin("manifestNewBuild()/manifestNewLoad()/manifestSave()"))
     {
         CHECK(testScale() <= 1000000);
 
@@ -394,11 +228,12 @@ testRun(void)
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("build manifest");
 
-        TimeMSec timeBegin = timeMSec();
-        MemContext *memContext = memContextNew("test");
+        MemContext *testContext = memContextNew("test");
+        memContextKeep();
         Manifest *manifest = NULL;
+        TimeMSec timeBegin = timeMSec();
 
-        MEM_CONTEXT_BEGIN(memContext)
+        MEM_CONTEXT_BEGIN(testContext)
         {
             TEST_ASSIGN(
                 manifest, manifestNewBuild(storagePg, PG_VERSION_91, false, false, NULL, NULL), "build with %" PRIu64 " files",
@@ -407,9 +242,52 @@ testRun(void)
         MEM_CONTEXT_END();
 
         TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
-        TEST_LOG_FMT("memory used %zu", memContextSize(memContext));
+        TEST_LOG_FMT("memory used %zu", memContextSize(testContext));
 
         TEST_RESULT_UINT(manifestFileTotal(manifest), driver.fileTotal, "   check file total");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("save manifest");
+
+        Buffer *contentSave = bufNew(0);
+        timeBegin = timeMSec();
+
+        manifestSave(manifest, ioBufferWriteNew(contentSave));
+
+        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
+
+        memContextFree(testContext);
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("load manifest");
+
+        testContext = memContextNew("test");
+        memContextKeep();
+        timeBegin = timeMSec();
+
+        MEM_CONTEXT_BEGIN(testContext)
+        {
+            manifest = manifestNewLoad(ioBufferReadNew(contentSave));
+        }
+        MEM_CONTEXT_END();
+
+        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
+        TEST_LOG_FMT("memory used %zu", memContextSize(testContext));
+
+        TEST_RESULT_UINT(manifestFileTotal(manifest), driver.fileTotal, "   check file total");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("find all files");
+
+        timeBegin = timeMSec();
+
+        for (unsigned int fileIdx = 0; fileIdx < manifestFileTotal(manifest); fileIdx++)
+        {
+            const ManifestFile *file = manifestFile(manifest, fileIdx);
+            CHECK(file == manifestFileFind(manifest, file->name));
+        }
+
+        TEST_LOG_FMT("completed in %ums", (unsigned int)(timeMSec() - timeBegin));
     }
 
     FUNCTION_HARNESS_RESULT_VOID();
