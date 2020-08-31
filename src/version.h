05ef03feb5550f7151ee34afe6b0358637f65fc8
David Steele <david@pgmasters.net>
2020-08-31 07:41:48 -0400

    Begin v2.30 development.

--- src/version.h
+++ src/version.h
@@ -23,6 +23,6 @@ repository will be invalid unless migration functions are written.
 /***********************************************************************************************************************************
 Software version.  Currently this value is maintained in Version.pm and updated by test.pl.
 ***********************************************************************************************************************************/
-#define PROJECT_VERSION                                             "2.29"
+#define PROJECT_VERSION                                             "2.30dev"
 
 #endif
