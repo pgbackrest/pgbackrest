22b398fd436ccb59d770efff7157782965b6c5ba
David Steele <david@pgmasters.net>
2020-10-21 18:24:30 -0400

    Allow [, #, and space as the first character in database names.

--- src/info/info.c
+++ src/info/info.c
@@ -352,7 +352,7 @@ infoSaveValue(InfoSave *infoSaveData, const String *section, const String *key, 
     ASSERT(key != NULL);
     ASSERT(jsonValue != NULL);
     ASSERT(strSize(jsonValue) != 0);
-    // The json value must not be an array because this may be confused with a section in the ini file
+    // The JSON value must not be an array because this may be confused with a section in the ini file
     ASSERT(strZ(jsonValue)[0] != '[');
 
     // Save section
