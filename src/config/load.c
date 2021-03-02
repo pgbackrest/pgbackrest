7a475c5caf071b46fc2f4b8a3cdc4de012aff6ae
David Steele <david@pgmasters.net>
2021-03-02 17:23:18 -0500

    Remove repo option requirement for repo-* commands.

--- src/config/load.c
+++ src/config/load.c
@@ -70,9 +70,8 @@ cfgLoadUpdateOption(void)
 
     // Make sure repo option is set for the default command role when it is not internal and more than one repo is configured or the
     // first configured repo is not key 1. Filter out any commands where this does not apply.
-    if (!cfgCommandHelp() && cfgCommand() != cfgCmdBackup && cfgCommand() != cfgCmdInfo && cfgCommand() != cfgCmdExpire &&
-        cfgCommand() != cfgCmdArchiveGet && cfgCommand() != cfgCmdRestore && cfgOptionValid(cfgOptRepo) &&
-        !cfgOptionTest(cfgOptRepo) && (cfgOptionGroupIdxTotal(cfgOptGrpRepo) > 1 || cfgOptionGroupIdxToKey(cfgOptGrpRepo, 0) != 1))
+    if (!cfgCommandHelp() && cfgCommand() == cfgCmdStanzaDelete && !cfgOptionTest(cfgOptRepo) &&
+        (cfgOptionGroupIdxTotal(cfgOptGrpRepo) > 1 || cfgOptionGroupIdxToKey(cfgOptGrpRepo, 0) != 1))
     {
         THROW_FMT(
             OptionRequiredError,
