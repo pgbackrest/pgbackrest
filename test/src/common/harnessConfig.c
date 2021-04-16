e3868b0a1c549a9b0ec1c3790d5efca2bd1846e6
David Steele <david@pgmasters.net>
2021-04-16 17:58:03 -0400

    Add hrnCfgArgRawStrId() and hrnCfgArgKeyRawStrId().
    Allow StringIds to be easily added to configurations in unit tests.

--- test/src/common/harnessConfig.c
+++ test/src/common/harnessConfig.c
@@ -161,6 +161,21 @@ hrnCfgArgKeyRawZ(StringList *argList, ConfigOption optionId, unsigned optionKey,
 }
 
 void
+hrnCfgArgRawStrId(StringList *argList, ConfigOption optionId, StringId value)
+{
+    hrnCfgArgKeyRawStrId(argList, optionId, 1, value);
+}
+
+void
+hrnCfgArgKeyRawStrId(StringList *argList, ConfigOption optionId, unsigned optionKey, StringId value)
+{
+    char buffer[STRID_MAX + 1];
+    strIdToZ(value, buffer);
+
+    hrnCfgArgKeyRawZ(argList, optionId, optionKey, buffer);
+}
+
+void
 hrnCfgArgRawBool(StringList *argList, ConfigOption optionId, bool value)
 {
     hrnCfgArgKeyRawBool(argList, optionId, 1, value);
