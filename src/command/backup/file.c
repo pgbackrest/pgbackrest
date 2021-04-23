5b4e95ed14b72def3f647c1f19f557fc4a2223ce
David Steele <david@pgmasters.net>
2021-04-23 11:21:23 -0400

    Fix typo in comment.

--- src/command/backup/file.c
+++ src/command/backup/file.c
@@ -228,7 +228,7 @@ backupFile(
             }
 
             // Setup the repo file for write. There is no need to write the file atomically (e.g. via a temp file on Posix) because
-            // checksums are tested on resume after a failed backup. The path does not need be synced for each file because all
+            // checksums are tested on resume after a failed backup. The path does not need to be synced for each file because all
             // paths are synced at the end of the backup.
             StorageWrite *write = storageNewWriteP(
                 storageRepoWrite(), repoPathFile, .compressible = compressible, .noAtomic = true, .noSyncPath = true);
