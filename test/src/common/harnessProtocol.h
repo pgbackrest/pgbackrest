58369c02df39ac9916d5de42d2df9ad509c33b6f
David Steele <david@pgmasters.net>
2021-05-25 18:16:59 -0400

    Add remote process shim.
    Run the remote process inside a forked child process instead of exec'ing it. This allows coverage to accumulate in the remote process rather than needing to test the remote protocol functions directly, resulting in better end-to-end testing and less test duplication. Another advantage is that the pgbackrest binary does not need to be built for the test and the test does not need to run in a container.

--- test/src/common/harnessProtocol.h
+++ test/src/common/harnessProtocol.h
@@ -11,3 +11,8 @@ Functions
 // functions should be required. A side benefit is that the pgbackrest binary does not need to be built since there is no exec.
 void hrnProtocolLocalShimInstall(const ProtocolServerHandler *const handlerList, const unsigned int handlerListSize);
 void hrnProtocolLocalShimUninstall(void);
+
+// Install/uninstall the shim that allows protocalRemoteGet() to start a remote in a forked process rather than being exec'd via
+// SSH. The benefits are the same as hrnProtocolLocalShimInstall().
+void hrnProtocolRemoteShimInstall(const ProtocolServerHandler *const handlerList, const unsigned int handlerListSize);
+void hrnProtocolRemoteShimUninstall(void);
