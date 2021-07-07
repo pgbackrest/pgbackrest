b329b0d51dcb994ca2c1a2aa57869d4f97f27410
David Steele <david@pgmasters.net>
2021-07-07 18:32:43 -0400

    More work.

--- src/command/server/server.c
+++ src/command/server/server.c
@@ -6,6 +6,7 @@ Server Command
 #include "command/server/server.h"
 #include "common/debug.h"
 #include "common/io/tls/server.h"
+#include "common/io/socket/server.h"
 #include "config/config.h"
 
 /**********************************************************************************************************************************/
@@ -14,13 +15,23 @@ cmdServer(void)
 {
     FUNCTION_LOG_VOID(logLevelDebug);
 
+    const String *const host = STRDEF("localhost");
+
     MEM_CONTEXT_TEMP_BEGIN()
     {
         IoServer *tlsServer = tlsServerNew(
-           STRDEF("127.0.0.1"), cfgOptionStr(cfgOptTlsServerKey), cfgOptionStr(cfgOptTlsServerCert),
-           cfgOptionUInt64(cfgOptProtocolTimeout));
+           host, cfgOptionStr(cfgOptTlsServerKey), cfgOptionStr(cfgOptTlsServerCert), cfgOptionUInt64(cfgOptProtocolTimeout));
+        IoServer *socketServer = sckServerNew(host, cfgOptionUInt(cfgOptTlsServerPort), cfgOptionUInt64(cfgOptProtocolTimeout));
+
+        // do
+        // {
+        //     IoSession *socketSession = ioServerAccept(socketServer, NULL);
+        //     (void)socketSession; // !!!
+        // }
+        // while (true);
 
         (void)tlsServer; // !!!
+        (void)socketServer; // !!!
 
 // port 8242
     }
