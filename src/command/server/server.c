68bd6c7a72d0c4f298bc157039698f7900b1aa2c
David Steele <david@pgmasters.net>
2021-10-15 19:12:07 -0400

    Hack to make it work for 32-bit.

--- src/command/server/server.c
+++ src/command/server/server.c
@@ -26,7 +26,7 @@ cmdServer(uint64_t connectionMax)
     {
         IoServer *const tlsServer = tlsServerNew(
            cfgOptionStr(cfgOptTlsServerAddress), cfgOptionStr(cfgOptTlsServerCaFile), cfgOptionStr(cfgOptTlsServerKeyFile),
-           cfgOptionStr(cfgOptTlsServerCertFile), true, cfgOptionUInt64(cfgOptProtocolTimeout));
+           cfgOptionStr(cfgOptTlsServerCertFile), sizeof(size_t) == 8, cfgOptionUInt64(cfgOptProtocolTimeout));
         IoServer *const socketServer = sckServerNew(
             cfgOptionStr(cfgOptTlsServerAddress), cfgOptionUInt(cfgOptTlsServerPort), cfgOptionUInt64(cfgOptProtocolTimeout));
 
