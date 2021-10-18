90f7f11a9f71152185219bbb57bf1de001e3a91b
David Steele <david@pgmasters.net>
2021-10-18 12:22:48 -0400

    Add missing static keywords in test modules.

--- test/src/module/common/errorTest.c
+++ test/src/module/common/errorTest.c
@@ -19,11 +19,11 @@ ERROR_DEFINE(200, TestChildError, TestParent2Error);
 /***********************************************************************************************************************************
 testTryRecurse - test to blow up try stack
 ***********************************************************************************************************************************/
-volatile int testTryRecurseTotal = 0;
-bool testTryRecurseCatch = false;
-bool testTryRecurseFinally = false;
+static volatile int testTryRecurseTotal = 0;
+static bool testTryRecurseCatch = false;
+static bool testTryRecurseFinally = false;
 
-void
+static void
 testTryRecurse(void)
 {
     TRY_BEGIN()
@@ -58,7 +58,7 @@ testErrorHandler(unsigned int tryDepth)
 /***********************************************************************************************************************************
 Test Run
 ***********************************************************************************************************************************/
-void
+static void
 testRun(void)
 {
     FUNCTION_HARNESS_VOID();
