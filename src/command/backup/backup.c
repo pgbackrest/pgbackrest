742edff343be02255ad6319308569e5c72881f71
Stefan Fercot <stefan.fercot@enterprisedb.com>
2021-11-10 10:44:46 +0100

    fix opt display

--- src/command/backup/backup.c
+++ src/command/backup/backup.c
@@ -852,13 +852,13 @@ backupStart(BackupData *backupData)
 
             // When the start-fast option is disabled and db-timeout is smaller than checkpoint_timeout, the command may timeout
             // before the backup actually starts
-            int64_t dbTimeout = cfgOptionInt64(cfgOptDbTimeout) / (int64_t)(MSEC_PER_SEC);
-            if (!cfgOptionBool(cfgOptStartFast) && dbTimeout <= dbCheckpointTimeout(backupData->dbPrimary))
+            if (!cfgOptionBool(cfgOptStartFast) &&
+                cfgOptionInt64(cfgOptDbTimeout) <= (int64_t)(dbCheckpointTimeout(backupData->dbPrimary) * MSEC_PER_SEC))
             {
                 LOG_WARN_FMT(
-                    CFGOPT_START_FAST " is disabled and " CFGOPT_DB_TIMEOUT " (%lds) is smaller than the checkpoint_timeout (%us)"
-                    " reported by the database - timeout may occur before the backup actually starts", dbTimeout,
-                    dbCheckpointTimeout(backupData->dbPrimary));
+                    CFGOPT_START_FAST " is disabled and " CFGOPT_DB_TIMEOUT " (%ss) is smaller than the checkpoint_timeout (%us)"
+                    " reported by the database - timeout may occur before the backup actually starts",
+                    strZ(cfgOptionDisplay(cfgOptDbTimeout)), dbCheckpointTimeout(backupData->dbPrimary));
             }
 
             MEM_CONTEXT_PRIOR_BEGIN()
