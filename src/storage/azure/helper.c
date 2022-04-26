6f2654a5eb0e2cb63a4ff3af187f991817189dca
David Steele <david@pgmasters.net>
2022-04-26 12:53:55 -0400

    Fix leaks in the storage/azure module.
    storageAzureHelper() leaked a few Strings which ended up in a long-lived context.
    
    storageAzureNew() failed to make a copy of the endpoint. This worked because storageAzureHelper() leaked the endpoint into the long-lived parent context.
    
    storageAzureRequest() leaked an HttpRequest.
    
    storageAzureInfo() leaked an HttpResponse.
    
    storageAzurePathRemoveCallback() leaked an HttpResponse.
    
    storageAzureRemove() leaked an HttpResponse.

--- src/storage/azure/helper.c
+++ src/storage/azure/helper.c
@@ -22,56 +22,70 @@ storageAzureHelper(const unsigned int repoIdx, const bool write, StoragePathExpr
 
     ASSERT(cfgOptionIdxStrId(cfgOptRepoType, repoIdx) == STORAGE_AZURE_TYPE);
 
-    // Parse the endpoint url
-    const HttpUrl *const url = httpUrlNewParseP(cfgOptionIdxStr(cfgOptRepoAzureEndpoint, repoIdx), .type = httpProtocolTypeHttps);
-    const String *endpoint = httpUrlHost(url);
-    unsigned int port = httpUrlPort(url);
+    Storage *result = NULL;
 
-    StorageAzureUriStyle uriStyle = (StorageAzureUriStyle)cfgOptionIdxStrId(cfgOptRepoAzureUriStyle, repoIdx);
-
-    // If the host is set then set it as the endpoint. The host option is used to set path-style URIs when working with Azurite.
-    // This was ill-advised, so the uri-style option was added to allow the user to select the URI style used by the server.
-    // Preserve the old behavior when uri-style is defaulted.
-    if (cfgOptionIdxStrNull(cfgOptRepoStorageHost, repoIdx) != NULL)
+    MEM_CONTEXT_TEMP_BEGIN()
     {
-        const HttpUrl *const url = httpUrlNewParseP(cfgOptionIdxStr(cfgOptRepoStorageHost, repoIdx), .type = httpProtocolTypeHttps);
+        // Parse the endpoint url
+        const HttpUrl *const url = httpUrlNewParseP(
+            cfgOptionIdxStr(cfgOptRepoAzureEndpoint, repoIdx), .type = httpProtocolTypeHttps);
+        const String *endpoint = httpUrlHost(url);
+        unsigned int port = httpUrlPort(url);
 
-        endpoint = httpUrlHost(url);
-        port = httpUrlPort(url);
+        StorageAzureUriStyle uriStyle = (StorageAzureUriStyle)cfgOptionIdxStrId(cfgOptRepoAzureUriStyle, repoIdx);
 
-        if (cfgOptionIdxSource(cfgOptRepoAzureUriStyle, repoIdx) == cfgSourceDefault)
-            uriStyle = storageAzureUriStylePath;
-    }
-
-    // Ensure the key is valid base64 when key type is shared
-    const StorageAzureKeyType keyType = (StorageAzureKeyType)cfgOptionIdxStrId(cfgOptRepoAzureKeyType, repoIdx);
-    const String *const key = cfgOptionIdxStr(cfgOptRepoAzureKey, repoIdx);
-
-    if (keyType == storageAzureKeyTypeShared)
-    {
-        TRY_BEGIN()
+        // If the host is set then set it as the endpoint. The host option is used to set path-style URIs when working with Azurite.
+        // This was ill-advised, so the uri-style option was added to allow the user to select the URI style used by the server.
+        // Preserve the old behavior when uri-style is defaulted.
+        if (cfgOptionIdxStrNull(cfgOptRepoStorageHost, repoIdx) != NULL)
         {
-            bufNewDecode(encodeBase64, key);
+            const HttpUrl *const url = httpUrlNewParseP(
+                cfgOptionIdxStr(cfgOptRepoStorageHost, repoIdx), .type = httpProtocolTypeHttps);
+
+            endpoint = httpUrlHost(url);
+            port = httpUrlPort(url);
+
+            if (cfgOptionIdxSource(cfgOptRepoAzureUriStyle, repoIdx) == cfgSourceDefault)
+                uriStyle = storageAzureUriStylePath;
         }
-        CATCH(FormatError)
+
+        // Ensure the key is valid base64 when key type is shared
+        const StorageAzureKeyType keyType = (StorageAzureKeyType)cfgOptionIdxStrId(cfgOptRepoAzureKeyType, repoIdx);
+        const String *const key = cfgOptionIdxStr(cfgOptRepoAzureKey, repoIdx);
+
+        if (keyType == storageAzureKeyTypeShared)
         {
-            THROW_FMT(
-                OptionInvalidValueError, "invalid value for '%s' option: %s\n"
-                "HINT: value must be valid base64 when '%s = " CFGOPTVAL_REPO_AZURE_KEY_TYPE_SHARED_Z "'.",
-                cfgOptionIdxName(cfgOptRepoAzureKey, repoIdx), errorMessage(), cfgOptionIdxName(cfgOptRepoAzureKeyType, repoIdx));
+            TRY_BEGIN()
+            {
+                bufNewDecode(encodeBase64, key);
+            }
+            CATCH(FormatError)
+            {
+                THROW_FMT(
+                    OptionInvalidValueError, "invalid value for '%s' option: %s\n"
+                    "HINT: value must be valid base64 when '%s = " CFGOPTVAL_REPO_AZURE_KEY_TYPE_SHARED_Z "'.",
+                    cfgOptionIdxName(cfgOptRepoAzureKey, repoIdx), errorMessage(),
+                    cfgOptionIdxName(cfgOptRepoAzureKeyType, repoIdx));
+            }
+            TRY_END();
         }
-        TRY_END();
+
+        // If port was specified, overwrite the parsed/default port
+        if (cfgOptionIdxSource(cfgOptRepoStoragePort, repoIdx) != cfgSourceDefault)
+            port = cfgOptionIdxUInt(cfgOptRepoStoragePort, repoIdx);
+
+        MEM_CONTEXT_PRIOR_BEGIN()
+        {
+            result = storageAzureNew(
+                cfgOptionIdxStr(cfgOptRepoPath, repoIdx), write, pathExpressionCallback,
+                cfgOptionIdxStr(cfgOptRepoAzureContainer, repoIdx), cfgOptionIdxStr(cfgOptRepoAzureAccount, repoIdx), keyType, key,
+                STORAGE_AZURE_BLOCKSIZE_MIN, endpoint, uriStyle, port, ioTimeoutMs(), cfgOptionIdxBool(cfgOptRepoStorageVerifyTls,
+                repoIdx), cfgOptionIdxStrNull(cfgOptRepoStorageCaFile, repoIdx),
+                cfgOptionIdxStrNull(cfgOptRepoStorageCaPath, repoIdx));
+        }
+        MEM_CONTEXT_PRIOR_END();
     }
-
-    // If port was specified, overwrite the parsed/default port
-    if (cfgOptionIdxSource(cfgOptRepoStoragePort, repoIdx) != cfgSourceDefault)
-        port = cfgOptionIdxUInt(cfgOptRepoStoragePort, repoIdx);
-
-    Storage *const result = storageAzureNew(
-        cfgOptionIdxStr(cfgOptRepoPath, repoIdx), write, pathExpressionCallback,
-        cfgOptionIdxStr(cfgOptRepoAzureContainer, repoIdx), cfgOptionIdxStr(cfgOptRepoAzureAccount, repoIdx), keyType, key,
-        STORAGE_AZURE_BLOCKSIZE_MIN, endpoint, uriStyle, port, ioTimeoutMs(), cfgOptionIdxBool(cfgOptRepoStorageVerifyTls, repoIdx),
-        cfgOptionIdxStrNull(cfgOptRepoStorageCaFile, repoIdx), cfgOptionIdxStrNull(cfgOptRepoStorageCaPath, repoIdx));
+    MEM_CONTEXT_TEMP_END();
 
     FUNCTION_LOG_RETURN(STORAGE, result);
 }
