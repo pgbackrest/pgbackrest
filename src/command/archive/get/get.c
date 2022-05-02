03c71aa606bc0dc89303b3907e7fcbd83ace79d6
David Steele <david@pgmasters.net>
2022-05-02 08:49:13 -0400

    Add hint to check the log on archive-get/archive-push async error.
    If this error is thrown rather than a specific error returned from the async process, it means the async process is unable to write the status files for some reason and the only way to get the error is out of the async log.
    
    This hint includes the exact async log path and name to make finding errors easier.

--- src/command/archive/get/get.c
+++ src/command/archive/get/get.c
@@ -20,6 +20,7 @@ Archive Get Command
 #include "common/wait.h"
 #include "config/config.h"
 #include "config/exec.h"
+#include "config/load.h"
 #include "info/infoArchive.h"
 #include "postgres/interface.h"
 #include "protocol/helper.h"
@@ -776,8 +777,11 @@ cmdArchiveGet(void)
                 if (!foundOk)
                 {
                     THROW_FMT(
-                        ArchiveTimeoutError, "unable to get WAL file '%s' from the archive asynchronously after %s second(s)",
-                        strZ(walSegment), strZ(strNewDbl((double)cfgOptionInt64(cfgOptArchiveTimeout) / MSEC_PER_SEC)));
+                        ArchiveTimeoutError,
+                        "unable to get WAL file '%s' from the archive asynchronously after %s second(s)\n"
+                        "HINT: check '%s' for errors.",
+                        strZ(walSegment), strZ(strNewDbl((double)cfgOptionInt64(cfgOptArchiveTimeout) / MSEC_PER_SEC)),
+                        strZ(cfgLoadLogFileName(cfgCmdRoleAsync)));
                 }
                 // Else report that the WAL segment could not be found
                 else
