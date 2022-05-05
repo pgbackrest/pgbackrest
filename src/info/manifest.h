b6bfd9f99d74b1f5e631cf6462b68342589d2b80
David Steele <david@pgmasters.net>
2022-05-05 09:20:49 -0400

    Strip extensions from history manifest before showing in error message.
    In cases where clock skew or timezone issues are preventing backup label generation the user could see an error like this:
    
    new backup label '20220504-152308F' is not later than latest backup label '20220504-222042F_20220504-222141I.manifest.gz'
    
    This will happen if the most recent label is drawn from the history. It is cleaner (and probably less confusing) to strip off the extensions so the user sees:
    
    new backup label '20220504-152308F' is not later than latest backup label '20220504-222042F_20220504-222141I'

--- src/info/manifest.h
+++ src/info/manifest.h
@@ -13,7 +13,8 @@ nothing is missing or corrupt.  It is also useful for reporting, e.g. size of ba
 /***********************************************************************************************************************************
 Constants
 ***********************************************************************************************************************************/
-#define BACKUP_MANIFEST_FILE                                        "backup.manifest"
+#define BACKUP_MANIFEST_EXT                                         ".manifest"
+#define BACKUP_MANIFEST_FILE                                        "backup" BACKUP_MANIFEST_EXT
     STRING_DECLARE(BACKUP_MANIFEST_FILE_STR);
 
 #define MANIFEST_PATH_BUNDLE                                        "bundle"
