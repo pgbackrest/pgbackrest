f452cfe32c37be0179719b2de7e4694f56197e96
Reid Thompson <jreidthompson@nc.rr.com>
2022-05-05 20:07:16 -0400

    refactor out LF_STR

--- src/command/verify/verify.c
+++ src/command/verify/verify.c
@@ -1510,7 +1510,7 @@ verifyProcess(const bool verboseText)
             }
             CATCH_ANY()
             {
-                strCatFmt(resultStr, "%s%s", strZ(LF_STR), errorMessage());
+                strCatFmt(resultStr, "\n%s", errorMessage());
                 errorTotal++;
             }
             TRY_END();
