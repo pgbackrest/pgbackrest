b4c1ca7b807a906d42d540307da3603052c4a674
David Steele <david@pgmasters.net>
2022-05-09 14:19:05 -0400

    Split 32-bit CI tests.
    This helps rebalance some of the tests that are running long, i.e. d9 and u20.
    
    I would be better to move more PostgreSQL versions to d9, but the base VM does not contain more versions. New minor versions will be out later in the week so that seems a better time to be rebuilding containers.

--- test/lib/pgBackRestTest/Common/VmTest.pm
+++ test/lib/pgBackRestTest/Common/VmTest.pm
@@ -205,6 +205,8 @@ my $oyVm =
         &VM_DB_TEST =>
         [
             PG_VERSION_90,
+            PG_VERSION_91,
+            PG_VERSION_92,
         ],
     },
 
@@ -280,8 +282,6 @@ my $oyVm =
 
         &VM_DB_TEST =>
         [
-            PG_VERSION_91,
-            PG_VERSION_92,
             PG_VERSION_93,
             PG_VERSION_94,
             PG_VERSION_95,
