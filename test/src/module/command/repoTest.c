pgbackrest/pgbackrest

commit 5ae84d5e474e95dcc00c174aa5111815a5a70ba5

Improve path validation for repo-* commands.
Check for invalid path in repo-* commands. Perform path validation and throw an error when appropriate. Path may not contain '//'. Strip trailing '/' from path. Absolute path must fall under repo path.

doc/xml/release.xml
@@ -43,6 +43,17 @@
                     </release-item>
 
                     <release-item>
+                        <github-pull-request id="1697"/>
+
+                        <release-item-contributor-list>
+                            <release-item-contributor id="reid.thompson"/>
+                            <release-item-reviewer id="david.steele"/>
+                        </release-item-contributor-list>
+
+                        <p>Improve path validation for <cmd>repo-*</cmd> commands.</p>
+                    </release-item>
+
+                    <release-item>
                         <github-issue id="1608"/>
                         <github-pull-request id="1676"/>
 

src/command/repo/common.c
@@ -1,0 +1,63 @@
+/***********************************************************************************************************************************
+Common Functions and Definitions for Repo Commands
+***********************************************************************************************************************************/
+#include "build.auto.h"
+
+#include "command/repo/common.h"
+#include "common/debug.h"
+#include "config/config.h"
+
+/**********************************************************************************************************************************/
+String *
+repoPathIsValid(const String *path)
+{
+    FUNCTION_LOG_BEGIN(logLevelDebug);
+        FUNCTION_LOG_PARAM(STRING, path);
+    FUNCTION_LOG_END();
+
+    ASSERT(path != NULL);
+
+    String *result = NULL;
+
+    MEM_CONTEXT_TEMP_BEGIN()
+    {
+        // Make sure there are no occurrences of //
+        if (strstr(strZ(path), "//") != NULL)
+            THROW_FMT(ParamInvalidError, "path '%s' cannot contain //", strZ(path));
+
+        // Make sure the path does not end with a slash
+        if (strEndsWith(path, FSLASH_STR))
+            path = strPath(path);
+
+        // Validate absolute paths
+        if (strBeginsWith(path, FSLASH_STR))
+        {
+            // Check that the file path begins with the repo path
+            if (!strBeginsWith(path, cfgOptionStr(cfgOptRepoPath)))
+            {
+                THROW_FMT(
+                    ParamInvalidError, "absolute path '%s' is not in base path '%s'", strZ(path),
+                    strZ(cfgOptionDisplay(cfgOptRepoPath)));
+            }
+
+            MEM_CONTEXT_PRIOR_BEGIN()
+            {
+                // Get the relative part of the file
+                result = strSub(
+                    path, strEq(cfgOptionStr(cfgOptRepoPath), FSLASH_STR) ? 1 : strSize(cfgOptionStr(cfgOptRepoPath)) + 1);
+            }
+            MEM_CONTEXT_PRIOR_END();
+        }
+        else
+        {
+            MEM_CONTEXT_PRIOR_BEGIN()
+            {
+                result = strDup(path);
+            }
+            MEM_CONTEXT_PRIOR_END();
+        }
+    }
+    MEM_CONTEXT_TEMP_END();
+
+    FUNCTION_LOG_RETURN(STRING, result);
+}

test/src/module/command/repoTest.c
@@ -165,7 +165,18 @@ testRun(void)
         HRN_CFG_LOAD(cfgCmdRepoLs, argListTmp);
 
         TEST_ERROR(
-            storageListRender(ioBufferWriteNew(output)), AssertError, "absolute path '/' is not in base path '" TEST_PATH "/repo'");
+            storageListRender(ioBufferWriteNew(output)), ParamInvalidError,
+            "absolute path '/' is not in base path '" TEST_PATH "/repo'");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("error on //");
+
+        argListTmp = strLstDup(argList);
+        strLstAddZ(argListTmp, "bbb//");
+        HRN_CFG_LOAD(cfgCmdRepoLs, argListTmp);
+
+        TEST_ERROR(
+            storageListRender(ioBufferWriteNew(output)), ParamInvalidError, "path 'bbb//' cannot contain //");
 
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("subdirectory");
@@ -594,7 +605,7 @@ testRun(void)
 
         writeBuffer = bufNew(0);
         TEST_ERROR(
-            storageGetProcess(ioBufferWriteNew(writeBuffer)), OptionInvalidValueError,
+            storageGetProcess(ioBufferWriteNew(writeBuffer)), ParamInvalidError,
             "absolute path '/somewhere/" INFO_ARCHIVE_FILE "' is not in base path '" TEST_PATH "/repo'");
 
         // -------------------------------------------------------------------------------------------------------------------------

src/Makefile.in
@@ -70,6 +70,7 @@ SRCS = \
 	command/control/start.c \
 	command/control/stop.c \
 	command/local/local.c \
+	command/repo/common.c \
 	command/repo/create.c \
 	command/repo/get.c \
 	command/repo/ls.c \

src/command/repo/common.h
@@ -1,0 +1,15 @@
+/***********************************************************************************************************************************
+Repo Command Common
+***********************************************************************************************************************************/
+#ifndef COMMAND_REPO_COMMON_H
+#define COMMAND_REPO_COMMON_H
+
+#include "common/type/string.h"
+
+/***********************************************************************************************************************************
+Functions and Definitions
+***********************************************************************************************************************************/
+// Path cannot contain //. Strip trailing /. Absolute path must fall under repo path. Throw error or return validated relative path.
+String *repoPathIsValid(const String *path);
+
+#endif

src/command/repo/get.c
@@ -5,6 +5,7 @@ Repository Get Command
 
 #include <unistd.h>
 
+#include "command/repo/common.h"
 #include "common/crypto/cipherBlock.h"
 #include "common/debug.h"
 #include "common/io/fdWrite.h"
@@ -40,20 +41,8 @@ storageGetProcess(IoWrite *destination)
 
     MEM_CONTEXT_TEMP_BEGIN()
     {
-        // If the source path is absolute then get the relative part of the file
-        if (strBeginsWith(file, FSLASH_STR))
-        {
-            // Check that the file path begins with the repo path
-            if (!strBeginsWith(file, cfgOptionStr(cfgOptRepoPath)))
-            {
-                THROW_FMT(
-                    OptionInvalidValueError, "absolute path '%s' is not in base path '%s'", strZ(file),
-                    strZ(cfgOptionDisplay(cfgOptRepoPath)));
-            }
-
-            // Get the relative part of the file
-            file = strSub(file, strEq(cfgOptionStr(cfgOptRepoPath), FSLASH_STR) ? 1 : strSize(cfgOptionStr(cfgOptRepoPath)) + 1);
-        }
+        // Is path valid for repo?
+        file = repoPathIsValid(file);
 
         // Create new file read
         IoRead *source = storageReadIo(storageNewReadP(storageRepo(), file, .ignoreMissing = cfgOptionBool(cfgOptIgnoreMissing)));

src/command/repo/ls.c
@@ -5,6 +5,7 @@ Repository List Command
 
 #include <unistd.h>
 
+#include "command/repo/common.h"
 #include "common/debug.h"
 #include "common/io/fdWrite.h"
 #include "common/log.h"
@@ -124,14 +125,9 @@ storageListRender(IoWrite *write)
     // Get path
     const String *path = NULL;
 
+    // Get and validate if path is valid for repo
     if (strLstSize(cfgCommandParam()) == 1)
-    {
-        path = strLstGet(cfgCommandParam(), 0);
-
-        // Make sure the path does not end with a slash unless it is only /
-        if (strEndsWith(path, FSLASH_STR) && strSize(path) > 1)
-            path = strPath(path);
-    }
+        path = repoPathIsValid(strLstGet(cfgCommandParam(), 0));
     else if (strLstSize(cfgCommandParam()) > 1)
         THROW(ParamInvalidError, "only one path may be specified");
 

src/command/repo/put.c
@@ -5,6 +5,7 @@ Repository Put Command
 
 #include <unistd.h>
 
+#include "command/repo/common.h"
 #include "common/crypto/cipherBlock.h"
 #include "common/debug.h"
 #include "common/io/fdRead.h"
@@ -34,6 +35,9 @@ storagePutProcess(IoRead *source)
 
     MEM_CONTEXT_TEMP_BEGIN()
     {
+        // Is path valid for repo?
+        file = repoPathIsValid(file);
+
         StorageWrite *destination = storageNewWriteP(storageRepoWrite(), file);
 
         // Add encryption if needed

commit 19dd015d589ed3efd31a2a238249a150fb3748c1

Fix issues in improved path validation for repo-* commands.
If the user requested the exact repo path then strSub() would be passed an invalid start value leading to an assertion:

$ pgbackrest --stanza=test repo-ls /var/lib/pgbackrest
ASSERT: [025]: start <= this->pub.size (on dev builds)
ASSERT: [025]: string size must be <= 1073741824 bytes (on prod builds)

Fix this by checking if the requested path exactly equals the repo path and returning an empty relative path in this case.

Another issue was that invalid subpaths were not detected if they started with the repo path. For example, /var/lib/pgbackrestsub would not generate an error if the repo path was /var/lib/pgbackrest. Fix this by explictly checking for a / between the repo path and the subpath. This also requires special handling when the repo path is /.

This is not a live bug since the issues were found in an unreleased feature introduced in 5ae84d5.

doc/xml/release.xml
@@ -188,7 +188,12 @@
                     </release-item>
 
                     <release-item>
-                        <github-pull-request id="1697"/>
+                        <commit subject="Improve path validation for repo-* commands.">
+                            <github-pull-request id="1697"/>
+                        </commit>
+                        <commit subject="Fix issues in improved path validation for repo-* commands.">
+                            <github-pull-request id="1751"/>
+                        </commit>
 
                         <release-item-contributor-list>
                             <release-item-contributor id="reid.thompson"/>

src/command/repo/common.c
@@ -32,21 +32,34 @@ repoPathIsValid(const String *path)
         // Validate absolute paths
         if (strBeginsWith(path, FSLASH_STR))
         {
-            // Check that the file path begins with the repo path
-            if (!strBeginsWith(path, cfgOptionStr(cfgOptRepoPath)))
+            // If the path is exactly equal to the repo path then the relative path is empty
+            if (strEq(path, cfgOptionStr(cfgOptRepoPath)))
             {
-                THROW_FMT(
-                    ParamInvalidError, "absolute path '%s' is not in base path '%s'", strZ(path),
-                    strZ(cfgOptionDisplay(cfgOptRepoPath)));
+                MEM_CONTEXT_PRIOR_BEGIN()
+                {
+                    result = strNew();
+                }
+                MEM_CONTEXT_PRIOR_END();
             }
+            // Else check that the file path begins with the repo path
+            else
+            {
+                if (!strEq(cfgOptionStr(cfgOptRepoPath), FSLASH_STR) &&
+                    !strBeginsWith(path, strNewFmt("%s/", strZ(cfgOptionStr(cfgOptRepoPath)))))
+                {
+                    THROW_FMT(
+                        ParamInvalidError, "absolute path '%s' is not in base path '%s'", strZ(path),
+                        strZ(cfgOptionDisplay(cfgOptRepoPath)));
+                }
 
-            MEM_CONTEXT_PRIOR_BEGIN()
-            {
-                // Get the relative part of the file
-                result = strSub(
-                    path, strEq(cfgOptionStr(cfgOptRepoPath), FSLASH_STR) ? 1 : strSize(cfgOptionStr(cfgOptRepoPath)) + 1);
+                MEM_CONTEXT_PRIOR_BEGIN()
+                {
+                    // Get the relative part of the path/file
+                    result = strSub(
+                        path, strEq(cfgOptionStr(cfgOptRepoPath), FSLASH_STR) ? 1 : strSize(cfgOptionStr(cfgOptRepoPath)) + 1);
+                }
+                MEM_CONTEXT_PRIOR_END();
             }
-            MEM_CONTEXT_PRIOR_END();
         }
         else
         {

test/src/module/command/repoTest.c
@@ -97,6 +97,18 @@ testRun(void)
         cfgOptionSet(cfgOptFilter, cfgSourceParam, NULL);
 
         // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("exact repo path");
+
+        StringList *argListTmp = strLstDup(argList);
+        strLstAddZ(argListTmp, TEST_PATH "/repo");
+        HRN_CFG_LOAD(cfgCmdRepoLs, argListTmp);
+
+        output = bufNew(0);
+        cfgOptionSet(cfgOptOutput, cfgSourceParam, VARUINT64(CFGOPTVAL_OUTPUT_TEXT));
+        TEST_RESULT_VOID(storageListRender(ioBufferWriteNew(output)), "empty directory (text)");
+        TEST_RESULT_STR_Z(strNewBuf(output), "", "check output");
+
+        // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("add path and file");
 
         cfgOptionSet(cfgOptSort, cfgSourceParam, VARUINT64(CFGOPTVAL_SORT_ASC));
@@ -160,13 +172,24 @@ testRun(void)
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("error on /");
 
-        StringList *argListTmp = strLstDup(argList);
+        argListTmp = strLstDup(argList);
         strLstAddZ(argListTmp, "/");
         HRN_CFG_LOAD(cfgCmdRepoLs, argListTmp);
 
         TEST_ERROR(
             storageListRender(ioBufferWriteNew(output)), ParamInvalidError,
             "absolute path '/' is not in base path '" TEST_PATH "/repo'");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("error on path that starts with repo path");
+
+        argListTmp = strLstDup(argList);
+        strLstAddZ(argListTmp, TEST_PATH "/reposub");
+        HRN_CFG_LOAD(cfgCmdRepoLs, argListTmp);
+
+        TEST_ERROR(
+            storageListRender(ioBufferWriteNew(output)), ParamInvalidError,
+            "absolute path '" TEST_PATH "/reposub' is not in base path '" TEST_PATH "/repo'");
 
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("error on //");