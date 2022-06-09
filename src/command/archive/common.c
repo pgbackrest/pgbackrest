1a00ab10033e9ac6e6f797627567e8b91e143432
Andrey Sokolov <a.sokolov@arenadata.io>
2022-06-09 14:34:11 +0300

    Fix compile error when DEBUG_EXEC_TIME is defined without DEBUG.
    If DEBUG is not defined then the ASSERT() macro expands to nothing. In this case the timeBegin variable is never used and a compilation error occurs.
    
    This test should work without DEBUG defined so use CHECK() instead of ASSERT().

--- src/command/archive/common.c
+++ src/command/archive/common.c
@@ -286,7 +286,7 @@ archiveAsyncExec(ArchiveMode archiveMode, const StringList *commandExec)
     // If the process does not exit immediately then something probably went wrong with the double fork.  It's possible that this
     // test will fail on very slow systems so it may need to be tuned.  The idea is to make sure that the waitpid() above is not
     // waiting on the async process.
-    ASSERT(timeMSec() - timeBegin < 10);
+    CHECK(AssertError, timeMSec() - timeBegin < 10, "the process does not exit immediately");
 #endif
 
     FUNCTION_LOG_RETURN_VOID();
