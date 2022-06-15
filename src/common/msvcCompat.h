bd9fbd869d78f6a5fc2036a29b83451ede7aebbe
Samuel Bassaly <shkshk90@gmail.com>
2022-06-15 23:02:08 +0200

    Port files required for _error_ and _stack-trace_ test

---
+++ src/common/msvcCompat.h
@@ -0,0 +1,38 @@
+/***********************************************************************************************************************************
+MSVC Comptibility Header
+
+Defines the necessary or missing macros / typedefs, that are available on POSIX plaforms.
+
+***********************************************************************************************************************************/
+#ifndef MSVC_COMPAT_H
+#define MSVC_COMPAT_H
+
+#ifndef _MSC_VER
+    #error "Only MSVC is supported on Windows platforms."
+#endif
+
+#ifndef _WIN64
+    #error "Only 64-bit builds are supported on Windows platforms."
+#endif
+
+/***********************************************************************************************************************************
+Error-silencing macros
+***********************************************************************************************************************************/
+
+// Fix for the absence of __attribute__ in msvc
+#define __attribute__(...)
+
+// Silence noisy warnings about non-secure CRT functions
+#define _CRT_SECURE_NO_WARNINGS
+
+// Silence noisy warnings about old Winsock API
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+/***********************************************************************************************************************************
+Missing types
+***********************************************************************************************************************************/
+typedef __int64 ssize_t;
+
+typedef unsigned short mode_t;
+
+#endif
