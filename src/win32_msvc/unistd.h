22955bd4521d115205ab0c600593a5be7c7da16a
Samuel Bassaly <shkshk90@gmail.com>
2022-06-17 12:11:05 +0200

    Add <unistd.h>

---
+++ src/win32_msvc/unistd.h
@@ -0,0 +1,23 @@
+/***********************************************************************************************************************************
+MSVC compatibility header for <unistd.h>
+***********************************************************************************************************************************/
+#ifndef UNISTD_WIN32_H
+#define UNISTD_WIN32_H
+
+#if defined(_WIN64) && defined(_MSC_VER)
+
+// For io functions
+#include <io.h>
+
+// Values from https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/fileno?view=msvc-170
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+#define write _write
+#define open  _open
+#define close _close
+#define lseek _lseek
+
+#endif
+
+#endif
