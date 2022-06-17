f37bc8bcd08c6ac69d9df76e207b711242e3ddfe
Samuel Bassaly <shkshk90@gmail.com>
2022-06-17 16:53:36 +0200

    Add target type/{json + stat}, remove xml due to dependency

--- src/common/type/json.c
+++ src/common/type/json.c
@@ -167,7 +167,16 @@ jsonReadTypeNext(JsonRead *const this)
 
         // Integer
         case '-':
-        case '0' ... '9':
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
             result = jsonTypeNumber;
             break;
 
