a4c320881924379482d1014d8405ef403329148f
Samuel Bassaly <shkshk90@gmail.com>
2022-06-20 20:40:21 +0200

    Disable user.c functions

--- src/common/user.c
+++ src/common/user.c
@@ -3,8 +3,15 @@ System User/Group Management
 ***********************************************************************************************************************************/
 #include "build.auto.h"
 
-#include <grp.h>
-#include <pwd.h>
+#ifndef _WIN32
+    #include <grp.h>
+    #include <pwd.h>
+#else
+    #include <Windows.h>
+    #include <Lmcons.h>
+    #include <tchar.h>
+#endif
+
 #include <unistd.h>
 
 #include "common/debug.h"
@@ -27,6 +34,8 @@ static struct
 } userLocalData;
 
 /**********************************************************************************************************************************/
+
+#ifndef _WIN32
 static void
 userInitInternal(void)
 {
@@ -172,3 +181,161 @@ userRoot(void)
     FUNCTION_TEST_VOID();
     FUNCTION_TEST_RETURN(BOOL, userLocalData.userRoot);
 }
+#else
+static void
+userInitInternal(void)
+{
+    FUNCTION_TEST_VOID();
+
+    MEM_CONTEXT_BEGIN(memContextTop())
+    {
+        MEM_CONTEXT_NEW_BEGIN(UserLocalData, .childQty = MEM_CONTEXT_QTY_MAX)
+        {
+            char buffer[UNLEN + 1];
+            DWORD bufferSize;
+
+            GetUserNameA(buffer, bufferSize);
+
+            userLocalData.memContext = MEM_CONTEXT_NEW();
+
+            userLocalData.userId = (uid_t)-1;
+            userLocalData.userName = strNewZN(buffer, bufferSize);
+            userLocalData.userRoot = userLocalData.userId == 0;
+
+            userLocalData.groupId = (gid_t)-1;
+            userLocalData.groupName = NULL;
+        }
+        MEM_CONTEXT_NEW_END();
+    }
+    MEM_CONTEXT_END();
+
+    FUNCTION_TEST_RETURN_VOID();
+}
+
+void
+userInit(void)
+{
+    FUNCTION_TEST_VOID();
+
+    if (!userLocalData.memContext)
+        userInitInternal();
+
+    FUNCTION_TEST_RETURN_VOID();
+}
+
+/**********************************************************************************************************************************/
+gid_t
+groupId(void)
+{
+    FUNCTION_TEST_VOID();
+    FUNCTION_TEST_RETURN_TYPE(gid_t, userLocalData.groupId);
+}
+
+/**********************************************************************************************************************************/
+gid_t
+groupIdFromName(const String *groupName)
+{
+    FUNCTION_TEST_BEGIN();
+    FUNCTION_TEST_PARAM(STRING, groupName);
+    FUNCTION_TEST_END();
+
+    //if (groupName != NULL)
+    //{
+    //    struct group *groupData = getgrnam(strZ(groupName));
+
+    //    if (groupData != NULL)
+    //        FUNCTION_TEST_RETURN_TYPE(gid_t, groupData->gr_gid);
+    //}
+
+    // group name is always NULL
+    // getgrnam is not available on Windows
+
+    FUNCTION_TEST_RETURN_TYPE(gid_t, (gid_t)-1);
+}
+
+/**********************************************************************************************************************************/
+const String *
+groupName(void)
+{
+    FUNCTION_TEST_VOID();
+    FUNCTION_TEST_RETURN_CONST(STRING, userLocalData.groupName);
+}
+
+/**********************************************************************************************************************************/
+String *
+groupNameFromId(gid_t groupId)
+{
+    FUNCTION_TEST_BEGIN();
+    FUNCTION_TEST_PARAM(UINT, groupId);
+    FUNCTION_TEST_END();
+
+    //struct group *groupData = getgrgid(groupId);
+
+    //if (groupData != NULL)
+    //    FUNCTION_TEST_RETURN(STRING, strNewZ(groupData->gr_name));
+
+    // group data is always NULL
+    // getgrgid is not available on Windows
+
+    FUNCTION_TEST_RETURN(STRING, NULL);
+}
+
+/**********************************************************************************************************************************/
+uid_t
+userId(void)
+{
+    FUNCTION_TEST_VOID();
+    FUNCTION_TEST_RETURN_TYPE(uid_t, userLocalData.userId);
+}
+
+/**********************************************************************************************************************************/
+uid_t
+userIdFromName(const String *userName)
+{
+    FUNCTION_TEST_BEGIN();
+    FUNCTION_TEST_PARAM(STRING, userName);
+    FUNCTION_TEST_END();
+
+    //if (userName != NULL)
+    //{
+    //    struct passwd *userData = getpwnam(strZ(userName));
+
+    //    if (userData != NULL)
+    //        FUNCTION_TEST_RETURN_TYPE(uid_t, userData->pw_uid);
+    //}
+
+    FUNCTION_TEST_RETURN_TYPE(uid_t, (uid_t)-1);
+}
+
+/**********************************************************************************************************************************/
+const String *
+userName(void)
+{
+    FUNCTION_TEST_VOID();
+    FUNCTION_TEST_RETURN_CONST(STRING, userLocalData.userName);
+}
+
+/**********************************************************************************************************************************/
+String *
+userNameFromId(uid_t userId)
+{
+    FUNCTION_TEST_BEGIN();
+    FUNCTION_TEST_PARAM(UINT, userId);
+    FUNCTION_TEST_END();
+
+    //struct passwd *userData = getpwuid(userId);
+
+    //if (userData != NULL)
+    //    FUNCTION_TEST_RETURN(STRING, strNewZ(userData->pw_name));
+
+    FUNCTION_TEST_RETURN(STRING, NULL);
+}
+
+/**********************************************************************************************************************************/
+bool
+userRoot(void)
+{
+    FUNCTION_TEST_VOID();
+    FUNCTION_TEST_RETURN(BOOL, userLocalData.userRoot);
+}
+#endif 
