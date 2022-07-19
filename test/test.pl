8fdeed780787a2c6df6672f5d9c75fb7b5b613bc
David Steele <david@pgmasters.net>
2022-07-19 17:57:13 -0400

    Copy repository links as files for testing.
    This makes the test code a bit simpler where we are listing a path but not following links.
    
    Links in the repository can be used for testing but should never be committed to the main branch.

--- test/test.pl
+++ test/test.pl
@@ -501,7 +501,7 @@ eval
         # Copy the repo
         executeTest(
             "git -C ${strBackRestBase} ls-files -c --others --exclude-standard |" .
-                " rsync -rtW --delete --files-from=- --exclude=test/result" .
+                " rsync -rLtW --delete --files-from=- --exclude=test/result" .
                 # This option is not supported on MacOS. The eventual plan is to remove the need for it.
                 (trim(`uname`) ne 'Darwin' ? ' --ignore-missing-args' : '') .
                 " ${strBackRestBase}/ ${strRepoCachePath}");
@@ -784,7 +784,7 @@ eval
                             "bash -c 'git clone https://salsa.debian.org/postgresql/pgbackrest.git /root/package-src 2>&1'");
 
                         executeTest(
-                            "rsync -r --exclude=.vagrant --exclude=.git --exclude=test/result ${strBackRestBase}/" .
+                            "rsync -rL --exclude=.vagrant --exclude=.git --exclude=test/result ${strBackRestBase}/" .
                                 " ${strBuildPath}/");
                         executeTest(
                             ($strVm ne VM_NONE ? "docker exec -i test-build " : '') .
