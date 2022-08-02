9b5e8c93390ac5bb9fa55f936993ea9219a92f2d
David Steele <david@pgmasters.net>
2022-08-02 16:54:10 -0400

    New interface that can support retries.

--- src/storage/read.c
+++ src/storage/read.c
@@ -14,7 +14,8 @@ Object type
 struct StorageRead
 {
     StorageReadPub pub;                                             // Publicly accessible variables
-    void *driver;
+    void *driver;                                                   // Driver
+    IoRead *io;                                                     // Driver read interface
 };
 
 /***********************************************************************************************************************************
@@ -25,9 +26,107 @@ Macros for function logging
 #define FUNCTION_LOG_STORAGE_READ_INTERFACE_FORMAT(value, buffer, bufferSize)                                                      \
     objToLog(&value, "StorageReadInterface", buffer, bufferSize)
 
+/***********************************************************************************************************************************
+Open the file
+***********************************************************************************************************************************/
+static bool
+storageReadOpen(THIS_VOID)
+{
+    THIS(StorageRead);
+
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(STORAGE_READ, this);
+    FUNCTION_LOG_END();
+
+    ASSERT(this != NULL);
+
+    FUNCTION_LOG_RETURN(BOOL, ioReadOpen(this->io));
+}
+
+/***********************************************************************************************************************************
+Read from a file and retry when there is a read failure
+***********************************************************************************************************************************/
+static size_t
+storageRead(THIS_VOID, Buffer *buffer, bool block)
+{
+    THIS(StorageRead);
+
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(STORAGE_READ, this);
+        FUNCTION_LOG_PARAM(BUFFER, buffer);
+        FUNCTION_LOG_PARAM(BOOL, block);
+    FUNCTION_LOG_END();
+
+    ASSERT(this != NULL);
+
+    FUNCTION_LOG_RETURN(SIZE, ioRead(this->io, buffer));
+}
+
+/***********************************************************************************************************************************
+Close the file
+***********************************************************************************************************************************/
+static void
+storageReadClose(THIS_VOID)
+{
+    THIS(StorageRead);
+
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(STORAGE_READ, this);
+    FUNCTION_LOG_END();
+
+    ASSERT(this != NULL);
+
+    ioReadClose(this->io);
+
+    FUNCTION_LOG_RETURN_VOID();
+}
+
+/***********************************************************************************************************************************
+Has file reached EOF?
+***********************************************************************************************************************************/
+static bool
+storageReadEof(THIS_VOID)
+{
+    THIS(StorageRead);
+
+    FUNCTION_TEST_BEGIN();
+        FUNCTION_TEST_PARAM(STORAGE_READ, this);
+    FUNCTION_TEST_END();
+
+    ASSERT(this != NULL);
+
+    FUNCTION_TEST_RETURN(BOOL, ioReadEof(this->io));
+}
+
+/***********************************************************************************************************************************
+Get file descriptor
+***********************************************************************************************************************************/
+static int
+storageReadFd(const THIS_VOID)
+{
+    THIS(const StorageRead);
+
+    FUNCTION_TEST_BEGIN();
+        FUNCTION_TEST_PARAM(STORAGE_READ, this);
+    FUNCTION_TEST_END();
+
+    ASSERT(this != NULL);
+
+    FUNCTION_TEST_RETURN(INT, ioReadFd(this->io));
+}
+
 /**********************************************************************************************************************************/
+static const IoReadInterface storageIoReadInterface =
+{
+    .open = storageReadOpen,
+    .read = storageRead,
+    .close = storageReadClose,
+    .eof = storageReadEof,
+    .fd = storageReadFd,
+};
+
 StorageRead *
-storageReadNew(void *driver, const StorageReadInterface *interface)
+storageReadNew(void *const driver, const StorageReadInterface *const interface)
 {
     FUNCTION_LOG_BEGIN(logLevelTrace);
         FUNCTION_LOG_PARAM_P(VOID, driver);
@@ -47,8 +146,9 @@ storageReadNew(void *driver, const StorageReadInterface *interface)
         {
             .memContext = memContextCurrent(),
             .interface = interface,
-            .io = ioReadNew(driver, interface->ioInterface),
+            .io = ioReadNew(this, storageIoReadInterface),
         },
+        .io = ioReadNew(driver, interface->ioInterface),
         .driver = driver,
     };
 
