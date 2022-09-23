deb876a48ba5930c106ff796c3873c670221a2c0
David Steele <david@pgmasters.net>
2022-09-23 12:59:31 -0700

    Use larger buffers.

--- test/src/command/test/build.c
+++ test/src/command/test/build.c
@@ -349,7 +349,9 @@ testBldUnit(TestBuild *const this)
             MESON_COMMENT_BLOCK "\n"
             "configure_file(output: 'build.auto.h', configuration: configuration)\n"
             "\n"
-            "add_global_arguments('-DERROR_MESSAGE_BUFFER_SIZE=131072', language : 'c')\n");
+            "# Use large buffer sizes to capture large error/log outputs\n"
+            "add_global_arguments('-DERROR_MESSAGE_BUFFER_SIZE=262144', language : 'c')\n"
+            "add_global_arguments('-DLOG_BUFFER_SIZE=262144', language : 'c')\n");
 
         // Configure features
         if (module->feature != NULL)
