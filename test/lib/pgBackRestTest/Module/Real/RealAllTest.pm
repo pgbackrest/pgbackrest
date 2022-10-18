7ec440b2b98e614461ef78fdbf46052f1a3b9240
David Steele <david@pgmasters.net>
2022-10-18 17:49:31 +1300

    Update tests.

--- test/lib/pgBackRestTest/Module/Real/RealAllTest.pm
+++ test/lib/pgBackRestTest/Module/Real/RealAllTest.pm
@@ -52,19 +52,19 @@ sub run
 
     foreach my $rhRun
     (
-        {pg => '9.0', dst => HOST_DB_PRIMARY, tls => 0, stg =>   GCS, enc => 1, cmp =>  BZ2, rt => 2, bnd => 1, bi => 0},
-        {pg => '9.1', dst => HOST_DB_STANDBY, tls => 1, stg =>   GCS, enc => 0, cmp =>   GZ, rt => 1, bnd => 0, bi => 0},
-        {pg => '9.2', dst => HOST_DB_STANDBY, tls => 0, stg => POSIX, enc => 1, cmp => NONE, rt => 1, bnd => 1, bi => 0},
-        {pg => '9.3', dst =>     HOST_BACKUP, tls => 0, stg => AZURE, enc => 0, cmp => NONE, rt => 2, bnd => 0, bi => 0},
-        {pg => '9.4', dst => HOST_DB_STANDBY, tls => 0, stg => POSIX, enc => 1, cmp =>  LZ4, rt => 1, bnd => 1, bi => 0},
-        {pg => '9.5', dst =>     HOST_BACKUP, tls => 1, stg =>    S3, enc => 0, cmp =>  BZ2, rt => 1, bnd => 0, bi => 0},
-        {pg => '9.6', dst =>     HOST_BACKUP, tls => 0, stg => POSIX, enc => 0, cmp => NONE, rt => 2, bnd => 1, bi => 1},
-        {pg =>  '10', dst => HOST_DB_STANDBY, tls => 1, stg =>    S3, enc => 1, cmp =>   GZ, rt => 2, bnd => 0, bi => 0},
-        {pg =>  '11', dst =>     HOST_BACKUP, tls => 1, stg => AZURE, enc => 0, cmp =>  ZST, rt => 2, bnd => 1, bi => 0},
-        {pg =>  '12', dst =>     HOST_BACKUP, tls => 0, stg =>    S3, enc => 1, cmp =>  LZ4, rt => 1, bnd => 0, bi => 0},
-        {pg =>  '13', dst => HOST_DB_STANDBY, tls => 1, stg =>   GCS, enc => 0, cmp =>  ZST, rt => 1, bnd => 1, bi => 0},
-        {pg =>  '14', dst =>     HOST_BACKUP, tls => 0, stg => POSIX, enc => 1, cmp =>  LZ4, rt => 2, bnd => 0, bi => 0},
-        {pg =>  '15', dst => HOST_DB_STANDBY, tls => 0, stg => AZURE, enc => 0, cmp => NONE, rt => 2, bnd => 1, bi => 1},
+        {pg => '9.0', dst => 'db-primary', tls => 0, stg =>   GCS, enc => 1, cmp =>  BZ2, rt => 2, bnd => 1, bi => 0},
+        {pg => '9.1', dst => 'db-standby', tls => 1, stg =>   GCS, enc => 0, cmp =>   GZ, rt => 1, bnd => 0, bi => 0},
+        {pg => '9.2', dst => 'db-standby', tls => 0, stg => POSIX, enc => 1, cmp => NONE, rt => 1, bnd => 1, bi => 0},
+        {pg => '9.3', dst =>     'backup', tls => 0, stg => AZURE, enc => 0, cmp => NONE, rt => 2, bnd => 0, bi => 0},
+        {pg => '9.4', dst => 'db-standby', tls => 0, stg => POSIX, enc => 1, cmp =>  LZ4, rt => 1, bnd => 1, bi => 0},
+        {pg => '9.5', dst =>     'backup', tls => 1, stg =>    S3, enc => 0, cmp =>  BZ2, rt => 1, bnd => 0, bi => 0},
+        {pg => '9.6', dst =>     'backup', tls => 0, stg => POSIX, enc => 0, cmp => NONE, rt => 2, bnd => 1, bi => 1},
+        {pg =>  '10', dst => 'db-standby', tls => 1, stg =>    S3, enc => 1, cmp =>   GZ, rt => 2, bnd => 0, bi => 0},
+        {pg =>  '11', dst =>     'backup', tls => 1, stg => AZURE, enc => 0, cmp =>  ZST, rt => 2, bnd => 1, bi => 0},
+        {pg =>  '12', dst =>     'backup', tls => 0, stg =>    S3, enc => 1, cmp =>  LZ4, rt => 1, bnd => 0, bi => 0},
+        {pg =>  '13', dst => 'db-standby', tls => 1, stg =>   GCS, enc => 0, cmp =>  ZST, rt => 1, bnd => 1, bi => 0},
+        {pg =>  '14', dst =>     'backup', tls => 0, stg => POSIX, enc => 1, cmp =>  LZ4, rt => 2, bnd => 0, bi => 0},
+        {pg =>  '15', dst => 'db-standby', tls => 0, stg => AZURE, enc => 0, cmp => NONE, rt => 2, bnd => 1, bi => 1},
     )
     {
         # Only run tests for this pg version
