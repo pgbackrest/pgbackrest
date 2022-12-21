5ea00263ca9928649b917a6262d5c62250c66f09
David Steele <david@pgmasters.net>
2022-12-21 11:56:35 +0700

    Remove dead comment.

--- src/command/verify/file.c
+++ src/command/verify/file.c
@@ -52,8 +52,6 @@ verifyFile(
         if (compressType != compressTypeNone)
             ioFilterGroupAdd(filterGroup, decompressFilter(compressType));
 
-        // !!! ADD SUPPORT FOR BLOCK INCR
-
         // Add sha1 filter
         ioFilterGroupAdd(filterGroup, cryptoHashNew(hashTypeSha1));
 
