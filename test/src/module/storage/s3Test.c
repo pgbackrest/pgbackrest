5506987099969f300903bf22738255a809f31469
David Steele <david@pgmasters.net>
2023-03-06 09:26:59 +0700

    Increase timeout.

--- test/src/module/storage/s3Test.c
+++ test/src/module/storage/s3Test.c
@@ -403,7 +403,7 @@ testRun(void)
             }
             HRN_FORK_CHILD_END();
 
-            HRN_FORK_CHILD_BEGIN(.prefix = "auth server", .timeout = 10000)
+            HRN_FORK_CHILD_BEGIN(.prefix = "auth server", .timeout = 15000)
             {
                 TEST_RESULT_VOID(
                     hrnServerRunP(HRN_FORK_CHILD_READ(), hrnServerProtocolSocket, .port = authPort), "auth server run");
