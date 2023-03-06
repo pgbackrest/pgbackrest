44a401168bfd08a019842ccd730f32255d0aff3a
Reid Thompson <jreidthompson@nc.rr.com>
2023-03-06 14:39:21 -0500

    Add new file HostSftpTest.pm for sftp

---
+++ test/lib/pgBackRestTest/Env/Host/HostSftpTest.pm
@@ -0,0 +1,77 @@
+####################################################################################################################################
+# SFTP Test Host
+####################################################################################################################################
+package pgBackRestTest::Env::Host::HostSftpTest;
+use parent 'pgBackRestTest::Common::HostTest';
+
+####################################################################################################################################
+# Perl includes
+####################################################################################################################################
+use strict;
+use warnings FATAL => qw(all);
+use Carp qw(confess);
+
+use Cwd qw(abs_path);
+use Exporter qw(import);
+    our @EXPORT = qw();
+use File::Basename qw(dirname);
+use Storable qw(dclone);
+
+use pgBackRestDoc::Common::Exception;
+use pgBackRestDoc::Common::Ini;
+use pgBackRestDoc::Common::Log;
+use pgBackRestDoc::ProjectInfo;
+
+use pgBackRestTest::Common::ContainerTest;
+use pgBackRestTest::Common::ExecuteTest;
+use pgBackRestTest::Common::HostGroupTest;
+use pgBackRestTest::Common::RunTest;
+use pgBackRestTest::Common::StorageRepo;
+use pgBackRestTest::Common::Wait;
+use pgBackRestTest::Env::Host::HostBaseTest;
+use pgBackRestTest::Env::Manifest;
+
+####################################################################################################################################
+# SFTP defaults
+####################################################################################################################################
+use constant HOST_SFTP_ACCOUNT                                      => TEST_USER;
+    push @EXPORT, qw(HOST_SFTP_ACCOUNT);
+use constant HOST_SFTP_HOSTKEY_HASH_TYPE                            => 'sha1';
+    push @EXPORT, qw(HOST_SFTP_HOSTKEY_HASH_TYPE);
+
+####################################################################################################################################
+# new
+####################################################################################################################################
+sub new
+{
+    my $class = shift;          # Class name
+
+    # Assign function parameters, defaults, and log debug info
+    my
+    (
+        $strOperation,
+    ) =
+        logDebugParam
+        (
+            __PACKAGE__ . '->new', \@_,
+        );
+
+    # Create the host
+    my $strProjectPath = dirname(dirname(abs_path($0)));
+    my $strTestSshPath = "${strProjectPath}/test/ssh";
+
+    my $self = $class->SUPER::new(
+                #strName   strContainer                                     strImage       strUser  OS    mount  options paramd
+                HOST_SFTP, 'test-' . testRunGet()->vmId() . '-' . HOST_SFTP, 'atmoz/sftp', 'root', 'u18', ["${strTestSshPath}:/home/vagrant/.ssh"], undef, 'vagrant:pass:1000:1000:.ssh,/,/archive,/backup',
+        false);
+    bless $self, $class;
+
+    # Return from function and log return values if any
+    return logDebugReturn
+    (
+        $strOperation,
+        {name => 'self', value => $self, trace => true}
+    );
+}
+
+1;
