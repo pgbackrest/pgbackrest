95ff53a4478e1eebd22c748efe36dff55c90f30c
David Steele <david@pgmasters.net>
2023-04-25 11:59:11 +0300

    Update list.

--- test/lib/pgBackRestTest/Common/DbVersion.pm
+++ test/lib/pgBackRestTest/Common/DbVersion.pm
@@ -49,7 +49,7 @@ sub versionSupport
     my ($strOperation) = logDebugParam(__PACKAGE__ . '->versionSupport');
 
     my @strySupportVersion = (PG_VERSION_93, PG_VERSION_94, PG_VERSION_95, PG_VERSION_96, PG_VERSION_10, PG_VERSION_11,
-                              PG_VERSION_12, PG_VERSION_13, PG_VERSION_14, PG_VERSION_15, PG_VERSION_15);
+                              PG_VERSION_12, PG_VERSION_13, PG_VERSION_14, PG_VERSION_15, PG_VERSION_16);
 
     # Return from function and log return values if any
     return logDebugReturn
