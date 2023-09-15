9039d20b5bd73d3732ed6dac0a2bba4558176d8f
David Steele <david@pgmasters.net>
2023-09-15 09:30:40 -0400

    Add report option to check command.
    This option is intended to eventually create a comprehensive report about the state of the pgBackRest configuration based on the results of the check command.
    
    Implement a detailed report of the configuration options in the environment and configuration files. This should be useful information when debugging configuration errors, since invalid options and configurations are automatically noted. While custom config locations will not be found automatically, it will at least be clear that the config is not in a standard location.
    
    For now keep this option internal since there is a lot of work to be done, but commit it so that it can be used when needed and tested in various environments.
    
    Note that for now when --report is specified, the check command is not being run at all. Only the config report is generated. This behavior will be improved in the future.

--- src/config/parse.h
+++ src/config/parse.h
@@ -4,8 +4,20 @@ Parse Configuration
 #ifndef CONFIG_PARSE_H
 #define CONFIG_PARSE_H
 
+#include "common/ini.h"
 #include "config/config.h"
 #include "storage/storage.h"
+
+/***********************************************************************************************************************************
+Define global section name
+***********************************************************************************************************************************/
+#define CFGDEF_SECTION_GLOBAL                                       "global"
+
+/***********************************************************************************************************************************
+Prefix for environment variables
+***********************************************************************************************************************************/
+#define PGBACKREST_ENV                                              "PGBACKREST_"
+#define PGBACKREST_ENV_SIZE                                         (sizeof(PGBACKREST_ENV) - 1)
 
 /***********************************************************************************************************************************
 Option type enum
@@ -62,6 +74,9 @@ FN_EXTERN String *cfgParseCommandRoleName(const ConfigCommand commandId, const C
 // Convert command role enum to String
 FN_EXTERN const String *cfgParseCommandRoleStr(ConfigCommandRole commandRole);
 
+// Return the parsed ini config
+FN_EXTERN const Ini *cfgParseIni(void);
+
 // Parse option name and return option info
 typedef struct CfgParseOptionParam
 {
@@ -79,6 +94,7 @@ typedef struct CfgParseOptionResult
     bool reset;                                                     // Was the option reset?
     bool deprecated;                                                // Is the option deprecated?
     bool beta;                                                      // Is the option in beta?
+    bool multi;                                                     // Can the option be specified multiple times?
 } CfgParseOptionResult;
 
 #define cfgParseOptionP(optionName, ...)                                                                                            \
@@ -107,6 +123,7 @@ FN_EXTERN ConfigOptionDataType cfgParseOptionDataType(ConfigOption optionId);
 // Is the option required?
 FN_EXTERN bool cfgParseOptionRequired(ConfigCommand commandId, ConfigOption optionId);
 
+// Get list of stanzas in the configuration
 FN_EXTERN StringList *cfgParseStanzaList(void);
 
 // Is the option valid for the command?
