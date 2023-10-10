81536cd486d716ebd10eb0637ef6b1b9a61d9dd0
David Steele <david@pgmasters.net>
2023-10-10 16:49:05 -0400

    Simplify description rendering in command and configuration reference.
    The reference documentation was still using a very old version of rendering from before the user guide was introduced. This was preserved in the initial C migration to reduce the diff between Perl and C for testing purposes. The old version used hard linefeeds to simulate paragraphs and reduce the amount of markup that needed to be used. In retrospect this was not a great idea.
    
    Instead use more natural rendering that does not depend on using hard linefeeds between paragraphs.

--- doc/src/command/build/reference.c
+++ doc/src/command/build/reference.c
@@ -41,7 +41,7 @@ referenceOptionRender(
         if (optCfg->beta)
             xmlNodeContentSet(xmlNodeAdd(xmlOption, STRDEF("p")), STRDEF("FOR BETA TESTING ONLY. DO NOT USE IN PRODUCTION."));
 
-        xmlNodeChildAdd(xmlNodeAdd(xmlOption, STRDEF("p")), optHlp->description);
+        xmlNodeChildAdd(xmlOption, optHlp->description);
 
         // Add default value
         StringList *const blockList = strLstNew();
@@ -159,7 +159,7 @@ referenceConfigurationRender(const BldCfg *const bldCfg, const BldHlp *const bld
 
         xmlNodeAttributeSet(xmlIntro, STRDEF("id"), STRDEF("introduction"));
         xmlNodeContentSet(xmlNodeAdd(xmlIntro, STRDEF("title")), STRDEF("Introduction"));
-        xmlNodeChildAdd(xmlNodeAdd(xmlIntro, STRDEF("text")), bldHlp->optIntroduction);
+        xmlNodeChildAdd(xmlIntro, bldHlp->optIntroduction);
 
         for (unsigned int sctIdx = 0; sctIdx < lstSize(bldHlp->sctList); sctIdx++)
         {
@@ -169,7 +169,7 @@ referenceConfigurationRender(const BldCfg *const bldCfg, const BldHlp *const bld
 
             xmlNodeAttributeSet(xmlSection, STRDEF("id"), strNewFmt("section-%s", strZ(section->id)));
             xmlNodeContentSet(xmlSectionTitle, strNewFmt("%s Options", strZ(section->name)));
-            xmlNodeChildAdd(xmlNodeAdd(xmlSection, STRDEF("text")), section->introduction);
+            xmlNodeChildAdd(xmlSection, section->introduction);
 
             for (unsigned int optIdx = 0; optIdx < lstSize(bldHlp->optList); optIdx++)
             {
@@ -238,7 +238,7 @@ referenceCommandRender(const BldCfg *const bldCfg, const BldHlp *const bldHlp)
 
         xmlNodeAttributeSet(xmlIntro, STRDEF("id"), STRDEF("introduction"));
         xmlNodeContentSet(xmlNodeAdd(xmlIntro, STRDEF("title")), STRDEF("Introduction"));
-        xmlNodeChildAdd(xmlNodeAdd(xmlIntro, STRDEF("text")), bldHlp->cmdIntroduction);
+        xmlNodeChildAdd(xmlIntro, bldHlp->cmdIntroduction);
 
         for (unsigned int cmdIdx = 0; cmdIdx < lstSize(bldHlp->cmdList); cmdIdx++)
         {
@@ -257,7 +257,7 @@ referenceCommandRender(const BldCfg *const bldCfg, const BldHlp *const bldHlp)
             xmlNodeContentSet(xmlSectionTitle, strNewFmt("%s Command (", strZ(cmdHlp->title)));
             xmlNodeContentSet(xmlNodeAdd(xmlSectionTitle, STRDEF("id")), cmdHlp->name);
             xmlNodeContentSet(xmlSectionTitle, STRDEF(")"));
-            xmlNodeChildAdd(xmlNodeAdd(xmlSection, STRDEF("text")), cmdHlp->description);
+            xmlNodeChildAdd(xmlSection, cmdHlp->description);
 
             // Build option list for command
             StringList *const sctList = strLstNew();
