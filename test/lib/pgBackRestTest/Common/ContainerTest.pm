f3e4957d77ff54034b2b32e6112e86a10ae6d967
David Steele <david@pgmasters.net>
2023-10-26 13:11:16 -0400

    Update pg_control patch.

--- test/lib/pgBackRestTest/Common/ContainerTest.pm
+++ test/lib/pgBackRestTest/Common/ContainerTest.pm
@@ -664,7 +664,7 @@ sub containerBuild
                     "    git clone https://git.postgresql.org/git/postgresql.git /root/postgresql\n" .
                     "RUN apt-get update\n" .
                     "RUN apt-get install -y libreadline-dev bison flex\n" .
-                    "RUN git -C /root/postgresql fetch origin && git -C /root/postgresql checkout 9ba9c707\n" .
+                    "RUN git -C /root/postgresql fetch origin && git -C /root/postgresql checkout 26f988212ea\n" .
                     "COPY patch/pgcontrol-version.patch /root/pgcontrol-version.patch\n" .
                     "RUN git -C /root/postgresql apply /root/pgcontrol-version.patch\n" .
                     "COPY patch/pgcontrol.patch /root/pgcontrol.patch\n" .
