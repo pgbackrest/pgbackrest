c8795094d418802a0e24d4d19da66973c71b00e5
David Steele <david@pgmasters.net>
2023-12-22 12:48:01 -0300

    Allow const checksum buffers to be returned from backupFile().
    This allows less duplication of buffers.
    
    For delta check return file->pgFileSize/file->pgFileChecksum instead of pgTestSize/pgTestChecksum since this saves one buffer duplication and we know these values are equal since we just checked them.
    
    Also add an assert to ensure copyChecksum is valid relative to size.

--- src/command/backup/protocol.c
+++ src/command/backup/protocol.c
@@ -5,6 +5,7 @@ Backup Protocol Handler
 
 #include "command/backup/file.h"
 #include "command/backup/protocol.h"
+#include "common/crypto/hash.h"
 #include "common/debug.h"
 #include "common/io/io.h"
 #include "common/log.h"
@@ -87,6 +88,10 @@ backupFileProtocol(PackRead *const param, ProtocolServer *const server)
         for (unsigned int resultIdx = 0; resultIdx < lstSize(result); resultIdx++)
         {
             const BackupFileResult *const fileResult = lstGet(result, resultIdx);
+
+            ASSERT(
+                fileResult->backupCopyResult == backupCopyResultSkip || fileResult->copySize != 0 ||
+                bufEq(fileResult->copyChecksum, HASH_TYPE_SHA1_ZERO_BUF));
 
             pckWriteStrP(resultPack, fileResult->manifestFile);
             pckWriteU32P(resultPack, fileResult->backupCopyResult);
