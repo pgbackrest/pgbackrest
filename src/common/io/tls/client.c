b37e21ddd4669d90572588a9e38971674400770b
David Steele <david@pgmasters.net>
2024-07-08 12:52:01 +0700

    Headers try again.

--- src/common/io/tls/client.c
+++ src/common/io/tls/client.c
@@ -3,7 +3,10 @@ TLS Client
 ***********************************************************************************************************************************/
 #include "build.auto.h"
 
+// {uncrustify_off - header order required for FreeBSD}
+#include <sys/socket.h>
 #include <arpa/inet.h>
+// {uncrustify_on}
 #include <strings.h>
 
 #include "common/crypto/common.h"
