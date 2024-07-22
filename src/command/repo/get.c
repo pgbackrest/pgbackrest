e70de63daf0888f58d87b903d6803a093b78f246
David Steele <david@pgmasters.net>
2024-07-22 14:50:45 +0700

    Remove obsolete comment.

--- src/command/repo/get.c
+++ src/command/repo/get.c
@@ -51,7 +51,6 @@ storageGetProcess(IoWrite *const destination)
         {
             const CipherType repoCipherType = cfgOptionStrId(cfgOptRepoCipherType);
 
-            // !!! VERSION-ID WILL NOT WORK CORRECTLY IF ENCRYPTED AND MANIFEST HAS BEEN REMOVED
             if (repoCipherType != cipherTypeNone)
             {
                 // Check for a passphrase parameter
