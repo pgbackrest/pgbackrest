pgbackrest/pgbackrest

commit e634fd85cedc1ae3aef7c4fa2556d7d73d05cb8a

Prevent invalid recovery when backup_label removed.
If backup_label is removed from a restored backup then PostgreSQL will instead use checkpoint information from pg_control to attempt (what is thinks is) crash recovery. This will nearly always result in a corrupt cluster because the checkpoint will not be from the beginning of the backup, and even if it is, the end point will not be specified, which could lead to recovery stopping too early.

To prevent this, invalidate the checkpoint LSN in pg_control on restore. If backup_label is removed then recovery will still fail because PostgreSQL will not be able to find the invalid checkpoint. The LSN of the checkpoint is not logged but it will be visible in pg_controldata output as 0/DEAD. This value is invalid because PostgreSQL always skips the first WAL segment when initializing a cluster.

src/command/restore/restore.c
@@ -2612,6 +2612,22 @@ cmdRestore(void)
         // from being started.
         if (storageExistsP(storagePg(), STRDEF(PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL "." STORAGE_FILE_TEMP_EXT)))
         {
+            // Invalidate the checkpoint in pg_control so the cluster cannot be started without backup_label
+            if (manifestData(jobData.manifest)->backupOptionOnline)
+            {
+                Buffer *const pgControlBuffer = storageGetP(
+                    storageNewReadP(storagePg(), STRDEF(PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL "." STORAGE_FILE_TEMP_EXT)));
+                const ManifestFile pgControlFile = manifestFileFind(
+                    jobData.manifest, STRDEF(MANIFEST_TARGET_PGDATA "/" PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL));
+
+                pgControlCheckpointInvalidate(pgControlBuffer, cfgOptionStrNull(cfgOptPgVersionForce));
+                storagePutP(
+                    storageNewWriteP(
+                        storagePgWrite(), STRDEF(PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL "." STORAGE_FILE_TEMP_EXT),
+                        .modeFile = pgControlFile.mode, .timeModified = pgControlFile.timestamp),
+                    pgControlBuffer);
+            }
+
             LOG_INFO(
                 "restore " PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL " (performed last to ensure aborted restores cannot be started)");
 

doc/xml/release/2024/2.51.xml
@@ -75,6 +75,17 @@
             </release-item>
 
             <release-item>
+                <github-pull-request id="2232"/>
+
+                <release-item-contributor-list>
+                    <release-item-contributor id="david.steele"/>
+                    <release-item-reviewer id="stephen.frost"/>
+                </release-item-contributor-list>
+
+                <p>Prevent invalid recovery when <file>backup_label</file> removed.</p>
+            </release-item>
+
+            <release-item>
                 <github-pull-request id="2277"/>
 
                 <release-item-contributor-list>

src/command/archive/get/get.c
@@ -375,8 +375,16 @@ archiveGetCheck(const StringList *archiveRequestList)
         // List of warnings
         StringList *warnList = strLstNew();
 
-        // Get pg control info
+        // Get pg_control info. Error if the invalid checkpoint written by restore is detected and backup_label is not present.
         PgControl controlInfo = pgControlFromFile(storagePg(), cfgOptionStrNull(cfgOptPgVersionForce));
+
+        if (controlInfo.checkpoint == PG_CONTROL_CHECKPOINT_INVALID && !storageExistsP(storagePg(), STRDEF(PG_FILE_BACKUPLABEL)))
+        {
+            THROW(
+                FormatError,
+                "pg_control from backup is not valid without backup_label\n"
+                "HINT: was the backup_label file removed?");
+        }
 
         // Build list of repos/archiveIds where WAL may be found
         List *cacheRepoList = lstNewP(sizeof(ArchiveGetFindCacheRepo));

src/postgres/interface.c
@@ -71,6 +71,9 @@ typedef struct PgInterface
     // Get control crc offset
     size_t (*controlCrcOffset)(void);
 
+    // Invalidate control checkpoint
+    void (*controlCheckpointInvalidate)(unsigned char *);
+
     // Get the control version for this version of PostgreSQL
     uint32_t (*controlVersion)(void);
 
@@ -191,6 +194,88 @@ pgWalSegmentSizeCheck(unsigned int pgVersion, unsigned int walSegmentSize)
     FUNCTION_TEST_RETURN_VOID();
 }
 
+/***********************************************************************************************************************************
+Validate the CRC in pg_control. If the version has been forced this may required scanning pg_control for the offset. Return the
+offset found so it can be used to update the CRC.
+***********************************************************************************************************************************/
+static size_t
+pgControlCrcValidate(const Buffer *const controlFile, const PgInterface *const interface, const bool pgVersionForce)
+{
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(BUFFER, controlFile);
+        FUNCTION_LOG_PARAM_P(VOID, interface);
+        FUNCTION_LOG_PARAM(BOOL, pgVersionForce);
+    FUNCTION_LOG_END();
+
+    ASSERT(controlFile != NULL);
+    ASSERT(interface != NULL);
+
+    size_t result = interface->controlCrcOffset();
+
+    do
+    {
+        // Calculate CRC and retrieve expected CRC
+        const uint32_t crcCalculated =
+            interface->version > PG_VERSION_94 ?
+                crc32cOne(bufPtrConst(controlFile), result) : crc32One(bufPtrConst(controlFile), result);
+        const uint32_t crcExpected = *((uint32_t *)(bufPtrConst(controlFile) + result));
+
+        // If CRC does not match
+        if (crcCalculated != crcExpected)
+        {
+            // If version is forced then the CRC might be later in the file (assuming the fork added extra fields to pg_control).
+            // Increment the offset by CRC data size and continue to try again.
+            if (pgVersionForce)
+            {
+                result += sizeof(uint32_t);
+
+                if (result <= bufUsed(controlFile) - sizeof(uint32_t))
+                    continue;
+            }
+
+            // If no retry then error
+            THROW_FMT(
+                ChecksumError,
+                "calculated " PG_FILE_PGCONTROL " checksum does not match expected value\n"
+                "HINT: calculated 0x%x but expected value is 0x%x\n"
+                "%s"
+                "HINT: is " PG_FILE_PGCONTROL " corrupt?\n"
+                "HINT: does " PG_FILE_PGCONTROL " have a different layout than expected?",
+                crcCalculated, crcExpected,
+                pgVersionForce ? "HINT: checksum values may be misleading due to forced version scan\n" : "");
+        }
+
+        // Do not retry if the CRC is valid
+        break;
+    }
+    while (true);
+
+    FUNCTION_LOG_RETURN(SIZE, result);
+}
+
+/***********************************************************************************************************************************
+Update the CRC in pg_control
+***********************************************************************************************************************************/
+static void
+pgControlCrcUpdate(Buffer *const controlFile, const unsigned int pgVersion, const size_t crcOffset)
+{
+    FUNCTION_LOG_BEGIN(logLevelTrace);
+        FUNCTION_LOG_PARAM(BUFFER, controlFile);
+        FUNCTION_LOG_PARAM(UINT, pgVersion);
+        FUNCTION_LOG_PARAM(SIZE, crcOffset);
+    FUNCTION_LOG_END();
+
+    ASSERT(controlFile != NULL);
+    ASSERT(pgVersion != 0);
+    ASSERT(crcOffset != 0);
+
+    *((uint32_t *)(bufPtr(controlFile) + crcOffset)) =
+        pgVersion > PG_VERSION_94 ?
+            crc32cOne(bufPtrConst(controlFile), crcOffset) : crc32One(bufPtrConst(controlFile), crcOffset);
+
+    FUNCTION_LOG_RETURN_VOID();
+}
+
 /**********************************************************************************************************************************/
 static PgControl
 pgControlFromBuffer(const Buffer *controlFile, const String *const pgVersionForce)
@@ -237,46 +322,8 @@ pgControlFromBuffer(const Buffer *controlFile, const String *const pgVersionForc
     PgControl result = interface->control(bufPtrConst(controlFile));
     result.version = interface->version;
 
-    // Check CRC
-    size_t crcOffset = interface->controlCrcOffset();
-
-    do
-    {
-        // Calculate CRC and retrieve expected CRC
-        const uint32_t crcCalculated =
-            result.version > PG_VERSION_94 ?
-                crc32cOne(bufPtrConst(controlFile), crcOffset) : crc32One(bufPtrConst(controlFile), crcOffset);
-        const uint32_t crcExpected = *((uint32_t *)(bufPtrConst(controlFile) + crcOffset));
-
-        // If CRC does not match
-        if (crcCalculated != crcExpected)
-        {
-            // If version is forced then the CRC might be later in the file (assuming the fork added extra fields to pg_control).
-            // Increment the offset by CRC data size and continue to try again.
-            if (pgVersionForce != NULL)
-            {
-                crcOffset += sizeof(uint32_t);
-
-                if (crcOffset <= bufUsed(controlFile) - sizeof(uint32_t))
-                    continue;
-            }
-
-            // If no retry then error
-            THROW_FMT(
-                ChecksumError,
-                "calculated " PG_FILE_PGCONTROL " checksum does not match expected value\n"
-                "HINT: calculated 0x%x but expected value is 0x%x\n"
-                "%s"
-                "HINT: is " PG_FILE_PGCONTROL " corrupt?\n"
-                "HINT: does " PG_FILE_PGCONTROL " have a different layout than expected?",
-                crcCalculated, crcExpected,
-                pgVersionForce == NULL ? "" : "HINT: checksum values may be misleading due to forced version scan\n");
-        }
-
-        // Do not retry if the CRC is valid
-        break;
-    }
-    while (true);
+    // Validate the CRC
+    pgControlCrcValidate(controlFile, interface, pgVersionForce != NULL);
 
     // Check the segment size
     pgWalSegmentSizeCheck(result.version, result.walSegmentSize);
@@ -387,6 +434,27 @@ pgControlFromFile(const Storage *const storage, const String *const pgVersionFor
     bufFree(buffer);
 
     FUNCTION_LOG_RETURN(PG_CONTROL, result);
+}
+
+/**********************************************************************************************************************************/
+FN_EXTERN void
+pgControlCheckpointInvalidate(Buffer *const buffer, const String *const pgVersionForce)
+{
+    FUNCTION_LOG_BEGIN(logLevelDebug);
+        FUNCTION_LOG_PARAM(BUFFER, buffer);
+        FUNCTION_LOG_PARAM(STRING, pgVersionForce);
+    FUNCTION_LOG_END();
+
+    ASSERT(buffer != NULL);
+
+    const PgControl pgControl = pgControlFromBuffer(buffer, pgVersionForce);
+    const PgInterface *const interface = pgInterfaceVersion(pgControl.version);
+    const size_t crcOffset = pgControlCrcValidate(buffer, interface, pgVersionForce);
+
+    pgInterfaceVersion(pgControl.version)->controlCheckpointInvalidate(bufPtr(buffer));
+    pgControlCrcUpdate(buffer, interface->version, crcOffset);
+
+    FUNCTION_LOG_RETURN_VOID();
 }
 
 /**********************************************************************************************************************************/

src/postgres/interface.h
@@ -95,6 +95,11 @@ WAL segment size is supported for versions below 11.
 #define PG_WAL_SEGMENT_SIZE_DEFAULT                                 ((unsigned int)(16 * 1024 * 1024))
 
 /***********************************************************************************************************************************
+Checkpoint written into pg_control on restore. This will prevent PostgreSQL from starting if backup_label is not present.
+***********************************************************************************************************************************/
+#define PG_CONTROL_CHECKPOINT_INVALID                               0xDEAD
+
+/***********************************************************************************************************************************
 PostgreSQL Control File Info
 ***********************************************************************************************************************************/
 typedef struct PgControl
@@ -137,6 +142,9 @@ FN_EXTERN bool pgDbIsSystemId(unsigned int id);
 // Get info from pg_control
 FN_EXTERN Buffer *pgControlBufferFromFile(const Storage *storage, const String *pgVersionForce);
 FN_EXTERN PgControl pgControlFromFile(const Storage *storage, const String *pgVersionForce);
+
+// Invalidate checkpoint record in pg_control
+FN_EXTERN void pgControlCheckpointInvalidate(Buffer *buffer, const String *pgVersionForce);
 
 // Get the control version for a PostgreSQL version
 FN_EXTERN uint32_t pgControlVersion(unsigned int pgVersion);

src/postgres/interface/version.intern.h
@@ -93,6 +93,22 @@ Get control crc offset
 #endif
 
 /***********************************************************************************************************************************
+Invalidate control checkpoint. PostgreSQL skips the first segment so any LSN in that segment is invalid.
+***********************************************************************************************************************************/
+#if PG_VERSION > PG_VERSION_MAX
+
+#elif PG_VERSION >= PG_VERSION_93
+
+#define PG_INTERFACE_CONTROL_CHECKPOINT_INVALIDATE(version)                                                                        \
+    static void                                                                                                                    \
+    pgInterfaceControlCheckpointInvalidate##version(unsigned char *const controlFile)                                              \
+    {                                                                                                                              \
+        ((ControlFileData *)controlFile)->checkPoint = PG_CONTROL_CHECKPOINT_INVALID;                                              \
+    }
+
+#endif
+
+/***********************************************************************************************************************************
 Get the control version
 ***********************************************************************************************************************************/
 #if PG_VERSION > PG_VERSION_MAX

test/src/module/command/archiveGetTest.c
@@ -136,9 +136,27 @@ testRun(void)
         TEST_STORAGE_LIST_EMPTY(storageSpool(), STORAGE_SPOOL_ARCHIVE_IN);
 
         // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("pg_control from backup is not valid without backup_label");
+
+        HRN_PG_CONTROL_PUT(storagePgWrite(), PG_VERSION_10, .checkpoint = PG_CONTROL_CHECKPOINT_INVALID);
+
+        strLstAddZ(argList, "000000010000000100000001");
+        HRN_CFG_LOAD(cfgCmdArchiveGet, argList, .role = cfgCmdRoleAsync);
+
+        TEST_ERROR(
+            cmdArchiveGetAsync(), FormatError,
+            "pg_control from backup is not valid without backup_label\n"
+            "HINT: was the backup_label file removed?");
+
+        TEST_RESULT_LOG(
+            "P00   INFO: get 1 WAL file(s) from archive: 000000010000000100000001");
+        TEST_STORAGE_LIST(storageSpoolWrite(), STORAGE_SPOOL_ARCHIVE_IN, "global.error\n", .remove = true);
+
+        // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("no segments to find");
 
-        HRN_PG_CONTROL_PUT(storagePgWrite(), PG_VERSION_10);
+        // Success not that backup_label exists
+        HRN_STORAGE_PUT_EMPTY(storagePgWrite(), PG_FILE_BACKUPLABEL);
 
         HRN_INFO_PUT(
             storageRepoWrite(), INFO_ARCHIVE_PATH_FILE,
@@ -148,7 +166,6 @@ testRun(void)
             "[db:history]\n"
             "1={\"db-id\":" HRN_PG_SYSTEMID_10_Z ",\"db-version\":\"10\"}\n");
 
-        strLstAddZ(argList, "000000010000000100000001");
         HRN_CFG_LOAD(cfgCmdArchiveGet, argList, .role = cfgCmdRoleAsync);
 
         TEST_RESULT_VOID(cmdArchiveGetAsync(), "get async");

test/src/module/command/restoreTest.c
@@ -2503,6 +2503,8 @@ testRun(void)
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("incremental delta");
 
+        const char *pgControlSha1 = NULL;
+
         argList = strLstNew();
         hrnCfgArgRawZ(argList, cfgOptStanza, "test1");
         hrnCfgArgRaw(argList, cfgOptRepoPath, repoPath);
@@ -2548,13 +2550,12 @@ testRun(void)
             HRN_MANIFEST_PATH_ADD(manifest, .name = TEST_PGDATA PG_PATH_GLOBAL);
 
             // global/pg_control
-            Buffer *fileBuffer = bufNew(8192);
-            memset(bufPtr(fileBuffer), 255, bufSize(fileBuffer));
-            bufUsedSet(fileBuffer, bufSize(fileBuffer));
+            Buffer *fileBuffer = hrnPgControlToBuffer(0, 0, (PgControl){.version = PG_VERSION_10});
+            pgControlSha1 = strZ(strNewEncode(encodingHex, cryptoHashOne(hashTypeSha1, fileBuffer)));
 
             HRN_MANIFEST_FILE_ADD(
                 manifest, .name = TEST_PGDATA PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL, .size = 8192, .timestamp = 1482182860,
-                .checksumSha1 = "5e2b96c19c4f5c63a5afa2de504d29fe64a4c908");
+                .checksumSha1 = pgControlSha1);
             HRN_STORAGE_PUT(storageRepoWrite(), TEST_REPO_PATH PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL, fileBuffer);
 
             // global/888
@@ -2834,7 +2835,7 @@ testRun(void)
 
         TEST_RESULT_VOID(cmdRestore(), "successful restore");
 
-        TEST_RESULT_LOG(
+        TEST_RESULT_LOG_FMT(
             "P00   INFO: repo1: restore backup set 20161219-212741F_20161219-212918I\n"
             "P00   INFO: map link 'pg_hba.conf' to '../config/pg_hba.conf'\n"
             "P00   INFO: map link 'pg_wal' to '../wal'\n"
@@ -2865,7 +2866,7 @@ testRun(void)
             "P01 DETAIL: restore file " TEST_PATH "/pg/base/16384/16385 (16KB, [PCT]) checksum"
             " d74e5f7ebe52a3ed468ba08c5b6aefaccd1ca88f\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/global/pg_control.pgbackrest.tmp (8KB, [PCT])"
-            " checksum 5e2b96c19c4f5c63a5afa2de504d29fe64a4c908\n"
+            " checksum %s\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/base/1/2 (8KB, [PCT]) checksum 4d7b2a36c5387decf799352a3751883b7ceb96aa\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/postgresql.conf (15B, [PCT]) checksum"
             " 98b8abb2e681e2a5a7d8ab082c0a79727887558d\n"
@@ -2911,7 +2912,8 @@ testRun(void)
             "P00 DETAIL: sync path '" TEST_PATH "/pg/pg_tblspc/1/PG_10_201707211'\n"
             "P00   INFO: restore global/pg_control (performed last to ensure aborted restores cannot be started)\n"
             "P00 DETAIL: sync path '" TEST_PATH "/pg/global'\n"
-            "P00   INFO: restore size = [SIZE], file total = 23");
+            "P00   INFO: restore size = [SIZE], file total = 23",
+            pgControlSha1);
 
         TEST_STORAGE_LIST(
             storagePg(), NULL,
@@ -3028,7 +3030,7 @@ testRun(void)
 
         TEST_RESULT_VOID(cmdRestore(), "successful restore");
 
-        TEST_RESULT_LOG(
+        TEST_RESULT_LOG_FMT(
             "P00   INFO: repo2: restore backup set 20161219-212741F_20161219-212918I, recovery will start at [TIME]\n"
             "P00   INFO: map link 'pg_hba.conf' to '../config/pg_hba.conf'\n"
             "P00   INFO: map link 'pg_wal' to '../wal'\n"
@@ -3058,7 +3060,7 @@ testRun(void)
             "P01 DETAIL: restore file " TEST_PATH "/pg/base/16384/16385 - exists and matches backup (16KB, [PCT])"
             " checksum d74e5f7ebe52a3ed468ba08c5b6aefaccd1ca88f\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/global/pg_control.pgbackrest.tmp (8KB, [PCT])"
-            " checksum 5e2b96c19c4f5c63a5afa2de504d29fe64a4c908\n"
+            " checksum %s\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/base/1/2 (8KB, [PCT]) checksum 4d7b2a36c5387decf799352a3751883b7ceb96aa\n"
             "P01 DETAIL: restore file " TEST_PATH "/pg/postgresql.conf - exists and matches backup (15B, [PCT])"
             " checksum 98b8abb2e681e2a5a7d8ab082c0a79727887558d\n"
@@ -3104,7 +3106,8 @@ testRun(void)
             "P00 DETAIL: sync path '" TEST_PATH "/pg/pg_tblspc/1/PG_10_201707211'\n"
             "P00   INFO: restore global/pg_control (performed last to ensure aborted restores cannot be started)\n"
             "P00 DETAIL: sync path '" TEST_PATH "/pg/global'\n"
-            "P00   INFO: restore size = [SIZE], file total = 23");
+            "P00   INFO: restore size = [SIZE], file total = 23",
+            pgControlSha1);
 
         // Check stanza archive spool path was removed
         TEST_STORAGE_LIST_EMPTY(storageSpool(), STORAGE_PATH_ARCHIVE);

commit ed9b0c260a6e1f05a17d5d2b86c274afdd2d1b05

Fix permissions when restore run as root user.
When restore was run as the root user the pg_control file would end up with root permissions. This bug was introduced in e634fd8. Fix this by directly overwriting the pg_control temp file rather than doing an atomic write that updates permissions. Also update other parameters to more closely match similar calls.

There was also an adjacent error where restore as the root user would fail if the base path did not exist. Fix this by ignoring the missing path since it will be created later and this logic is just trying to find an alternate user for permissions if the user in the manifest does not exist.

doc/xml/release/2024/2.54.xml
@@ -1,5 +1,20 @@
 <release date="XXXX-XX-XX" version="2.54dev" title="UNDER DEVELOPMENT">
     <release-core-list>
+        <release-bug-list>
+            <release-item>
+                <github-issue id="2407"/>
+                <github-pull-request id="2416"/>
+
+                <release-item-contributor-list>
+                    <release-item-ideator id="will.m"/>
+                    <release-item-contributor id="david.steele"/>
+                    <release-item-reviewer id="stefan.fercot"/>
+                </release-item-contributor-list>
+
+                <p>Fix permissions when <cmd>restore</cmd> run as root user.</p>
+            </release-item>
+        </release-bug-list>
+
         <release-improvement-list>
             <release-item>
                 <github-pull-request id="2399"/>

doc/xml/release/contributor.xml
@@ -1098,6 +1098,10 @@
     <contributor-id type="github">vthriller</contributor-id>
 </contributor>
 
+<contributor id="will.m">
+    <contributor-name-display>Will M</contributor-name-display>
+</contributor>
+
 <contributor id="william.cox">
     <contributor-name-display>William Cox</contributor-name-display>
     <contributor-id type="github">mydimension</contributor-id>

src/command/restore/restore.c
@@ -778,7 +778,7 @@ restoreManifestOwner(const Manifest *const manifest, const String **const rootRe
         if (userRoot())
         {
             // Get user/group info from data directory to use for invalid user/groups
-            StorageInfo pathInfo = storageInfoP(storagePg(), manifestTargetBase(manifest)->path);
+            StorageInfo pathInfo = storageInfoP(storagePg(), manifestTargetBase(manifest)->path, .ignoreMissing = true);
 
             // If user/group is null then set it to root
             if (pathInfo.user == NULL)                                                                              // {vm_covered}
@@ -2618,7 +2618,7 @@ cmdRestore(void)
                 storagePutP(
                     storageNewWriteP(
                         storagePgWrite(), STRDEF(PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL "." STORAGE_FILE_TEMP_EXT),
-                        .modeFile = pgControlFile.mode, .timeModified = pgControlFile.timestamp),
+                        .timeModified = pgControlFile.timestamp, .noAtomic = true, .noCreatePath = true, .noSyncPath = true),
                     pgControlBuffer);
             }
 

test/src/common/harnessHost.c
@@ -266,6 +266,7 @@ hrnHostExecBr(HrnHost *const this, const char *const command, const HrnHostExecB
     FUNCTION_HARNESS_BEGIN();
         FUNCTION_HARNESS_PARAM(HRN_HOST, this);
         FUNCTION_HARNESS_PARAM(STRINGZ, command);
+        FUNCTION_HARNESS_PARAM(STRINGZ, param.user);
         FUNCTION_HARNESS_PARAM(STRINGZ, param.option);
         FUNCTION_HARNESS_PARAM(STRINGZ, param.param);
         FUNCTION_HARNESS_PARAM(INT, param.resultExpect);
@@ -278,6 +279,7 @@ hrnHostExecBr(HrnHost *const this, const char *const command, const HrnHostExecB
 
     MEM_CONTEXT_TEMP_BEGIN()
     {
+        const String *const user = param.user == NULL ? NULL : STR(param.user);
         String *const commandStr = strCatFmt(strNew(), "pgbackrest --stanza=" HRN_STANZA);
 
         if (param.option != NULL)
@@ -288,7 +290,7 @@ hrnHostExecBr(HrnHost *const this, const char *const command, const HrnHostExecB
         if (param.param != NULL)
             strCatFmt(commandStr, " %s", param.param);
 
-        strCat(result, hrnHostExecP(this, commandStr, .resultExpect = param.resultExpect));
+        strCat(result, hrnHostExecP(this, commandStr, .user = user, .resultExpect = param.resultExpect));
     }
     MEM_CONTEXT_TEMP_END();
 

test/src/common/harnessHost.h
@@ -315,6 +315,7 @@ String *hrnHostExec(HrnHost *this, const String *command, HrnHostExecParam param
 typedef struct HrnHostExecBrParam
 {
     VAR_PARAM_HEADER;
+    const char *user;                                               // User to execute command
     const char *option;                                             // Options
     const char *param;                                              // Parameters
     int resultExpect;                                               // Expected result, if not 0

test/src/module/integration/allTest.c
@@ -117,8 +117,48 @@ testRun(void)
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("standby restore");
         {
-            TEST_HOST_BR(
-                pg2, CFGCMD_RESTORE, .option = zNewFmt("--type=standby --tablespace-map=ts1='%s'", strZ(hrnHostPgTsPath(pg2))));
+            // If the repo is running on pg2 then switch the user to root to test restoring as root. Use this configuration so the
+            // repo is local and root does not need to be configured for TLS or SSH.
+            const char *user = NULL;
+
+            if (pg2 == repo)
+            {
+                user = "root";
+
+                // Create the pg/ts directory so it will not be created with root permissions
+                HRN_STORAGE_PATH_CREATE(hrnHostDataStorage(pg2), strZ(hrnHostPgTsPath(pg2)), .mode = 0700);
+
+                // Create the restore log file so it will not be created with root permissions
+                HRN_STORAGE_PUT_EMPTY(hrnHostDataStorage(pg2), zNewFmt("%s/test-restore.log", strZ(hrnHostLogPath(pg2))));
+            }
+
+            // Run restore
+            const char *option = zNewFmt("--type=standby --tablespace-map=ts1='%s'", strZ(hrnHostPgTsPath(pg2)));
+            TEST_HOST_BR(pg2, CFGCMD_RESTORE, .option = option, .user = user);
+
+            // If the repo is running on pg2 then perform additional ownership tests
+            if (pg2 == repo)
+            {
+                // Update some ownership to root
+                hrnHostExecP(
+                    pg2,
+                    strNewFmt(
+                        "chown %s:%s %s &&"
+                        "chown %s:%s %s/" PG_PATH_GLOBAL " &&"
+                        "chown %s:%s %s/" PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL,
+                        user, user, strZ(hrnHostPgDataPath(pg2)), user, user, strZ(hrnHostPgDataPath(pg2)), user, user,
+                        strZ(hrnHostPgDataPath(pg2))),
+                    .user = STRDEF(user));
+
+                // Expect an error when running without root since permissions cannot be updated
+                TEST_HOST_BR(
+                    pg2, CFGCMD_RESTORE, .option = zNewFmt("--delta %s", option), .user = NULL,
+                    .resultExpect = errorTypeCode(&FileOpenError));
+
+                // Running as root fixes the ownership
+                TEST_HOST_BR(pg2, CFGCMD_RESTORE, .option = zNewFmt("--delta %s", option), .user = user);
+            }
+
             HRN_HOST_PG_START(pg2);
 
             // Check standby