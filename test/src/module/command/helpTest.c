f2bbfad29cee8468239d2340d5cf12204e352d10
David Steele <david@pgbackrest.org>
2025-02-21 08:24:57 -0600

    Fix case in option summary.

--- test/src/module/command/helpTest.c
+++ test/src/module/command/helpTest.c
@@ -347,7 +347,7 @@ testRun(void)
             "  --repo-s3-kms-key-id                S3 repository KMS key\n"
             "  --repo-s3-region                    S3 repository region\n"
             "  --repo-s3-role                      S3 repository role\n"
-            "  --repo-s3-sse-customer-key          S3 Repository SSE Customer Key\n"
+            "  --repo-s3-sse-customer-key          S3 repository SSE customer key\n"
             "  --repo-s3-token                     S3 repository security token\n"
             "  --repo-s3-uri-style                 S3 URI Style [default=host]\n"
             "  --repo-sftp-host                    SFTP repository host\n"
