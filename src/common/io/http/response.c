87339004e59c03d2a03a6ceb8426202346849c37
David Steele <david@pgbackrest.org>
2025-08-13 10:12:29 -0400

    Add HTTP retries for 408 and 429 errors.
    HTTP client errors 408 and 429 were not being retried but there seems to be some benefit to doing so.
    
    408 has only been seen once and in that case the server was returning 400 (with Request Timeout in the response body) but it seems worth doing since this could happen during times of high congestion. Requests are not sent until request content is ready so there is not much else to be done to handle this error.
    
    429 has been seen occasionally in the past but now seems to be common on Cloudflare R2. Ideally we would have a large initial back off here but it is not clear that is worth it at this time. The existing Fibonacci back off should be enough to allow operations to proceed if possible during the configured timeout.

--- src/common/io/http/response.c
+++ src/common/io/http/response.c
@@ -452,6 +452,16 @@ httpResponseContent(HttpResponse *const this)
 }
 
 /**********************************************************************************************************************************/
+FN_EXTERN bool
+httpResponseCodeRetry(const HttpResponse *const this)
+{
+    return
+        httpResponseCode(this) / 100 == HTTP_RESPONSE_CODE_CLASS_RETRY ||
+        httpResponseCode(this) == HTTP_RESPONSE_CODE_REQUEST_TIMEOUT ||
+        httpResponseCode(this) == HTTP_RESPONSE_CODE_TOO_MANY_REQUESTS;
+}
+
+/**********************************************************************************************************************************/
 FN_EXTERN HttpResponseMulti *
 httpResponseMultiNew(const Buffer *const content, const String *const contentType)
 {
