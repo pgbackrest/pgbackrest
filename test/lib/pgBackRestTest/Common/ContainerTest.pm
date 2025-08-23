5de5ed842a21f4fb7c7f7e9c94650e18dcae96d5
David Steele <david@pgbackrest.org>
2025-08-23 11:40:34 -0400

    New CI container build for PostgreSQL 18 beta3.
    Rebuild all containers to get the most recent versions of PostgreSQL.
    
    Update the Debian repository install to match current recommendations. This has already been done for the documentation in fcd00a45.

--- test/lib/pgBackRestTest/Common/ContainerTest.pm
+++ test/lib/pgBackRestTest/Common/ContainerTest.pm
@@ -525,11 +525,10 @@ sub containerBuild
                 if (vmPgRepo($strVm))
                 {
                     $strScript .=
-                        "    echo \"deb http://apt.postgresql.org/pub/repos/apt/ \$(lsb_release -s -c)-pgdg main" .
-                             ($strOS eq VM_U22 && ($strArch eq VM_ARCH_AARCH64 || $strArch eq VM_ARCH_X86_64) ? ' 18' : '') .
-                             "\" >> /etc/apt/sources.list.d/pgdg.list && \\\n" .
-                        "    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - && \\\n" .
-                        "    apt-get update && \\\n";
+                        "    apt-get install -y --no-install-recommends postgresql-common && \\\n" .
+                        "    /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y" .
+                            ($strOS eq VM_U22 && ($strArch eq VM_ARCH_AARCH64 || $strArch eq VM_ARCH_X86_64) ? ' -c 18' : '') .
+                            " && \\\n";
                 }
 
                 $strScript .=
@@ -538,7 +537,7 @@ sub containerBuild
                         "/etc/postgresql-common/createcluster.conf";
             }
 
-            if (defined($oOS->{&VM_DB}) && @{$oOS->{&VM_DB}} > 0)
+            if (defined($oOS->{&VM_DB}) && @{$oOS->{&VM_DB}} > 0 && ($strArch eq VM_ARCH_AARCH64 || $strArch eq VM_ARCH_X86_64))
             {
                 $strScript .= sectionHeader() .
                     "# Install PostgreSQL\n";
