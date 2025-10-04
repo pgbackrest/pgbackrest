24802a08b4a306becb6792f26c9cd8601b57ae5b
David Steele <david@pgbackrest.org>
2025-10-04 13:34:53 +0200

    Improve support for multi-architecture builds.
    On recent versions of Docker it is not necessary to specify the architecture of the require image. The --platform option takes care of it.
    
    Rebuild test images that were modified by this change.

--- test/lib/pgBackRestTest/Common/ContainerTest.pm
+++ test/lib/pgBackRestTest/Common/ContainerTest.pm
@@ -396,9 +396,7 @@ sub containerBuild
 
         # Base image
         ###########################################################################################################################
-        my $strImageParent =
-            (defined($strArch) ? "${strArch}/" : (vmArch($strOS) eq VM_ARCH_X86_64 ? '' : vmArch($strOS) . '/')) .
-            "$$oVm{$strOS}{&VM_IMAGE}";
+        my $strImageParent = $oVm->{$strOS}{&VM_IMAGE};
         $strArch = defined($strArch) ? $strArch : hostArch();
         my $strImage = "${strOS}-base" . (defined($strArch) ? "-${strArch}" : '-' . hostArch());
         my $strCopy = undef;
