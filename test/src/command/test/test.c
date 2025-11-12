pgbackrest/pgbackrest

commit 24802a08b4a306becb6792f26c9cd8601b57ae5b

Improve support for multi-architecture builds.
On recent versions of Docker it is not necessary to specify the architecture of the require image. The --platform option takes care of it.

Rebuild test images that were modified by this change.

test/container.yaml
@@ -16,6 +16,12 @@
   aarch64:
     u22: 1e0f3dbc1772764b31ffdac85a1d1c24abe97cb3
 
+  ppc64le:
+    u22: ff068e44059b064f963bf450831d7733611b8fab
+
+  s390x:
+    u22: ff068e44059b064f963bf450831d7733611b8fab
+
 20250926A:
   x86_64:
     u22: 1e0f3dbc1772764b31ffdac85a1d1c24abe97cb3
@@ -26,9 +32,3 @@
     d11: be044bf120ca3e7d8bed69d5a83b4de4926be87f
     f42: f1c2eceeed7b66d8096a9b891f28eb30f141d1b2
     rh8: 4d141c845abfbdbf402ba447cf2bd2e4357c8a63
-
-  ppc64le:
-    u22: cf3637d799de2b3d847f74777c839271b5c8e268
-
-  s390x:
-    u22: 131ba5585c13943dd950d5aa025ac73ac7c30767

test/lib/pgBackRestTest/Common/ContainerTest.pm
@@ -396,9 +396,7 @@ sub containerBuild
 
         # Base image
         ###########################################################################################################################
-        my $strImageParent =
-            (defined($strArch) ? "${strArch}/" : (vmArch($strOS) eq VM_ARCH_X86_64 ? '' : vmArch($strOS) . '/')) .
-            "$$oVm{$strOS}{&VM_IMAGE}";
+        my $strImageParent = $oVm->{$strOS}{&VM_IMAGE};
         $strArch = defined($strArch) ? $strArch : hostArch();
         my $strImage = "${strOS}-base" . (defined($strArch) ? "-${strArch}" : '-' . hostArch());
         my $strCopy = undef;

commit d83019924bd07bf563ec2d8f7b8417694fd51519

Fix 32-bit CI testing.
32-bit testing was broken by 24802a08, which was attempting to fix multi-architecture builds by using docker to set the architecture.

i386 is not a special case but the prior alternate architectures did not run integration tests. This requires passing the architecture around since the integration test main process runs on the host system, which may be a different architecture.

.github/workflows/test.yml
@@ -29,7 +29,7 @@ jobs:
       matrix:
         include:
           # All tests (without coverage or valgrind) for 32-bit
-          - param: test --vm=d11 --param=no-performance --param=no-coverage --param=no-valgrind
+          - param: test --vm=d11 --vm-arch=i386 --param=no-performance --param=no-coverage --param=no-valgrind
 
           # Debian/Ubuntu documentation
           - param: doc --vm=u22

test/ci.pl
@@ -57,6 +57,7 @@ test.pl [options] doc|test
 # Command line parameters
 ####################################################################################################################################
 my $strVm = "none";
+my $strVmArch;
 my @stryParam;
 my $bNoTempFs;
 my $bSudo;
@@ -66,7 +67,8 @@ GetOptions ('help' => \$bHelp,
             'param=s@' => \@stryParam,
             'no-tempfs' => \$bNoTempFs,
             'sudo' => \$bSudo,
-            'vm=s' => \$strVm)
+            'vm=s' => \$strVm,
+            'vm-arch=s' => \$strVmArch)
     or pod2usage(2);
 
 ####################################################################################################################################
@@ -214,17 +216,20 @@ eval
         }
 
         # Build the container
+        my $strVmArchParam = defined($strVmArch) ? " --vm-arch=${strVmArch}" : '';
+
         if ($strVm ne VM_NONE)
         {
             processBegin("${strVm} build");
-            processExec("${strTestExe} --vm-build --vm=${strVm}", {bShowOutputAsync => true, bOutLogOnError => false});
+            processExec(
+                "${strTestExe} --vm-build --vm=${strVm}${strVmArchParam}", {bShowOutputAsync => true, bOutLogOnError => false});
             processEnd();
         }
 
         processBegin(($strVm eq VM_NONE ? "no container" : $strVm) . ' test');
         processExec(
-            "${strTestExe} --gen-check --log-level-test-file=off --no-coverage-report --vm-max=2 --vm=${strVm}" .
-            (@stryParam != 0 ? " --" . join(" --", @stryParam) : ''),
+            "${strTestExe} --gen-check --log-level-test-file=off --no-coverage-report --vm-max=2 --vm=${strVm}${strVmArchParam}" .
+                (@stryParam != 0 ? " --" . join(" --", @stryParam) : ''),
             {bShowOutputAsync => true, bOutLogOnError => false});
         processEnd();
     }

test/container.yaml
@@ -12,6 +12,10 @@
 # - docker login -u pgbackrest
 # - DATE=YYYYMMDDX;VM=X;ARCH=X;BASE=pgbackrest/test:${VM?}-base-${ARCH?};docker tag ${BASE?} ${BASE?}-${DATE?} && docker push ${BASE?}-${DATE?}
 # **********************************************************************************************************************************
+20251122A:
+  i386:
+    d11: 87481c86b7c24807d701756101eeec195677e895
+
 20251106A:
   aarch64:
     u22: 8ee008aecbcf21b205116969db4000838c989a4a
@@ -29,6 +33,5 @@
 20250822A:
   x86_64:
     a321: dab99d91eed1710c68dae28efa5cd0e27e0a7f60
-    d11: be044bf120ca3e7d8bed69d5a83b4de4926be87f
     f42: f1c2eceeed7b66d8096a9b891f28eb30f141d1b2
     rh8: 4d141c845abfbdbf402ba447cf2bd2e4357c8a63

test/lib/pgBackRestTest/Common/ContainerTest.pm
@@ -509,7 +509,8 @@ sub containerBuild
                         "/etc/postgresql-common/createcluster.conf";
             }
 
-            if (defined($oOS->{&VM_DB}) && @{$oOS->{&VM_DB}} > 0 && ($strArch eq VM_ARCH_AARCH64 || $strArch eq VM_ARCH_X86_64))
+            if (defined($oOS->{&VM_DB}) && @{$oOS->{&VM_DB}} > 0 &&
+                ($strArch eq VM_ARCH_AARCH64 || $strArch eq VM_ARCH_X86_64 || $strArch eq VM_ARCH_I386))
             {
                 $strScript .= sectionHeader() .
                     "# Install PostgreSQL\n";

test/lib/pgBackRestTest/Common/JobTest.pm
@@ -52,6 +52,7 @@ sub new
         $self->{bDryRun},
         $self->{bVmOut},
         $self->{strPlatform},
+        $self->{strVmArch},
         $self->{strImage},
         $self->{iVmIdx},
         $self->{iVmMax},
@@ -87,6 +88,7 @@ sub new
             {name => 'bDryRun'},
             {name => 'bVmOut'},
             {name => 'strPlatform'},
+            {name => 'strVmArch', required => false},
             {name => 'strImage'},
             {name => 'iVmIdx'},
             {name => 'iVmMax'},
@@ -259,6 +261,7 @@ sub run
                 $self->{strTestPath} . "/build/${strVm}/test/src/test-pgbackrest" .
                     ' --repo-path=' . $self->{strTestPath} . '/repo' . ' --test-path=' . $self->{strTestPath} .
                     " --log-level=$self->{strLogLevel}" . ' --vm=' . $self->{oTest}->{&TEST_VM} .
+                    (defined($self->{strVmArch}) ? ' --vm-arch=' . $self->{strVmArch} : '') .
                     ' --vm-id=' . $self->{iVmIdx} . ($self->{bProfile} ? ' --profile' : '') . $strCommandRunParam .
                     ($self->{bLogTimestamp} ? '' : ' --no-log-timestamp') .
                     ($self->{strTimeZone} ? " --tz='$self->{strTimeZone}'" : '') .

test/lib/pgBackRestTest/Common/VmTest.pm
@@ -22,8 +22,6 @@ use pgBackRestTest::Common::DbVersion;
 ####################################################################################################################################
 # VM hash keywords
 ####################################################################################################################################
-use constant VM_ARCH                                                => 'arch';
-    push @EXPORT, qw(VM_ARCH);
 use constant VM_DB                                                  => 'db';
     push @EXPORT, qw(VM_DB);
 use constant VM_DB_TEST                                             => 'db-test';
@@ -90,7 +88,6 @@ my $oyVm =
     &VM_NONE =>
     {
         &VM_OS_BASE => VM_OS_BASE_DEBIAN,
-        &VM_ARCH => VM_ARCH_X86_64,
         &VMDEF_COVERAGE_C => true,
         &VMDEF_PGSQL_BIN => '/usr/lib/postgresql/{[version]}/bin',
 
@@ -110,7 +107,6 @@ my $oyVm =
     {
         &VM_OS_BASE => VM_OS_BASE_ALPINE,
         &VM_IMAGE => 'alpine:3.21',
-        &VM_ARCH => VM_ARCH_X86_64,
         &VMDEF_PG_REPO => false,
         &VMDEF_PGSQL_BIN => '/usr/lib/postgresql/{[version]}/bin',
 
@@ -130,7 +126,6 @@ my $oyVm =
     {
         &VM_OS_BASE => VM_OS_BASE_DEBIAN,
         &VM_IMAGE => 'debian:11',
-        &VM_ARCH => VM_ARCH_I386,
         &VMDEF_PG_REPO => false,
         &VMDEF_PGSQL_BIN => '/usr/lib/postgresql/{[version]}/bin',
 
@@ -150,7 +145,6 @@ my $oyVm =
     {
         &VM_OS_BASE => VM_OS_BASE_RHEL,
         &VM_IMAGE => 'rockylinux/rockylinux:8',
-        &VM_ARCH => VM_ARCH_X86_64,
         &VMDEF_PGSQL_BIN => '/usr/pgsql-{[version]}/bin',
 
         &VM_DB =>
@@ -176,7 +170,6 @@ my $oyVm =
     {
         &VM_OS_BASE => VM_OS_BASE_RHEL,
         &VM_IMAGE => 'fedora:42',
-        &VM_ARCH => VM_ARCH_X86_64,
         &VMDEF_PGSQL_BIN => '/usr/pgsql-{[version]}/bin',
         &VMDEF_COVERAGE_C => true,
 
@@ -200,7 +193,6 @@ my $oyVm =
     {
         &VM_OS_BASE => VM_OS_BASE_DEBIAN,
         &VM_IMAGE => 'ubuntu:22.04',
-        &VM_ARCH => VM_ARCH_X86_64,
         &VMDEF_COVERAGE_C => true,
         &VMDEF_PGSQL_BIN => '/usr/lib/postgresql/{[version]}/bin',
 
@@ -337,30 +329,6 @@ sub vmCoverageC
 }
 
 push @EXPORT, qw(vmCoverageC);
-
-####################################################################################################################################
-# Get vm architecture
-####################################################################################################################################
-sub vmArch
-{
-    my $strVm = shift;
-
-    return $oyVm->{$strVm}{&VM_ARCH};
-}
-
-push @EXPORT, qw(vmArch);
-
-####################################################################################################################################
-# Get vm architecture bits
-####################################################################################################################################
-sub vmArchBits
-{
-    my $strVm = shift;
-
-    return (vmArch($strVm) eq VM_ARCH_I386 ? 32 : 64);
-}
-
-push @EXPORT, qw(vmArchBits);
 
 ####################################################################################################################################
 # Get host architecture

test/src/build/config/config.yaml
@@ -157,6 +157,13 @@ option:
     command:
       test: {}
 
+  vm-arch:
+    type: string
+    internal: true
+    required: false
+    command:
+      test: {}
+
   vm-id:
     type: integer
     required: false

test/src/build/help/help.xml
@@ -212,6 +212,16 @@
                     <example>rh8</example>
                 </option>
 
+                <option id="vm-arch">
+                    <summary>VM architecture to run test on.</summary>
+
+                    <text>
+                        <p>Architecture of VM to run the test on.</p>
+                    </text>
+
+                    <example>aarch64</example>
+                </option>
+
                 <option id="vm-id">
                     <summary>VM id to run test on.</summary>
 

test/src/command/test/test.c
@@ -48,13 +48,31 @@ cmdTestPathCreate(const Storage *const storage, const String *const path)
     FUNCTION_LOG_RETURN_VOID();
 }
 
+/***********************************************************************************************************************************
+Fix vm architectures to project standard
+***********************************************************************************************************************************/
+static const String *
+cmdTestVmArchFix(const String *vmArch)
+{
+    FUNCTION_LOG_BEGIN(logLevelDebug);
+        FUNCTION_LOG_PARAM(STRING, vmArch);
+    FUNCTION_LOG_END();
+
+    if (strEqZ(vmArch, "i686"))
+        vmArch = strNewZ("i386");
+    else if (strEqZ(vmArch, "arm64"))
+        vmArch = strNewZ("aarch64");
+
+    FUNCTION_LOG_RETURN_CONST(STRING, vmArch);
+}
+
 /**********************************************************************************************************************************/
 void
 cmdTest(
     const String *const pathRepo, const String *const pathTest, const String *vm, const unsigned int vmId,
     const String *const pgVersion, const String *moduleName, const unsigned int test, const uint64_t scale, const LogLevel logLevel,
-    const bool logTime, const String *const timeZone, const bool coverage, const bool profile, const bool optimize,
-    const bool backTrace)
+    const bool logTime, const String *const timeZone, const String *architecture, const bool coverage, const bool profile,
+    const bool optimize, const bool backTrace)
 {
     FUNCTION_LOG_BEGIN(logLevelDebug);
         FUNCTION_LOG_PARAM(STRING, pathRepo);
@@ -68,6 +86,7 @@ cmdTest(
         FUNCTION_LOG_PARAM(ENUM, logLevel);
         FUNCTION_LOG_PARAM(BOOL, logTime);
         FUNCTION_LOG_PARAM(STRING, timeZone);
+        FUNCTION_LOG_PARAM(STRING, architecture);
         FUNCTION_LOG_PARAM(BOOL, coverage);
         FUNCTION_LOG_PARAM(BOOL, profile);
         FUNCTION_LOG_PARAM(BOOL, optimize);
@@ -95,7 +114,8 @@ cmdTest(
             vm = strNewZ("none");
 
         // Get test architecture
-        const String *const architecture = strTrim(execOneP(STRDEF("uname -m")));
+        if (architecture == NULL)
+            architecture = cmdTestVmArchFix(strTrim(execOneP(STRDEF("uname -m"))));
 
         // Build test
         bool buildRetry = false;

test/src/command/test/test.h
@@ -15,6 +15,6 @@ Functions
 void cmdTest(
     const String *pathRepo, const String *pathTest, const String *const vm, unsigned int vmId, const String *pgVersion,
     const String *moduleName, unsigned int test, uint64_t scale, LogLevel logLevel, bool logTime, const String *timeZone,
-    bool coverage, bool profile, bool optimize, bool backTrace);
+    const String *architecture, bool coverage, bool profile, bool optimize, bool backTrace);
 
 #endif

test/src/common/harnessHost.c
@@ -161,7 +161,8 @@ hrnHostNew(const StringId id, const String *const container, const String *const
 
             // Run container
             String *const command = strCatFmt(
-                strNew(), "docker run -itd -h %s --name=%s", strZ(hrnHostName(this)), strZ(hrnHostContainer(this)));
+                strNew(), "docker run -itd -h %s --platform linux/%s --name=%s", strZ(hrnHostName(this)), testArchitecture(),
+                strZ(hrnHostContainer(this)));
 
             if (hrnHostDataPath(this) != NULL)
             {

test/src/main.c
@@ -72,8 +72,8 @@ main(int argListSize, const char *argList[])
                         cfgOptionUInt(cfgOptVmId), cfgOptionStr(cfgOptPgVersion), strLstGet(cfgCommandParam(), 0),
                         cfgOptionTest(cfgOptTest) ? cfgOptionUInt(cfgOptTest) : 0, cfgOptionUInt64(cfgOptScale),
                         logLevelEnum(cfgOptionStrId(cfgOptLogLevelTest)), cfgOptionBool(cfgOptLogTimestamp),
-                        cfgOptionStrNull(cfgOptTz), cfgOptionBool(cfgOptCoverage), cfgOptionBool(cfgOptProfile),
-                        cfgOptionBool(cfgOptOptimize), cfgOptionBool(cfgOptBackTrace));
+                        cfgOptionStrNull(cfgOptTz), cfgOptionStrNull(cfgOptVmArch), cfgOptionBool(cfgOptCoverage),
+                        cfgOptionBool(cfgOptProfile), cfgOptionBool(cfgOptOptimize), cfgOptionBool(cfgOptBackTrace));
                 }
                 // Top-level test
                 else

test/src/module/command/backupTest.c
@@ -1951,7 +1951,7 @@ testRun(void)
             "P00   INFO: full backup size = 8KB, file total = 2",
             TEST_64BIT() ?
                 (TEST_BIG_ENDIAN() ? "a73da1cfddf849880b4f6f0f4209ce71043f2ac1" : "807d4464138ef085e41844a9f488f067b3d46d46") :
-                "a90290adaf2e8a36d53b0c15d277e23e1b321fdb");
+                "c09a7f1d2ff6131ec467e18b781970a86efd4e5b");
 
         // Make pg no longer appear to be running
         HRN_STORAGE_REMOVE(storagePgWrite(), PG_FILE_POSTMTRPID, .errorOnMissing = true);

test/src/module/test/testTest.c
@@ -58,12 +58,21 @@ testRun(void)
     const Storage *const storageTest = storagePosixNewP(STRDEF(TEST_PATH), .write = true);
 
     // *****************************************************************************************************************************
-    if (testBegin("cmdBldPathRelative()"))
+    if (testBegin("cmdBldPathRelative() and cmdTestVmArchFix()"))
     {
+        TEST_TITLE("cmdBldPathRelative()");
+
         TEST_ERROR(cmdBldPathRelative(STRDEF("/tmp"), STRDEF("/tmp")), AssertError, "assertion '!strEq(base, compare)' failed");
 
         TEST_RESULT_STR_Z(cmdBldPathRelative(STRDEF("/tmp/sub"), STRDEF("/tmp")), "..", "compare is sub of base");
         TEST_RESULT_STR_Z(cmdBldPathRelative(STRDEF("/tmp"), STRDEF("/tmp/sub")), "sub", "base is sub of compare");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+        TEST_TITLE("cmdTestVmArchFix()");
+
+        TEST_RESULT_STR_Z(cmdTestVmArchFix(STRDEF("i686")), "i386", "i686 -> i386");
+        TEST_RESULT_STR_Z(cmdTestVmArchFix(STRDEF("arm64")), "aarch64", "arm64 -> aarch64");
+        TEST_RESULT_STR_Z(cmdTestVmArchFix(STRDEF("x86_64")), "x86_64", "x86_64 -> x86_64");
     }
 
     // *****************************************************************************************************************************
@@ -87,7 +96,7 @@ testRun(void)
         TEST_ERROR(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("none"), 3, STRDEF("invalid"),
-                STRDEF("common/stack-trace"), 0, 1, logLevelDebug, true, NULL, false, false, false, true),
+                STRDEF("common/stack-trace"), 0, 1, logLevelDebug, true, NULL, NULL, false, false, false, true),
             FormatError, "4 linter error(s) in 'test.c' (see warnings above)");
 
         TEST_RESULT_LOG(
@@ -153,6 +162,9 @@ testRun(void)
         strReplace(testC, STRDEF("{[C_TEST_USER_ID_Z]}"), STRDEF("\"" TEST_USER_ID_Z "\""));
         strReplace(testC, STRDEF("{[C_TEST_USER_LEN]}"), strNewFmt("%zu", sizeof(TEST_USER) - 1));
         strReplace(testC, STRDEF("{[C_TEST_ARCHITECTURE]}"), STRDEF(TEST_ARCHITECTURE));
+
+        const String *const architecture =
+            strEqZ(strTrim(execOneP(STRDEF("uname -m"))), TEST_ARCHITECTURE) ? NULL : strNewZ(TEST_ARCHITECTURE);
 
         // Test definition
         // -------------------------------------------------------------------------------------------------------------------------
@@ -275,7 +287,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("none"), 3, STRDEF("invalid"),
-                STRDEF("common/stack-trace"), 0, 1, logLevelDebug, true, NULL, false, false, false, true),
+                STRDEF("common/stack-trace"), 0, 1, logLevelDebug, true, NULL, architecture, false, false, false, true),
             "new build");
 
         // Older versions of ninja may error on a rebuild so a retry may occur
@@ -402,7 +414,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("none"), 3, STRDEF("invalid"),
-                STRDEF("common/error"), 5, 1, logLevelDebug, true, NULL, false, false, false, true),
+                STRDEF("common/error"), 5, 1, logLevelDebug, true, NULL, architecture, false, false, false, true),
             "new build");
 
         // Older versions of ninja may error on a rebuild so a retry may occur
@@ -600,7 +612,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("test/shim"), 0, 1, logLevelDebug, true, NULL, true,
+                STRDEF("test/shim"), 0, 1, logLevelDebug, true, NULL, architecture, true,
 #ifdef DEBUG_COVERAGE
                 true,
 #else
@@ -763,7 +775,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("test/shim"), 0, 1, logLevelDebug, true, NULL, true,
+                STRDEF("test/shim"), 0, 1, logLevelDebug, true, NULL, architecture, true,
 #ifdef DEBUG_COVERAGE
                 true,
 #else
@@ -792,7 +804,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("real/all"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), false,
+                STRDEF("real/all"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), architecture, false,
 #ifdef DEBUG_COVERAGE
                 true,
 #else
@@ -961,7 +973,7 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), false,
+                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), architecture, false,
 #ifdef DEBUG_COVERAGE
                 true,
 #else
@@ -1112,7 +1124,8 @@ testRun(void)
         TEST_RESULT_VOID(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), false, false, false, false),
+                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), STRDEF(TEST_ARCHITECTURE), false,
+                false, false, false),
             "new build");
 
         // Older versions of ninja may error on a rebuild so a retry may occur
@@ -1152,7 +1165,8 @@ testRun(void)
         TEST_ERROR(
             cmdTest(
                 STRDEF(TEST_PATH "/repo"), storagePathP(storageTest, STRDEF("test")), STRDEF("uXX"), 3, STRDEF("invalid"),
-                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), false, false, false, false),
+                STRDEF("performance/type"), 0, 1, logLevelDebug, true, STRDEF("America/New_York"), architecture, false, false,
+                false, false),
             FileOpenError,
             "build failed for unit performance/type: unable to open file '" TEST_PATH "/repo/meson.build' for read: [13] Permission"
             " denied");

test/test.pl
@@ -972,7 +972,7 @@ eval
             {
                 my $oJob = new pgBackRestTest::Common::JobTest(
                     $oStorageTest, $strBackRestBase, $strTestPath, $$oyTestRun[$iTestIdx], $bDryRun, $bVmOut, $strPlatform,
-                    $strImage, $iVmIdx, $iVmMax, $strMakeCmd, $iTestIdx, $iTestMax, $strLogLevel, $strLogLevelTest,
+                    $strVmArch, $strImage, $iVmIdx, $iVmMax, $strMakeCmd, $iTestIdx, $iTestMax, $strLogLevel, $strLogLevelTest,
                     $strLogLevelTestFile, !$bNoLogTimestamp, $bShowOutputAsync, $bNoCleanup, $iRetry, !$bNoBackTrace, !$bNoValgrind,
                     !$bNoCoverage, $bCoverageSummary, !$bNoOptimize, $bProfile, $iScale, $strTimeZone, !$bNoDebug, $bDebugTestTrace,
                     $iBuildMax / $iVmMax < 1 ? 1 : int($iBuildMax / $iVmMax));