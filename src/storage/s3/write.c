f35d2e3cea9ca60cbd2307fd788ce0d944be31c8
David Steele <david@pgbackrest.org>
2025-11-23 10:07:44 +0400

    Should be working except coverage.

--- src/storage/s3/write.c
+++ src/storage/s3/write.c
@@ -155,29 +155,43 @@ storageWriteS3(THIS_VOID, const Buffer *const buffer)
 
     size_t bytesTotal = 0;
 
-    // !!!
-    size_t partSize;
+    // Resize chunk buffer if it is less than chunk size
+    size_t partBufSize = bufSize(this->partBuffer);
 
-    if (bufSize(this->partBuffer) == 0)
+    if (partBufSize < this->partSize)
     {
-        if (bufFull(buffer))
-            partSize = bufSize(buffer) * 2;
+        // If this is the first chunk
+        if (partBufSize == 0)
+        {
+            // If the input buffer is full it usually means there will be more data coming so make space for it
+            if (bufFull(buffer))
+            {
+                partBufSize = bufSize(buffer) * 2;
+            }
+            // Else no more writes are expected so allocate only what is needed. Add one so multi-part write is not forced.
+            else
+                partBufSize = bufUsed(buffer) + 1;
+        }
+        // Else this is a subsequent chunk
         else
-            partSize = bufUsed(buffer) + 1;
-    }
-    else
-    {
-        if (bufFull(buffer))
-            partSize = bufSize(this->partBuffer) * 2;
-        else
-            partSize = bufSize(this->partBuffer);
-    }
+        {
+            // If the input buffer has been full at least twice then aggressively grow the chunk buffer
+            if (bufFull(buffer))
+            {
+                partBufSize *= 2;
+            }
+            // Else no more writes are expected so allocate only what is needed. Add one so multi-part write is not forced.
+            else if (bufRemains(this->partBuffer) < bufUsed(buffer))
+                partBufSize += bufUsed(buffer) - bufRemains(this->partBuffer) + 1;
+        }
 
-    if (partSize > this->partSize)
-        partSize = this->partSize;
+        // Chunk buffer cannot be larger than chunk size
+        if (partBufSize > this->partSize)
+            partBufSize = this->partSize;
 
-    bufResize(this->partBuffer, partSize);
-    fprintf(stdout, "!!!SIZE %zu\n", bufSize(this->partBuffer));fflush(stdout);
+        // Resize the buffer
+        bufResize(this->partBuffer, partBufSize);
+    }
 
     // Continue until the write buffer has been exhausted
     do
@@ -193,8 +207,6 @@ storageWriteS3(THIS_VOID, const Buffer *const buffer)
         // If the part buffer is full then write it
         if (bufRemains(this->partBuffer) == 0)
         {
-            fprintf(stdout, "!!!WRITE %zu\n", bufSize(this->partBuffer));fflush(stdout);
-
             storageWriteS3PartAsync(this);
             bufUsedZero(this->partBuffer);
 
