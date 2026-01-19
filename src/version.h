d9f778ee3238bd6aaef0e92dba97e70c92b1cd5e
David Steele <david@pgbackrest.org>
2026-01-19 17:17:07 +0700

    Begin v2.59.0 development.

--- src/version.h
+++ src/version.h
@@ -34,11 +34,11 @@ will be invalid unless migration functions are written.
 Project version components. PROJECT_VERSION and PROJECT_VERSION_NUM are automatically generated from the component parts.
 ***********************************************************************************************************************************/
 #define PROJECT_VERSION_MAJOR                                       2
-#define PROJECT_VERSION_MINOR                                       58
+#define PROJECT_VERSION_MINOR                                       59
 #define PROJECT_VERSION_PATCH                                       0
-#define PROJECT_VERSION_SUFFIX                                      ""
+#define PROJECT_VERSION_SUFFIX                                      "dev"
 
-#define PROJECT_VERSION                                             "2.58.0"
-#define PROJECT_VERSION_NUM                                         2058000
+#define PROJECT_VERSION                                             "2.59.0dev"
+#define PROJECT_VERSION_NUM                                         2059000
 
 #endif
