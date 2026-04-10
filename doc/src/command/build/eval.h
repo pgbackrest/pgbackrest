d3cdff17f5c038807045c26c604eed6677c6749b
David Steele <david@pgbackrest.org>
2026-04-10 13:26:21 +0700

    Migrate documentation block rendering to C.
    This also requires a fair amount of support code that cannot be removed from Perl yet.

--- doc/src/command/build/eval.h
+++ doc/src/command/build/eval.h
@@ -1,18 +1,25 @@
 /***********************************************************************************************************************************
-String Handler Extensions
+Expression Evaluation
+
+Simple expression evaluator that handles the subset of expressions used in doc XML if attributes.
+
+Supports:
+    - Negation: !expr
+    - Parentheses: (expr)
+    - String comparison: 'str1' eq 'str2', 'str1' ne 'str2'
+    - Numeric comparison: num > num, num >= num, num < num, num <= num
+    - Logical and: expr && expr
+    - Logical or: expr || expr
 ***********************************************************************************************************************************/
-#ifndef BUILD_COMMON_STRING_H
-#define BUILD_COMMON_STRING_H
+#ifndef DOC_COMMAND_EVAL_H
+#define DOC_COMMAND_EVAL_H
 
 #include "common/type/string.h"
 
 /***********************************************************************************************************************************
 Functions
 ***********************************************************************************************************************************/
-// Upper-case entire string
-String *strUpper(String *this);
-
-// Replace a substring with another string. Returns the number of replacements made.
-unsigned int strReplace(String *this, const String *replace, const String *with);
+// Evaluate an expression and return the boolean result
+FN_EXTERN bool eval(const String *expression);
 
 #endif
