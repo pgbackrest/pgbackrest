bb1be99f94b494580e986c5272d0f03111c67760
David Steele <david@pgbackrest.org>
2026-05-12 12:23:12 +0900

    Fix logging.

--- src/storage/sftp/read.h
+++ src/storage/sftp/read.h
@@ -19,4 +19,12 @@ FN_EXTERN StorageReadSftp *storageReadSftpNew(
     StorageSftp *storage, const String *name, LIBSSH2_SESSION *session, LIBSSH2_SFTP *sftpSession, LIBSSH2_SFTP_HANDLE *sftpHandle,
     uint64_t offset, const Variant *limit);
 
+/***********************************************************************************************************************************
+Macros for function logging
+***********************************************************************************************************************************/
+#define FUNCTION_LOG_STORAGE_READ_SFTP_TYPE                                                                                        \
+    StorageReadSftp *
+#define FUNCTION_LOG_STORAGE_READ_SFTP_FORMAT(value, buffer, bufferSize)                                                           \
+    objNameToLog(value, "StorageReadSftp", buffer, bufferSize)
+
 #endif
