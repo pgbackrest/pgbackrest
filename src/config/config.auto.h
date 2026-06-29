275ae38da975e9367cc1adb15620d22967361ed6
Simon Gratton <sgratton@altassian.com>
2026-06-29 17:26:39 +0800

    chore: correcting config

--- src/config/config.auto.h
+++ src/config/config.auto.h
@@ -144,7 +144,7 @@ Option constants
 #define CFGOPT_VERBOSE                                              "verbose"
 #define CFGOPT_VERSION                                              "version"
 
-#define CFG_OPTION_TOTAL                                            194
+#define CFG_OPTION_TOTAL                                            195
 
 /***********************************************************************************************************************************
 Option value constants
