4e5530f1e674e71bd0b14646a8ab16fc7518362d
David Steele <david@pgbackrest.org>
2026-08-06 10:55:15 +0700

    Revert "Move header check to memContext.c"
    This reverts commit b2456d34c91261f515a38dd01f293c62e471336e.

--- src/common/memContext.c
+++ src/common/memContext.c
@@ -14,19 +14,10 @@ Memory Context Manager
 Valgrind memcheck client requests keep leak, use-after-free, and uninitialized-read detection accurate when large freed
 allocations are retained for reuse rather than returned to the allocator (see the free list below). Without Valgrind support they
 compile to nothing.
-
-The header is detected here rather than in meson so that only this file depends on whether it is installed. A project-wide define
-would change the compile command of every file and so invalidate the entire compile cache when a build that has the header meets
-one that does not.
 ***********************************************************************************************************************************/
-#if defined(DEBUG) && defined(__has_include)
-#if __has_include(<valgrind/memcheck.h>)
+#ifdef WITH_VALGRIND
 #include <valgrind/memcheck.h>
-#define WITH_VALGRIND
-#endif
-#endif
-
-#ifndef WITH_VALGRIND
+#else
 #define VALGRIND_MAKE_MEM_NOACCESS(addr, size)                      ((void)0)
 #define VALGRIND_MAKE_MEM_UNDEFINED(addr, size)                     ((void)0)
 #endif
